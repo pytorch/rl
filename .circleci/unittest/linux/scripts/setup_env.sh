497748593960787f7c84c6f7e0ac4a48b7bab604
Vincent Moens <vincentmoens@gmail.com>
2022-03-10 09:17:13 +0000

    mujoco

--- .circleci/unittest/linux/scripts/setup_env.sh
+++ .circleci/unittest/linux/scripts/setup_env.sh
@@ -34,6 +34,16 @@ if [ ! -d "${env_dir}" ]; then
 fi
 conda activate "${env_dir}"
 
-# 3. Install Conda dependencies
+# 3. Install mujoco
+HERE=$(pwd)
+mkdir $HOME/.mujoco
+cd $HOME/.mujoco/
+wget https://github.com/deepmind/mujoco/releases/download/2.1.1/mujoco-2.1.1-linux-x86_64.tar.gz
+export MUJOCO_GL=egl
+export MJLIB_PATH=$HOME/.mujoco/mujoco-2.1.1/bin/libmujoco210.so
+tar -xf mujoco-2.1.1-linux-x86_64.tar.gz
+cd $HERE
+
+# 4. Install Conda dependencies
 printf "* Installing dependencies (except PyTorch)\n"
 conda env update --file "${this_dir}/environment.yml" --prune
