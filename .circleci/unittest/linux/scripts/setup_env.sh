36ae8ad897488eae078789a987291ec625ec8d65
Vincent Moens <vincentmoens@gmail.com>
2022-03-10 11:15:42 +0000

    osmesa

--- .circleci/unittest/linux/scripts/setup_env.sh
+++ .circleci/unittest/linux/scripts/setup_env.sh
@@ -42,7 +42,6 @@ wget https://github.com/deepmind/mujoco/releases/download/2.1.1/mujoco-2.1.1-lin
 tar -xf mujoco-2.1.1-linux-x86_64.tar.gz
 export MJLIB_PATH=$root_dir/.mujoco/mujoco-2.1.1/bin/libmujoco210.so
 cd $this_dir
-apt update && apt install -y libgl1-mesa-glx libosmesa6
 
 # 4. Install Conda dependencies
 printf "* Installing dependencies (except PyTorch)\n"
