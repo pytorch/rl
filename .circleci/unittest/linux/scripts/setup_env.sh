6c516bcc5aa3eb35dfc9cc76e595296c99a975e2
Vincent Moens <vincentmoens@gmail.com>
2022-03-10 10:41:00 +0000

    osmesa

--- .circleci/unittest/linux/scripts/setup_env.sh
+++ .circleci/unittest/linux/scripts/setup_env.sh
@@ -39,7 +39,6 @@ mkdir $root_dir/.mujoco
 cd $root_dir/.mujoco/
 wget https://github.com/deepmind/mujoco/releases/download/2.1.1/mujoco-2.1.1-linux-x86_64.tar.gz
 tar -xf mujoco-2.1.1-linux-x86_64.tar.gz
-export MUJOCO_GL=osmesa
 export MJLIB_PATH=$root_dir/.mujoco/mujoco-2.1.1/bin/libmujoco210.so
 cd $this_dir
 apt-get update && apt-get install -y libgl1-mesa-glx libosmesa6
