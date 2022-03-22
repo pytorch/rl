ea04077158b33c976760538d0c1df81839d5420a
Vincent Moens <vincentmoens@gmail.com>
2022-03-22 11:54:54 +0000

    g++ error

--- test/_utils_internal.py
+++ test/_utils_internal.py
@@ -14,5 +14,5 @@ def get_available_devices():
     n_cuda = torch.cuda.device_count()
     if n_cuda > 0:
         for i in range(n_cuda):
-            devices += torch.device(f"cuda:{i}")
+            devices += [torch.device(f"cuda:{i}")]
     return devices
