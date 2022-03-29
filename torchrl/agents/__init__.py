572115ade5d0a24a966b8b418780aed674190868
Vincent Moens <vincentmoens@gmail.com>
2022-03-29 17:24:09 +0100

    headers

--- torchrl/agents/__init__.py
+++ torchrl/agents/__init__.py
@@ -1,2 +1,7 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
 from .agents import *
 from .env_creator import *
--- torchrl/objectives/costs/common.py
+++ torchrl/objectives/costs/common.py
@@ -1,3 +1,8 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
 __all__ = ["_LossModule"]
 
 from typing import Iterator, Optional, Tuple
