7604974f595a7620874e28cd3f62eba24f0218d1
Vincent Moens <vincentmoens@gmail.com>
2022-05-11 11:56:58 +0100

    scale rewards for redq

--- examples/mj_envs/redq.py
+++ examples/mj_envs/redq.py
@@ -158,7 +158,7 @@ if __name__ == "__main__":
     for t in recorder.transform:
         if isinstance(t, RewardScaling):
             t.scale.fill_(1.0)
-            t.loc.fill_(1.0)
+            t.loc.fill_(0.0)
 
     trainer = make_trainer(
         collector,
