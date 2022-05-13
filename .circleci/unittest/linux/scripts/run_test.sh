fac7fbf71180696c6edcc9088e3fe03e5f784629
Vincent Moens <vincentmoens@gmail.com>
2022-05-13 15:48:51 +0100

    $PRIVATE_MUJOCO_GL

--- .circleci/unittest/linux/scripts/run_test.sh
+++ .circleci/unittest/linux/scripts/run_test.sh
@@ -5,11 +5,11 @@ set -e
 eval "$(./conda/bin/conda shell.bash hook)"
 conda activate ./env
 
-#if [[ $OSTYPE == 'darwin'* ]]; then
-#PRIVATE_MUJOCO_GL=osmesa
-#else
-#  PRIVATE_MUJOCO_GL=glfw
-#fi
+if [[ $OSTYPE == 'darwin'* ]]; then
+  PRIVATE_MUJOCO_GL=egl
+else
+  PRIVATE_MUJOCO_GL=glfw
+fi
 
 export PYTORCH_TEST_WITH_SLOW='1'
 python -m torch.utils.collect_env
@@ -19,6 +19,6 @@ export MUJOCO_PY_MUJOCO_PATH=$root_dir/.mujoco/mujoco210
 export DISPLAY=unix:0.0
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/root/project/.mujoco/mujoco210/bin
 
-pytest test/smoke_test.py -v --durations 20
-pytest test/smoke_test_deps.py -v --durations 20
-pytest -v --durations 20
+MUJOCO_GL=$PRIVATE_MUJOCO_GL pytest test/smoke_test.py -v --durations 20
+MUJOCO_GL=$PRIVATE_MUJOCO_GL pytest test/smoke_test_deps.py -v --durations 20
+MUJOCO_GL=$PRIVATE_MUJOCO_GL pytest -v --durations 20
--- .circleci/unittest/linux_stable/scripts/run_test.sh
+++ .circleci/unittest/linux_stable/scripts/run_test.sh
@@ -5,11 +5,11 @@ set -e
 eval "$(./conda/bin/conda shell.bash hook)"
 conda activate ./env
 
-#if [[ $OSTYPE == 'darwin'* ]]; then
-#PRIVATE_MUJOCO_GL=osmesa
-#else
-#  PRIVATE_MUJOCO_GL=glfw
-#fi
+if [[ $OSTYPE == 'darwin'* ]]; then
+  PRIVATE_MUJOCO_GL=osmesa
+else
+  PRIVATE_MUJOCO_GL=glfw
+fi
 
 export PYTORCH_TEST_WITH_SLOW='1'
 python -m torch.utils.collect_env
@@ -18,8 +18,7 @@ export MJLIB_PATH=$root_dir/.mujoco/mujoco-2.1.1/lib/libmujoco.so.2.1.1
 export MUJOCO_PY_MUJOCO_PATH=$root_dir/.mujoco/mujoco210
 export DISPLAY=unix:0.0
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/root/project/.mujoco/mujoco210/bin
-#MUJOCO_GL=glfw pytest --cov=torchrl --junitxml=test-results/junit.xml -v --durations 20
 
-pytest test/smoke_test.py -v --durations 20
-pytest test/smoke_test_deps.py -v --durations 20
-pytest -v --durations 20
+MUJOCO_GL=$PRIVATE_MUJOCO_GL pytest test/smoke_test.py -v --durations 20
+MUJOCO_GL=$PRIVATE_MUJOCO_GL pytest test/smoke_test_deps.py -v --durations 20
+MUJOCO_GL=$PRIVATE_MUJOCO_GL pytest -v --durations 20
