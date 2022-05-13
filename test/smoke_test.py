e56b55fe2aa8179a1bebd251fcbee10e3cdb50ae
Vincent Moens <vincentmoens@gmail.com>
2022-05-13 16:45:21 +0100

    amend

--- test/smoke_test.py
+++ test/smoke_test.py
@@ -1,5 +1,11 @@
-from torchrl.data import ReplayBuffer, PrioritizedReplayBuffer, TensorDict, TensorSpec
-from torchrl.envs import GymLikeEnv
-from torchrl.envs import TransformedEnv, Transform
-from torchrl.modules import TDModule
-from torchrl.objectives.costs.common import _LossModule
+def test_imports():
+    from torchrl.data import (
+        ReplayBuffer,
+        PrioritizedReplayBuffer,
+        TensorDict,
+        TensorSpec,
+    )
+    from torchrl.envs import GymLikeEnv
+    from torchrl.envs import TransformedEnv, Transform
+    from torchrl.modules import TDModule
+    from torchrl.objectives.costs.common import _LossModule
