1e39feba1a1bc1d893d5b640873d639ee8c3b280
Vincent Moens <vincentmoens@gmail.com>
2022-05-16 13:29:23 +0100

    init

--- examples/ddpg/ddpg.py
+++ examples/ddpg/ddpg.py
@@ -74,9 +74,8 @@ DEFAULT_REWARD_SCALING = {
     "humanoid": 100,
 }
 
-if __name__ == "__main__":
-    args = parser.parse_args()
 
+def main(args):
     args = correct_for_frame_skip(args)
 
     if not isinstance(args.reward_scaling, float):
@@ -151,6 +150,7 @@ if __name__ == "__main__":
     for t in recorder.transform:
         if isinstance(t, RewardScaling):
             t.scale.fill_(1.0)
+            t.loc.fill_(0.0)
 
     trainer = make_trainer(
         collector,
@@ -164,3 +164,8 @@ if __name__ == "__main__":
     )
 
     trainer.train()
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    main(args)
