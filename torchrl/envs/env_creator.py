90fe0751f38126bfea9c32ab366d4336d7ee2688
Vincent Moens <vincentmoens@gmail.com>
2022-05-17 17:51:16 +0100

    avoid deleting shadow env which carries state dict

--- torchrl/envs/env_creator.py
+++ torchrl/envs/env_creator.py
@@ -117,7 +117,6 @@ class EnvCreator:
             self.share_memory(self._transform_state_dict)
         self.initialized = True
         shadow_env.close()
-        del shadow_env
         return self
 
     def __call__(self, **kwargs) -> _EnvClass:
