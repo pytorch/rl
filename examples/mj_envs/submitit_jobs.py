f7fc613647b3639637b202531e6d3ebba7c56b5d
Vincent Moens <vincentmoens@gmail.com>
2022-06-07 12:15:40 +0100

    submitit

--- examples/mj_envs/submitit_jobs.py
+++ examples/mj_envs/submitit_jobs.py
@@ -16,6 +16,7 @@ jobs = []
 seeds = []
 seed_list = [1, 42, 1988]
 use_avg_pooling = [True, False]
+shared_mapping = [True, False]
 
 envs = []
 
@@ -26,19 +27,23 @@ envs = [k for k in gym.envs.registration.registry.env_specs.keys() if
         k.startswith("visual")]
 
 for env in envs:
-    for _use_avg_pooling in use_avg_pooling:
+    for _shared_mapping in shared_mapping:
         for _use_avg_pooling in use_avg_pooling:
             use_avg_pooling_str = ["avg_pooling"] if _use_avg_pooling else []
+            shared_mapping_str = ["shared_mapping"] if _shared_mapping else []
             for seed in seed_list:
                 flags = ["--config",
                          f"redq_configs_pixels/generic.txt",
                          "--env_name", env,
-                         "--exp_name", "".join(["SUBMITIT", env, "seed",
-                                                str(seed)] + use_avg_pooling_str),
+                         "--exp_name", "".join(
+                        ["SUBMITIT", env, "seed",
+                         str(seed)] + use_avg_pooling_str + shared_mapping_str),
                          "--collector_devices", "cuda:1", "cuda:2", "cuda:3",
                          "cuda:4", ]
-                if use_avg_pooling:
+                if _use_avg_pooling:
                     flags += ["--use_avg_pooling"]
+                if _shared_mapping:
+                    flags += ["--shared_mapping"]
                 config = parser_redq.parse_args(flags)
                 job = executor.submit(main_redq, config)
                 print("job id: ", job.job_id)  # ID of your job
