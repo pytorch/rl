062ade0bd483e9a576c7fcdd8c1b2d9247ddc106
Bhuvan Basireddy <bbreddy@devfair0832.h2.fair>
2022-06-08 18:48:01 -0700

    Added hydra dependencies to setup.py

--- setup.py
+++ setup.py
@@ -84,7 +84,7 @@ def _main():
             "gym_continuous": ["mujoco-py", "mujoco"],
             "rendering": ["moviepy"],
             "tests": ["pytest", "pyyaml"],
-            "utils": ["tqdm", "configargparse"],
+            "utils": ["tqdm", "configargparse", "hydra-core", "hydra-submitit-launcher"],
         },
     )
 
