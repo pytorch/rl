fb4bbafdeafed22d2a24c84a2802fc9a9999073d
Vincent Moens <vincentmoens@gmail.com>
2022-06-10 17:46:53 +0100

    random gpu allocation to dm_control (#192)

--- torchrl/envs/libs/dm_control.py
+++ torchrl/envs/libs/dm_control.py
@@ -4,6 +4,8 @@
 # LICENSE file in the root directory of this source tree.
 from __future__ import annotations
 
+import collections
+import os
 from typing import Optional, Tuple, Union
 
 import numpy as np
@@ -18,9 +20,10 @@ from torchrl.data import (
 from ...data.utils import numpy_to_torch_dtype_dict, DEVICE_TYPING
 from ..common import GymLikeEnv
 
-__all__ = ["DMControlEnv"]
-
-import collections
+if torch.has_cuda and torch.cuda.device_count() > 1:
+    n = torch.cuda.device_count() - 1
+    os.environ["EGL_DEVICE_ID"] = str(1 + (os.getpid() % n))
+    print("EGL_DEVICE_ID: ", os.environ["EGL_DEVICE_ID"])
 
 try:
 
@@ -32,6 +35,8 @@ try:
 
 except ImportError:
     _has_dmc = False
+
+__all__ = ["DMControlEnv"]
 
 
 def _dmcontrol_to_torchrl_spec_transform(
