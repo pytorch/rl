4673b4dbc0a5a55f64005f1ca88bf419cc03b120
Vincent Moens <vincentmoens@gmail.com>
2022-06-22 17:13:14 +0100

    init

--- test/test_collector.py
+++ test/test_collector.py
@@ -4,6 +4,7 @@
 # LICENSE file in the root directory of this source tree.
 
 import argparse
+import time
 
 import numpy as np
 import pytest
@@ -107,7 +108,6 @@ def test_concurrent_collector_consistency(num_env, env_name, seed=40):
             break
     with pytest.raises(AssertionError):
         assert_allclose_td(b1, b2)
-    # del collector
     # collector.shutdown()
 
     ccollector = aSyncDataCollector(
@@ -132,8 +132,7 @@ def test_concurrent_collector_consistency(num_env, env_name, seed=40):
     assert_allclose_td(b1c, b1)
     assert_allclose_td(b2c, b2)
 
-    # del ccollector
-    ccollector.shutdown()
+    # ccollector.shutdown()
 
 
 @pytest.mark.parametrize("num_env", [1, 3])
@@ -172,7 +171,7 @@ def test_collector_batch_size(num_env, env_name, seed=100):
         assert b.numel() == -(-frames_per_batch // num_env) * num_env
         if i == 5:
             break
-    del ccollector
+    ccollector.shutdown()
 
     ccollector = MultiSyncDataCollector(
         create_env_fn=[env_fn for _ in range(num_workers)],
@@ -190,7 +189,7 @@ def test_collector_batch_size(num_env, env_name, seed=100):
         )
         if i == 5:
             break
-    del ccollector
+    ccollector.shutdown()
 
 
 @pytest.mark.parametrize("num_env", [1, 3])
@@ -237,7 +236,7 @@ def test_concurrent_collector_seed(num_env, env_name, seed=100):
     assert_allclose_td(b1, b2)
     with pytest.raises(AssertionError):
         assert_allclose_td(b1, b3)
-    del ccollector
+    ccollector.shutdown()
 
 
 @pytest.mark.parametrize("num_env", [3, 1])
@@ -354,6 +353,7 @@ def test_traj_len_consistency(num_env, env_name, collector_class, seed=100):
     data1 = torch.cat(data1, 1)
     data1 = data1[:, :max_frames_per_traj]
 
+    collector1.shutdown()
     del collector1
 
     collector10 = collector_class(
@@ -378,6 +378,7 @@ def test_traj_len_consistency(num_env, env_name, collector_class, seed=100):
     data10 = torch.cat(data10, 1)
     data10 = data10[:, :max_frames_per_traj]
 
+    collector10.shutdown()
     del collector10
 
     collector20 = collector_class(
@@ -399,6 +400,7 @@ def test_traj_len_consistency(num_env, env_name, collector_class, seed=100):
         if count > max_frames_per_traj:
             break
 
+    collector20.shutdown()
     del collector20
     data20 = torch.cat(data20, 1)
     data20 = data20[:, :max_frames_per_traj]
@@ -514,6 +516,7 @@ def test_update_weights(use_async):
                 policy_state_dict[k].cpu(),
             )
 
+    collector.shutdown()
     del collector
 
 
@@ -551,7 +554,7 @@ def test_excluded_keys(collector_class, exclude):
         else:
             assert any(key.startswith("_") for key in keys)
         break
-    del collector
+    collector.shutdown()
     dummy_env.close()
 
 
