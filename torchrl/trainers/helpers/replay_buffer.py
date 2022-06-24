c627180142bf1ff6020bc3ba3d29a016aec2cbfd
Vincent Moens <vincentmoens@gmail.com>
2022-06-24 14:02:38 +0100

    amend

--- torchrl/trainers/helpers/replay_buffer.py
+++ torchrl/trainers/helpers/replay_buffer.py
@@ -2,7 +2,6 @@
 #
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
-import os
 from dataclasses import dataclass
 
 import torch
@@ -30,8 +29,7 @@ def make_replay_buffer(device: DEVICE_TYPING, cfg: "DictConfig") -> ReplayBuffer
             prefetch=3,
             storage=LazyMemmapStorage(
                 cfg.buffer_size,
-                # SCRATCH_DIR may depend upon job id etc, hence we
-                scratch_dir=os.environ.get("SCRATCH_DIR", None),
+                scratch_dir=cfg.buffer_scratch_dir,
             ),
         )
     else:
@@ -44,7 +42,7 @@ def make_replay_buffer(device: DEVICE_TYPING, cfg: "DictConfig") -> ReplayBuffer
             prefetch=3,
             storage=LazyMemmapStorage(
                 cfg.buffer_size,
-                scratch_dir=os.environ.get("SCRATCH_DIR", None),
+                scratch_dir=cfg.buffer_scratch_dir,
             ),
         )
     return buffer
@@ -56,3 +54,5 @@ class ReplayArgsConfig:
     # buffer size, in number of frames stored. Default=1e6
     prb: bool = False
     # whether a Prioritized replay buffer should be used instead of a more basic circular one.
+    buffer_scratch_dir: str = None
+    # directory where the buffer data should be stored
