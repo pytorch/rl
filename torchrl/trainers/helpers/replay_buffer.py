38dc28615489e0b1f64a147b385edf6d70cfdf3f
Vincent Moens <vincentmoens@gmail.com>
2022-06-24 22:27:30 +0100

    amend

--- torchrl/trainers/helpers/replay_buffer.py
+++ torchrl/trainers/helpers/replay_buffer.py
@@ -19,15 +19,19 @@ __all__ = ["make_replay_buffer"]
 from torchrl.data.replay_buffers.storages import LazyMemmapStorage
 
 
+def collate_fn(x):
+    return x.clone()
+
+
 def make_replay_buffer(device: DEVICE_TYPING, cfg: "DictConfig") -> ReplayBuffer:
     """Builds a replay buffer using the config built from ReplayArgsConfig."""
     device = torch.device(device)
     if not cfg.prb:
         buffer = TensorDictReplayBuffer(
             cfg.buffer_size,
-            collate_fn=lambda x: x,
+            collate_fn=collate_fn,
             pin_memory=device != torch.device("cpu"),
-            prefetch=3,
+            prefetch=args.buffer_prefetch,
             storage=LazyMemmapStorage(
                 cfg.buffer_size,
                 scratch_dir=cfg.buffer_scratch_dir,
@@ -38,9 +42,9 @@ def make_replay_buffer(device: DEVICE_TYPING, cfg: "DictConfig") -> ReplayBuffer
             cfg.buffer_size,
             alpha=0.7,
             beta=0.5,
-            collate_fn=lambda x: x,
+            collate_fn=collate_fn,
             pin_memory=device != torch.device("cpu"),
-            prefetch=3,
+            prefetch=args.buffer_prefetch,
             storage=LazyMemmapStorage(
                 cfg.buffer_size,
                 scratch_dir=cfg.buffer_scratch_dir,
@@ -57,3 +61,5 @@ class ReplayArgsConfig:
     # whether a Prioritized replay buffer should be used instead of a more basic circular one.
     buffer_scratch_dir: Optional[str] = None
     # directory where the buffer data should be stored. If none is passed, they will be placed in /tmp/
+    buffer_prefetch: int = 10
+    # prefetching queue length for the replay buffer
