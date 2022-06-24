336b9818c4677af603b6f525cccc1cb117f62f6e
Vincent Moens <vincentmoens@gmail.com>
2022-06-24 08:40:06 +0100

    Hydra integration (#202)

--- torchrl/trainers/helpers/collectors.py
+++ torchrl/trainers/helpers/collectors.py
@@ -3,8 +3,8 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-from argparse import ArgumentParser, Namespace
-from typing import Callable, List, Optional, Type, Union, Dict
+from dataclasses import dataclass, field
+from typing import Callable, List, Optional, Type, Union, Dict, Any
 
 from torchrl.collectors.collectors import (
     _DataCollector,
@@ -21,8 +21,6 @@ __all__ = [
     "sync_async_collector",
     "make_collector_offpolicy",
     "make_collector_onpolicy",
-    "parser_collector_args_offpolicy",
-    "parser_collector_args_onpolicy",
 ]
 
 from torchrl.envs.common import _EnvClass
@@ -241,7 +239,7 @@ def _make_collector(
 def make_collector_offpolicy(
     make_env: Callable[[], _EnvClass],
     actor_model_explore: Union[TensorDictModuleWrapper, ProbabilisticTensorDictModule],
-    args: Namespace,
+    cfg: "DictConfig",
     make_env_kwargs: Optional[Dict] = None,
 ) -> _DataCollector:
     """
@@ -250,19 +248,19 @@ def make_collector_offpolicy(
     Args:
         make_env (Callable): environment creator
         actor_model_explore (TensorDictModule): Model instance used for evaluation and exploration update
-        args (Namespace): argument namespace built from the parser constructor
+        cfg (DictConfig): config for creating collector object
         make_env_kwargs (dict): kwargs for the env creator
 
     """
-    if args.async_collection:
+    if cfg.async_collection:
         collector_helper = sync_async_collector
     else:
         collector_helper = sync_sync_collector
 
-    if args.multi_step:
+    if cfg.multi_step:
         ms = MultiStep(
-            gamma=args.gamma,
-            n_steps_max=args.n_steps_return,
+            gamma=cfg.gamma,
+            n_steps_max=cfg.n_steps_return,
         )
     else:
         ms = None
@@ -272,41 +270,41 @@ def make_collector_offpolicy(
         env_kwargs.update(make_env_kwargs)
     elif make_env_kwargs is not None:
         env_kwargs = make_env_kwargs
-    args.collector_devices = (
-        args.collector_devices
-        if len(args.collector_devices) > 1
-        else args.collector_devices[0]
+    cfg.collector_devices = (
+        cfg.collector_devices
+        if len(cfg.collector_devices) > 1
+        else cfg.collector_devices[0]
     )
     collector_helper_kwargs = {
         "env_fns": make_env,
         "env_kwargs": env_kwargs,
         "policy": actor_model_explore,
-        "max_frames_per_traj": args.max_frames_per_traj,
-        "frames_per_batch": args.frames_per_batch,
-        "total_frames": args.total_frames,
+        "max_frames_per_traj": cfg.max_frames_per_traj,
+        "frames_per_batch": cfg.frames_per_batch,
+        "total_frames": cfg.total_frames,
         "postproc": ms,
         "num_env_per_collector": 1,
         # we already took care of building the make_parallel_env function
-        "num_collectors": -args.num_workers // -args.env_per_collector,
-        "devices": args.collector_devices,
-        "passing_devices": args.collector_devices,
-        "init_random_frames": args.init_random_frames,
-        "pin_memory": args.pin_memory,
+        "num_collectors": -cfg.num_workers // -cfg.env_per_collector,
+        "devices": cfg.collector_devices,
+        "passing_devices": cfg.collector_devices,
+        "init_random_frames": cfg.init_random_frames,
+        "pin_memory": cfg.pin_memory,
         "split_trajs": ms is not None,
         # trajectories must be separated if multi-step is used
-        "init_with_lag": args.init_with_lag,
-        "exploration_mode": args.exploration_mode,
+        "init_with_lag": cfg.init_with_lag,
+        "exploration_mode": cfg.exploration_mode,
     }
 
     collector = collector_helper(**collector_helper_kwargs)
-    collector.set_seed(args.seed)
+    collector.set_seed(cfg.seed)
     return collector
 
 
 def make_collector_onpolicy(
     make_env: Callable[[], _EnvClass],
     actor_model_explore: Union[TensorDictModuleWrapper, ProbabilisticTensorDictModule],
-    args: Namespace,
+    cfg: "DictConfig",
     make_env_kwargs: Optional[Dict] = None,
 ) -> _DataCollector:
     collector_helper = sync_sync_collector
@@ -318,166 +316,83 @@ def make_collector_onpolicy(
         env_kwargs.update(make_env_kwargs)
     elif make_env_kwargs is not None:
         env_kwargs = make_env_kwargs
-    args.collector_devices = (
-        args.collector_devices
-        if len(args.collector_devices) > 1
-        else args.collector_devices[0]
+    cfg.collector_devices = (
+        cfg.collector_devices
+        if len(cfg.collector_devices) > 1
+        else cfg.collector_devices[0]
     )
     collector_helper_kwargs = {
         "env_fns": make_env,
         "env_kwargs": env_kwargs,
         "policy": actor_model_explore,
-        "max_frames_per_traj": args.max_frames_per_traj,
-        "frames_per_batch": args.frames_per_batch,
-        "total_frames": args.total_frames,
+        "max_frames_per_traj": cfg.max_frames_per_traj,
+        "frames_per_batch": cfg.frames_per_batch,
+        "total_frames": cfg.total_frames,
         "postproc": ms,
         "num_env_per_collector": 1,
         # we already took care of building the make_parallel_env function
-        "num_collectors": -args.num_workers // -args.env_per_collector,
-        "devices": args.collector_devices,
-        "passing_devices": args.collector_devices,
-        "pin_memory": args.pin_memory,
+        "num_collectors": -cfg.num_workers // -cfg.env_per_collector,
+        "devices": cfg.collector_devices,
+        "passing_devices": cfg.collector_devices,
+        "pin_memory": cfg.pin_memory,
         "split_trajs": True,
         # trajectories must be separated in online settings
-        "init_with_lag": args.init_with_lag,
-        "exploration_mode": args.exploration_mode,
+        "init_with_lag": cfg.init_with_lag,
+        "exploration_mode": cfg.exploration_mode,
     }
 
     collector = collector_helper(**collector_helper_kwargs)
-    collector.set_seed(args.seed)
+    collector.set_seed(cfg.seed)
     return collector
 
 
-def _parser_collector_args(parser: ArgumentParser) -> ArgumentParser:
-    parser.add_argument(
-        "--collector_devices",
-        "--collector-devices",
-        nargs="+",
-        default=["cpu"],
-        help="device on which the data collector should store the trajectories to be passed to this script."
-        "If the collector device differs from the policy device (cuda:0 if available), then the "
-        "weights of the collector policy are synchronized with collector.update_policy_weights_().",
-    )
-    parser.add_argument(
-        "--pin_memory",
-        "--pin-memory",
-        action="store_true",
-        help="if True, the data collector will call pin_memory before dispatching tensordicts onto the "
-        "passing device.",
-    )
-    parser.add_argument(
-        "--init_with_lag",
-        "--init-with-lag",
-        action="store_true",
-        help="if True, the first trajectory will be truncated earlier at a random step. This is helpful"
-        " to desynchronize the environments, such that steps do no match in all collected "
-        "rollouts. Especially useful for online training, to prevent cyclic sample indices.",
-    )
-    parser.add_argument(
-        "--frames_per_batch",
-        "--frames-per-batch",
-        type=int,
-        default=1000,
-        help="number of steps executed in the environment per collection."
-        "This value represents how many steps will the data collector execute and return in *each*"
-        "environment that has been created in between two rounds of optimization "
-        "(see the optim_steps_per_batch above). "
-        "On the one hand, a low value will enhance the data throughput between processes in async "
-        "settings, which can make the accessing of data a computational bottleneck. "
-        "High values will on the other hand lead to greater tensor sizes in memory and disk to be "
-        "written and read at each global iteration. One should look at the number of frames per second"
-        "in the log to assess the efficiency of the configuration.",
-    )
-    parser.add_argument(
-        "--total_frames",
-        "--total-frames",
-        type=int,
-        default=50000000,
-        help="total number of frames collected for training. Does account for frame_skip (i.e. will be "
-        "divided by the frame_skip). Default=50e6.",
-    )
-    parser.add_argument(
-        "--num_workers",
-        "--num-workers",
-        type=int,
-        default=32,
-        help="Number of workers used for data collection. ",
-    )
-    parser.add_argument(
-        "--env_per_collector",
-        "--env-per-collector",
-        default=8,
-        type=int,
-        help="Number of environments per collector. If the env_per_collector is in the range: "
-        "1<env_per_collector<=num_workers, then the collector runs"
-        "ceil(num_workers/env_per_collector) in parallel and executes the policy steps synchronously "
-        "for each of these parallel wrappers. If env_per_collector=num_workers, no parallel wrapper is created.",
-    )
-    parser.add_argument(
-        "--seed",
-        type=int,
-        default=42,
-        help="seed used for the environment, pytorch and numpy.",
-    )
-    parser.add_argument(
-        "--exploration_mode",
-        "--exploration-mode",
-        type=str,
-        default=None,
-        help="exploration mode of the data collector.",
-    )
-    parser.add_argument(
-        "--async_collection",
-        "--async-collection",
-        action="store_true",
-        help="whether data collection should be done asynchrously. Asynchrounous data collection means "
-        "that the data collector will keep on running the environment with the previous weights "
-        "configuration while the optimization loop is being done. If the algorithm is trained "
-        "synchronously, data collection and optimization will occur iteratively, not concurrently.",
-    )
-    return parser
+@dataclass
+class OnPolicyCollectorConfig:
+    collector_devices: Any = field(default_factory=lambda: ["cpu"])
+    # device on which the data collector should store the trajectories to be passed to this script.
+    # If the collector device differs from the policy device (cuda:0 if available), then the
+    # weights of the collector policy are synchronized with collector.update_policy_weights_().
+    pin_memory: bool = False
+    # if True, the data collector will call pin_memory before dispatching tensordicts onto the passing device
+    init_with_lag: bool = False
+    # if True, the first trajectory will be truncated earlier at a random step. This is helpful
+    # to desynchronize the environments, such that steps do no match in all collected
+    # rollouts. Especially useful for online training, to prevent cyclic sample indices.
+    frames_per_batch: int = 1000
+    # number of steps executed in the environment per collection.
+    # This value represents how many steps will the data collector execute and return in *each*
+    # environment that has been created in between two rounds of optimization
+    # (see the optim_steps_per_batch above).
+    # On the one hand, a low value will enhance the data throughput between processes in async
+    # settings, which can make the accessing of data a computational bottleneck.
+    # High values will on the other hand lead to greater tensor sizes in memory and disk to be
+    # written and read at each global iteration. One should look at the number of frames per second
+    # in the log to assess the efficiency of the configuration.
+    total_frames: int = 50000000
+    # total number of frames collected for training. Does account for frame_skip (i.e. will be
+    # divided by the frame_skip). Default=50e6.
+    num_workers: int = 32
+    # Number of workers used for data collection.
+    env_per_collector: int = 8
+    # Number of environments per collector. If the env_per_collector is in the range:
+    # 1<env_per_collector<=num_workers, then the collector runs
+    # ceil(num_workers/env_per_collector) in parallel and executes the policy steps synchronously
+    # for each of these parallel wrappers. If env_per_collector=num_workers, no parallel wrapper is created
+    seed: int = 42
+    # seed used for the environment, pytorch and numpy.
+    exploration_mode: str = ""
+    # exploration mode of the data collector.
+    async_collection: bool = False
+    # whether data collection should be done asynchrously. Asynchrounous data collection means
+    # that the data collector will keep on running the environment with the previous weights
+    # configuration while the optimization loop is being done. If the algorithm is trained
+    # synchronously, data collection and optimization will occur iteratively, not concurrently.
 
 
-def parser_collector_args_offpolicy(parser: ArgumentParser) -> ArgumentParser:
-    """
-    Populates the argument parser to build a data collector for on-policy algorithms (DQN, DDPG, SAC, REDQ).
-
-    Args:
-        parser (ArgumentParser): parser to be populated.
-
-    """
-    parser = _parser_collector_args(parser)
-    parser.add_argument(
-        "--multi_step",
-        "--multi-step",
-        dest="multi_step",
-        action="store_true",
-        help="whether or not multi-step rewards should be used.",
-    )
-    parser.add_argument(
-        "--n_steps_return",
-        "--n-steps-return",
-        type=int,
-        default=3,
-        help="If multi_step is set to True, this value defines the number of steps to look ahead for the "
-        "reward computation.",
-    )
-    parser.add_argument(
-        "--init_random_frames",
-        "--init-random-frames",
-        type=int,
-        default=50000,
-        help="Initial number of random frames used before the policy is being used. Default=5000.",
-    )
-    return parser
-
-
-def parser_collector_args_onpolicy(parser: ArgumentParser) -> ArgumentParser:
-    """
-    Populates the argument parser to build a data collector for on-policy algorithms (PPO).
-
-    Args:
-        parser (ArgumentParser): parser to be populated.
-    """
-    parser = _parser_collector_args(parser)
-    return parser
+@dataclass
+class OffPolicyCollectorConfig(OnPolicyCollectorConfig):
+    multi_step: bool = False
+    # whether or not multi-step rewards should be used.
+    n_steps_return: int = 3
+    # If multi_step is set to True, this value defines the number of steps to look ahead for the reward computation.
+    init_random_frames: int = 50000
