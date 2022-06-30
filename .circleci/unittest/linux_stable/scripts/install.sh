5ff4f80e00862ed9bd8eb0846b2bee33d0238efd
Vincent Moens <vincentmoens@gmail.com>
2022-06-30 17:25:17 +0100

    amend

--- .circleci/unittest/linux_stable/scripts/install.sh
+++ .circleci/unittest/linux_stable/scripts/install.sh
@@ -23,7 +23,7 @@ else
     echo "Using CUDA $CUDA_VERSION as determined by CU_VERSION ($CU_VERSION)"
     version="$(python -c "print('.'.join(\"${CUDA_VERSION}\".split('.')[:2]))")"
     cudatoolkit="cudatoolkit=${version}"
-    export CUDA=/usr/local/cuda-11.4/bin/nvcc
+    export PATH=$PATH:/usr/local/cuda-11.4/bin/nvcc
 fi
 
 case "$(uname -s)" in
