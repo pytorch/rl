0e01b69b8e0c00a73786dc51f4c328f0e87d1882
Vincent Moens <vincentmoens@gmail.com>
2022-07-04 14:52:19 +0100

    auditwheel

--- packaging/build_wheels.sh
+++ packaging/build_wheels.sh
@@ -36,7 +36,7 @@ if [[ "$OSTYPE" == "msys" ]]; then
   echo "ERROR: Windows installation is not supported yet." && exit 100
 else
     python setup.py bdist_wheel
-    auditwheel repair --plat manylinux_2_24_x86_64 dist/torchrl-*.whl
+    python -m auditwheel repair --plat manylinux_2_24_x86_64 dist/torchrl-*.whl
     ls -rtlh wheelhouse
 fi
 
