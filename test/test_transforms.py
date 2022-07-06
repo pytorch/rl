41a6f8857348da0d1f3ac0f5db8d971eea481921
Vincent Moens <vincentmoens@gmail.com>
2022-07-06 09:14:31 +0100

    removing locks

--- test/test_transforms.py
+++ test/test_transforms.py
@@ -91,30 +91,27 @@ def test_vecnorm_parallel(nprc):
         td = VecNorm.build_td_for_shared_vecnorm(
             ContinuousActionVecMockEnv(),
         )
-    with Lock() as lock:
-        for idx in range(nprc):
-            prc_queue_in = mp.Queue(1)
-            prc_queue_out = mp.Queue(1)
-            p = mp.Process(
-                target=_test_vecnorm_subproc,
-                args=(
-                    idx,
-                    prc_queue_in,
-                    prc_queue_out,
-                ),
-            )
-            p.start()
-            prc_queue_out.put(td)
-            prcs.append(p)
-            queues.append((prc_queue_in, prc_queue_out))
+    for idx in range(nprc):
+        prc_queue_in = mp.Queue(1)
+        prc_queue_out = mp.Queue(1)
+        p = mp.Process(
+            target=_test_vecnorm_subproc,
+            args=(
+                idx,
+                prc_queue_in,
+                prc_queue_out,
+            ),
+        )
+        p.start()
+        prc_queue_out.put(td)
+        prcs.append(p)
+        queues.append((prc_queue_in, prc_queue_out))
 
-    with Lock() as lock:
-        dones = [queue[0].get(timeout=TIMEOUT) for queue in queues]
-        assert all(dones)
-        msg = "all_done"
-    with Lock() as lock:
-        for idx in range(nprc):
-            queues[idx][1].put(msg)
+    dones = [queue[0].get(timeout=TIMEOUT) for queue in queues]
+    assert all(dones)
+    msg = "all_done"
+    for idx in range(nprc):
+        queues[idx][1].put(msg)
 
     obs_sum = td.get("next_observation_sum").clone()
     obs_ssq = td.get("next_observation_ssq").clone()
@@ -123,35 +120,33 @@ def test_vecnorm_parallel(nprc):
     reward_ssq = td.get("reward_ssq").clone()
     reward_count = td.get("reward_ssq").clone()
 
-    with Lock() as lock:
-        for idx in range(nprc):
-            td_out = queues[idx][0].get(timeout=TIMEOUT)
-            _obs_sum = td_out.get("obs_sum")
-            _obs_ssq = td_out.get("obs_ssq")
-            _obs_count = td_out.get("obs_count")
-            _reward_sum = td_out.get("reward_sum")
-            _reward_ssq = td_out.get("reward_ssq")
-            _reward_count = td_out.get("reward_count")
-            assert (obs_sum == _obs_sum).all()
-            assert (obs_ssq == _obs_ssq).all()
-            assert (obs_count == _obs_count).all()
-            assert (reward_sum == _reward_sum).all()
-            assert (reward_ssq == _reward_ssq).all()
-            assert (reward_count == _reward_count).all()
+    for idx in range(nprc):
+        td_out = queues[idx][0].get(timeout=TIMEOUT)
+        _obs_sum = td_out.get("obs_sum")
+        _obs_ssq = td_out.get("obs_ssq")
+        _obs_count = td_out.get("obs_count")
+        _reward_sum = td_out.get("reward_sum")
+        _reward_ssq = td_out.get("reward_ssq")
+        _reward_count = td_out.get("reward_count")
+        assert (obs_sum == _obs_sum).all()
+        assert (obs_ssq == _obs_ssq).all()
+        assert (obs_count == _obs_count).all()
+        assert (reward_sum == _reward_sum).all()
+        assert (reward_ssq == _reward_ssq).all()
+        assert (reward_count == _reward_count).all()
 
-            obs_sum, obs_ssq, obs_count, reward_sum, reward_ssq, reward_count = (
-                _obs_sum,
-                _obs_ssq,
-                _obs_count,
-                _reward_sum,
-                _reward_ssq,
-                _reward_count,
-            )
+        obs_sum, obs_ssq, obs_count, reward_sum, reward_ssq, reward_count = (
+            _obs_sum,
+            _obs_ssq,
+            _obs_count,
+            _reward_sum,
+            _reward_ssq,
+            _reward_count,
+        )
 
-    with Lock() as lock:
-        msg = "all_done"
-        for idx in range(nprc):
-            queues[idx][1].put(msg)
+    msg = "all_done"
+    for idx in range(nprc):
+        queues[idx][1].put(msg)
     del queues
 
 
@@ -195,71 +190,67 @@ def test_vecnorm_parallel_auto(nprc):
             lambda: TransformedEnv(ContinuousActionVecMockEnv(), VecNorm(decay=1.0))
         )
 
-    with Lock() as lock:
-        for idx in range(nprc):
-            prc_queue_in = mp.Queue(1)
-            prc_queue_out = mp.Queue(1)
-            p = mp.Process(
-                target=_test_vecnorm_subproc_auto,
-                args=(
-                    idx,
-                    make_env,
-                    prc_queue_in,
-                    prc_queue_out,
-                ),
-            )
-            p.start()
-            prcs.append(p)
-            queues.append((prc_queue_in, prc_queue_out))
+    for idx in range(nprc):
+        prc_queue_in = mp.Queue(1)
+        prc_queue_out = mp.Queue(1)
+        p = mp.Process(
+            target=_test_vecnorm_subproc_auto,
+            args=(
+                idx,
+                make_env,
+                prc_queue_in,
+                prc_queue_out,
+            ),
+        )
+        p.start()
+        prcs.append(p)
+        queues.append((prc_queue_in, prc_queue_out))
 
-        td = list(make_env.state_dict().values())[0]
-        dones = [queue[0].get() for queue in queues]
-        assert all(dones)
-        msg = "all_done"
-    with Lock() as lock:
-        for idx in range(nprc):
-            queues[idx][1].put(msg)
+    td = list(make_env.state_dict().values())[0]
+    dones = [queue[0].get() for queue in queues]
+    assert all(dones)
+    msg = "all_done"
+    for idx in range(nprc):
+        queues[idx][1].put(msg)
 
-        # sleep(0.01)
-        obs_sum = td.get("next_observation_sum").clone()
-        obs_ssq = td.get("next_observation_ssq").clone()
-        obs_count = td.get("next_observation_count").clone()
-        reward_sum = td.get("reward_sum").clone()
-        reward_ssq = td.get("reward_ssq").clone()
-        reward_count = td.get("reward_count").clone()
+    # sleep(0.01)
+    obs_sum = td.get("next_observation_sum").clone()
+    obs_ssq = td.get("next_observation_ssq").clone()
+    obs_count = td.get("next_observation_count").clone()
+    reward_sum = td.get("reward_sum").clone()
+    reward_ssq = td.get("reward_ssq").clone()
+    reward_count = td.get("reward_count").clone()
 
     assert obs_count == nprc * 11 + 2  # 10 steps + reset + init
 
-    with Lock() as lock:
-        for idx in range(nprc):
-            tup = queues[idx][0].get(timeout=TIMEOUT)
-            (
-                _obs_sum,
-                _obs_ssq,
-                _obs_count,
-                _reward_sum,
-                _reward_ssq,
-                _reward_count,
-            ) = tup
-            assert (obs_sum == _obs_sum).all(), "sum"
-            assert (obs_ssq == _obs_ssq).all(), "ssq"
-            assert (obs_count == _obs_count).all(), "count"
-            assert (reward_sum == _reward_sum).all(), "sum"
-            assert (reward_ssq == _reward_ssq).all(), "ssq"
-            assert (reward_count == _reward_count).all(), "count"
+    for idx in range(nprc):
+        tup = queues[idx][0].get(timeout=TIMEOUT)
+        (
+            _obs_sum,
+            _obs_ssq,
+            _obs_count,
+            _reward_sum,
+            _reward_ssq,
+            _reward_count,
+        ) = tup
+        assert (obs_sum == _obs_sum).all(), "sum"
+        assert (obs_ssq == _obs_ssq).all(), "ssq"
+        assert (obs_count == _obs_count).all(), "count"
+        assert (reward_sum == _reward_sum).all(), "sum"
+        assert (reward_ssq == _reward_ssq).all(), "ssq"
+        assert (reward_count == _reward_count).all(), "count"
 
-            obs_sum, obs_ssq, obs_count, reward_sum, reward_ssq, reward_count = (
-                _obs_sum,
-                _obs_ssq,
-                _obs_count,
-                _reward_sum,
-                _reward_ssq,
-                _reward_count,
-            )
-    with Lock() as lock:
-        msg = "all_done"
-        for idx in range(nprc):
-            queues[idx][1].put(msg)
+        obs_sum, obs_ssq, obs_count, reward_sum, reward_ssq, reward_count = (
+            _obs_sum,
+            _obs_ssq,
+            _obs_count,
+            _reward_sum,
+            _reward_ssq,
+            _reward_count,
+        )
+    msg = "all_done"
+    for idx in range(nprc):
+        queues[idx][1].put(msg)
     del queues
 
 
@@ -290,34 +281,33 @@ def test_parallelenv_vecnorm():
             lambda: TransformedEnv(ContinuousActionVecMockEnv(), VecNorm())
         )
         env_input_keys = ["action", ContinuousActionVecMockEnv._out_key]
-    with Lock() as lock:
-        parallel_env = ParallelEnv(3, make_env, env_input_keys=env_input_keys)
-        queue_out = mp.Queue(1)
-        queue_in = mp.Queue(1)
-        proc = mp.Process(
-            target=_run_parallelenv, args=(parallel_env, queue_out, queue_in)
-        )
-        proc.start()
-        parallel_sd = parallel_env.state_dict()
-        assert "worker0" in parallel_sd
-        worker_sd = parallel_sd["worker0"]
-        td = list(worker_sd.values())[0]
-        queue_out.put("start")
-        msg = queue_in.get(timeout=TIMEOUT)
-        assert msg == "first round"
-        values = td.clone()
-        queue_out.put("start")
-        msg = queue_in.get(timeout=TIMEOUT)
-        assert msg == "second round"
-        new_values = td.clone()
-        for k, item in values.items():
-            if k in ["reward_sum", "reward_ssq"] and not _has_gym:
-                # mocking env rewards are sparse
-                continue
-            assert (item != new_values.get(k)).any(), k
-        proc.join()
-        if not parallel_env.is_closed:
-            parallel_env.close()
+    parallel_env = ParallelEnv(3, make_env, env_input_keys=env_input_keys)
+    queue_out = mp.Queue(1)
+    queue_in = mp.Queue(1)
+    proc = mp.Process(
+        target=_run_parallelenv, args=(parallel_env, queue_out, queue_in)
+    )
+    proc.start()
+    parallel_sd = parallel_env.state_dict()
+    assert "worker0" in parallel_sd
+    worker_sd = parallel_sd["worker0"]
+    td = list(worker_sd.values())[0]
+    queue_out.put("start")
+    msg = queue_in.get(timeout=TIMEOUT)
+    assert msg == "first round"
+    values = td.clone()
+    queue_out.put("start")
+    msg = queue_in.get(timeout=TIMEOUT)
+    assert msg == "second round"
+    new_values = td.clone()
+    for k, item in values.items():
+        if k in ["reward_sum", "reward_ssq"] and not _has_gym:
+            # mocking env rewards are sparse
+            continue
+        assert (item != new_values.get(k)).any(), k
+    proc.join()
+    if not parallel_env.is_closed:
+        parallel_env.close()
 
 
 @pytest.mark.skipif(not _has_gym, reason="no gym library found")
