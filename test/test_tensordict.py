d75c854a03f86c319f540f738e3e8d668d6a4028
Vincent Moens <vincentmoens@gmail.com>
2022-07-11 12:25:25 +0100

    fix merge from main

--- test/test_tensordict.py
+++ test/test_tensordict.py
@@ -1130,8 +1130,7 @@ class TestTensorDicts:
         td = getattr(self, td_name)
         tdin = TensorDict({"inner": torch.randn(*td.shape, 1)}, [])
         td["inner_td"] = tdin
-        # tdin.batch_size = td.batch_size
-        print(td["inner_td"].to_tensordict(), tdin)
+        tdin.batch_size = td.batch_size
         assert (td["inner_td"] == tdin).all()
 
     def test_nested_td(self, td_name):
