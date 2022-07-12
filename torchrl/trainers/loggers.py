1c19d28d3b3021173f6cdedbd4897617dfdf58e7
Nicolas Dufour <ndufour@fb.com>
2022-07-12 18:07:53 +0100

    Added doc to loggers

--- torchrl/trainers/loggers.py
+++ torchrl/trainers/loggers.py
@@ -1,5 +1,11 @@
 from torch import Tensor
+
+
 class Logger:
+    """
+        A template for loggers
+
+    """
     def __init__(self, exp_name: str):
         self.exp_name = exp_name
         self.experiment = self._create_experiment()
@@ -15,6 +21,12 @@ class Logger:
         return self.experiment.__repr__()
 
 class TensorboardLogger(Logger):
+    """
+    Wrapper for the Tensoarboard logger.
+
+    exp_name: str. The name given to the experiment.
+    Will also be used as the log_dir
+    """
     def __init__(self, exp_name: str):
         super().__init__(exp_name=exp_name)
         self.log_dir = self.experiment.log_dir
@@ -22,16 +34,33 @@ class TensorboardLogger(Logger):
         self._has_imported_moviepy = False
         
     def _create_experiment(self):
+        """
+        Creates the SummaryWriter items
+        """
         try:
             from torch.utils.tensorboard import SummaryWriter
-        except:
+        except ImportError:
             raise ImportError("torch.utils.tensorboard could not be imported")
         return SummaryWriter(log_dir = self.exp_name)
 
     def log_scalar(self, name: str, value: float, step: int=None):
+        """
+        Function to log scalar inputs
+
+        name: str. The name of the logged variable.
+        value: float. The value to be logged.
+        step: int. Optionnal. Default is None. Allows to specify the step at which the data is logged
+        """
         self.experiment.add_scalar(name, value, global_step=step)
     
     def log_video(self, name: str, video: Tensor, step: int=None, **kwargs):
+        """
+        Function to log videos inputs
+
+        name: str. The name of the logged video.
+        video: Tensor. The video to be logged.
+        step: int. Optionnal. Default is None. Allows to specify the step at which the data is logged
+        """
         if not self._has_imported_moviepy:
             try:
                 import moviepy  # noqa
@@ -45,5 +74,9 @@ class TensorboardLogger(Logger):
             **kwargs,
         )
     def log_hparams(self, cfg: "DictConfig"):
+        """
+        Function to log hparams.
+        cfg: DictConfig. Input is given as a DictConfig.
+        """
         txt = "\n\t".join([f"{k}: {val}" for k, val in sorted(vars(cfg).items())])
         self.experiment.add_text("hparams", txt)
