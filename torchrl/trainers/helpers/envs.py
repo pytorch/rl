aa0528432f939de3f7e1c853b5cdb86a49242b7e
Nicolas Dufour <ndufour@fb.com>
2022-07-13 10:28:00 +0100

    Removed unused dependencies and formating

--- torchrl/trainers/helpers/envs.py
+++ torchrl/trainers/helpers/envs.py
@@ -3,7 +3,6 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-from asyncio.log import logger as asyncio_logger
 from dataclasses import dataclass
 from dataclasses import field as dataclass_field
 from typing import Callable, Optional, Union, Any
