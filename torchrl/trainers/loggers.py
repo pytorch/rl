b36006d8b2d4698c680b6ed7cf9640dc77a3abb7
Nicolas Dufour <ndufour@fb.com>
2022-07-14 13:43:31 +0100

    Added wandblogger

--- torchrl/trainers/loggers.py
+++ torchrl/trainers/loggers.py
@@ -4,6 +4,7 @@
 # LICENSE file in the root directory of this source tree.
 
 import abc
+import os
 
 from torch import Tensor
 
@@ -120,3 +121,101 @@ class TensorboardLogger(Logger):
 
     def __repr__(self) -> str:
         return self.experiment.__repr__()
+
+
+class WandbLogger(Logger):
+    """
+    Wrapper for the wandb logger.
+
+    Args:
+        exp_name (str): The name of the experiment.
+
+    """
+
+    def __init__(
+        self,
+        exp_name: str = None,
+        offline: bool = False,
+        save_dir: str = None,
+        id: str = None,
+        project: str = None,
+        **kwargs,
+    ) -> None:
+        self.offline = offline
+        self.save_dir = save_dir
+        self.id = id
+        self.project = project
+        self._wandb_kwargs = {
+            "name": exp_name,
+            "offline": offline,
+            "save_dir": save_dir,
+            "id": id,
+            "project": project,
+            "resume": "allow",
+            **kwargs,
+        }
+        super().__init__(exp_name=exp_name)
+        self.log_dir = self.experiment.save_dir
+
+        self._has_imported_moviepy = False
+
+    def _create_experiment(self) -> "WandbLogger":
+        """
+        Creates a wandb experiment.
+
+        Args:
+            exp_name (str): The name of the experiment.
+
+        """
+        try:
+            import wandb
+        except ImportError:
+            raise ImportError("wandb could not be imported")
+
+        if self.offline:
+            os.environ["WANDB_MODE"] = "dryrun"
+
+        return wandb.init(**self._wandb_kwargs)
+
+    def log_scalar(self, name: str, value: float, step: int = None) -> None:
+        """
+        Logs a scalar value to wandb.
+
+        Args:
+            name (str): The name of the scalar.
+            value (float): The value of the scalar.
+            step (int, optional): The step at which the scalar is logged. Defaults to None.
+        """
+        if step is not None:
+            self.experiment.log({name: value, "trainer/step": step})
+        else:
+            self.experiment.log({name: value})
+
+    def log_video(self, name: str, video: Tensor, step: int = None, **kwargs) -> None:
+        """
+        Log videos inputs to wandb.
+
+        Args:
+            name (str): The name of the video.
+            video (Tensor): The video to be logged.
+            step (int, optional): The step at which the video is logged. Defaults to None.
+        """
+        if not self._has_imported_moviepy:
+            try:
+                import moviepy  # noqa
+
+                self._has_imported_moviepy = True
+            except ImportError:
+                raise Exception(
+                    "moviepy not found, videos cannot be logged with TensorboardLogger"
+                )
+        self.experiment.log({"name": wandb.Video(video, fps=6, format="mp4")})
+
+    def log_hparams(self, cfg: "DictConfig") -> None:
+        """
+        Logs the hyperparameters of the experiment.
+
+        Args:
+            cfg (DictConfig): The configuration of the experiment.
+        """
+        self.experiment.config.update(cfg, allow_val_change=True)
