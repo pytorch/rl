827525d4ea29af3d108341457079aacc39d801f4
Ramon Medel <rmedel@fb.com>
2022-07-19 10:43:30 +0200

    moved result to a variable

--- torchrl/data/tensordict/tensordict.py
+++ torchrl/data/tensordict/tensordict.py
@@ -1295,13 +1295,14 @@ dtype=torch.float32)},
                 raise RuntimeError("repeated dim in permute")
             seen[idx] = True
 
-        return PermutedTensorDict(
+        result = PermutedTensorDict(
             source=self,
             custom_op="permute",
             inv_op="permute",
             custom_op_kwargs={"dims": dims_list},
             inv_op_kwargs={"dims": dims_list},
         )
+        return result
 
     def __repr__(self) -> str:
         fields = _td_fields(self)
