82f8286e989a710fbdef319458a048102a4c10d1
Nicolas Dufour <ndufour@fb.com>
2022-07-26 15:26:34 +0100

    Added tests for Compose.inv()

--- torchrl/envs/transforms/transforms.py
+++ torchrl/envs/transforms/transforms.py
@@ -551,12 +551,11 @@ class Compose(Transform):
         for t in self.transforms:
             tensordict = t(tensordict)
         return tensordict
-    
+
     def _inv_call(self, tensordict: TensorDictBase) -> TensorDictBase:
         for t in self.transforms[::-1]:
             tensordict = t.inv(tensordict)
         return tensordict
-    
 
     def transform_action_spec(self, action_spec: TensorSpec) -> TensorSpec:
         for t in self.transforms:
