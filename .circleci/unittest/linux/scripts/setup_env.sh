8d99c5d2d17806c849b672ba728f2db1bb48f491
Tony Tong <tonytong@fb.com>
2022-08-03 16:37:11 -0400

    should be good

--- .circleci/unittest/linux/scripts/setup_env.sh
+++ .circleci/unittest/linux/scripts/setup_env.sh
@@ -58,8 +58,6 @@ if [[ $OSTYPE == 'darwin'* ]]; then
 else
   # Software rendering requires GLX and OSMesa.
   yum install -y mesa-libOSMesa-devel.x86_64 mesa-libGL-devel.x86_64 mesa-libGLU-devel.x86_64
-  conda install -y -c conda-forge mesa
-  conda install -y -c conda-forge mesalib
   PRIVATE_MUJOCO_GL=osmesa
 fi
 
