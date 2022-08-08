a9b394a91d415e3e9bc0b9a1b3dd9d8c99e2f2b1
Nicolas Dufour <ndufour@fb.com>
2022-08-08 12:10:27 +0100

    Added free nats

--- torchrl/objectives/model_based/dreamer.py
+++ torchrl/objectives/model_based/dreamer.py
@@ -22,23 +22,31 @@ class DreamerModelLoss(nn.Module):
         reward_loss (nn.Module): loss function for the prediction of the reward
 
     """
+
     def __init__(
         self,
+        lambda_kl: float = 1.0,
+        lambda_reco: float = 1.0,
+        lambda_reward: float = 1.0,
         reco_loss: nn.Module = nn.MSELoss(reduction="none"),
         reward_loss: nn.Module = nn.MSELoss(),
+        free_nats: int = 3
     ):
         super().__init__()
         self.reco_loss = reco_loss
         self.reward_loss = reward_loss
+        self.lambda_kl = lambda_kl
+        self.lambda_reco = lambda_reco
+        self.lambda_reward = lambda_reward
+        self.free_nats = free_nats
 
-    def forward(self, tensordict:TensorDict)-> torch.Tensor:
-        flat_prior = d.Normal(
-            tensordict.get("prior_means"), tensordict.get("prior_stds")
+    def forward(self, tensordict: TensorDict) -> torch.Tensor:
+        kl_loss = self.kl_loss(
+            tensordict["prior_mean"],
+            tensordict["prior_std"],
+            tensordict["posterior_mean"],
+            tensordict["posterior_std"],
         )
-        flat_posterior = d.Normal(
-            tensordict.get("posterior_means"), tensordict.get("posterior_stds")
-        )
-        kl_loss = kl_divergence(flat_posterior, flat_prior).mean()
         reco_loss = (
             0.5
             * self.reco_loss(
@@ -51,4 +59,15 @@ class DreamerModelLoss(nn.Module):
         reward_loss = 0.5 * self.reward_loss(
             tensordict.get("reward"), tensordict.get("predicted_reward")
         )
-        return kl_loss + reco_loss + reward_loss
+        return (
+            self.lambda_kl * kl_loss
+            + self.lambda_reco * reco_loss
+            + self.lambda_reward * reward_loss
+        )
+
+    def kl_loss(self, prior_mean, prior_std, posterior_mean, posterior_std):
+        flat_prior = d.Normal(prior_mean, prior_std)
+        flat_posterior = d.Normal(posterior_mean, posterior_std)
+        kl = kl_divergence(flat_posterior, flat_prior)
+        kl = kl.clamp(min=self.free_nats).mean()
+        return kl
