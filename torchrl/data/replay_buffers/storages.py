e4a51efda1d9e23c4decc9e7ef11595cffa2ec81
bamaxw <maxw@fb.com>
2022-08-11 11:29:11 +0100

    ufmt

--- torchrl/data/replay_buffers/storages.py
+++ torchrl/data/replay_buffers/storages.py
@@ -120,7 +120,9 @@ class LazyTensorStorage(Storage):
             print("The storage is being created: ")
             for key, tensor in data.items():
                 if isinstance(tensor, TensorDictBase):
-                    out[key] = tensor.expand(self.max_size).clone().to(self.device).zero_()
+                    out[key] = (
+                        tensor.expand(self.max_size).clone().to(self.device).zero_()
+                    )
                 else:
                     out[key] = torch.empty(
                         self.max_size,
