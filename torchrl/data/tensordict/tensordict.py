d819efb377095856218a458ea02f4647261a9557
Vincent Moens <vincentmoens@gmail.com>
2022-08-12 21:02:31 +0200

    Revert "[BugFix] Functorch-Tensordict bug fixes (#361)" (#362)
    This reverts commit e5b03c1b8c30fc0f63bb417adbfbd82d9ac9e5ae.

--- torchrl/data/tensordict/tensordict.py
+++ torchrl/data/tensordict/tensordict.py
@@ -346,29 +346,23 @@ class TensorDictBase(Mapping, metaclass=abc.ABCMeta):
                 f" {sorted(list(self.keys()))}"
             )
 
-    def apply_(self, fn: Callable, inplace: bool=False) -> TensorDictBase:
+    def apply_(self, fn: Callable) -> TensorDictBase:
         """Applies a callable to all values stored in the tensordict and
         re-writes them in-place.
 
         Args:
             fn (Callable): function to be applied to the tensors in the
                 tensordict.
-            inplace (bool, optional): if True, changes are made in-place.
-                Default is False.
 
         Returns:
-            self or a copy of self with the function applied
+            self
 
         """
-        out = self if inplace else copy(self)
-        for key, item in out.items():
-            if isinstance(item, TensorDictBase):
-                item_trsf = item.apply_(fn, inplace=inplace)
-            else:
-                item_trsf = fn(item)
+        for key, item in self.items():
+            item_trsf = fn(item)
             if item_trsf is not None:
-                out.set(key, item_trsf, inplace=True)
-        return out
+                self.set(key, item_trsf, inplace=True)
+        return self
 
     def apply(
         self, fn: Callable, batch_size: Optional[Sequence[int]] = None
@@ -1408,7 +1402,7 @@ dtype=torch.float32)},
             yield self[i]
 
     def flatten_keys(
-        self, separator: str = ",", inplace: bool = False
+        self, separator: str = ",", inplace: bool = True
     ) -> TensorDictBase:
         to_flatten = []
         for key, meta_value in self.items_meta():
@@ -1441,7 +1435,7 @@ dtype=torch.float32)},
             return tensordict_out
 
     def unflatten_keys(
-        self, separator: str = ",", inplace: bool = False
+        self, separator: str = ",", inplace: bool = True
     ) -> TensorDictBase:
         to_unflatten = defaultdict(lambda: list())
         for key in self.keys():
