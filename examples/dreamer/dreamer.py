22a4b67cef650ab36ab0dec9de998d93cf9fdc28
Nicolas Dufour <ndufour@fb.com>
2022-08-15 17:44:07 +0100

    Added dm_control check

--- examples/dreamer/dreamer.py
+++ examples/dreamer/dreamer.py
@@ -103,7 +103,6 @@ def main(cfg: "DictConfig"):
         device = torch.device("cuda:0")
     else:
         device = torch.device("cpu")
-    device = torch.device("cpu")
     print(f"Using device {device}")
     exp_name = "_".join(
         [
