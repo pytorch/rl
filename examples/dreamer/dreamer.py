29b7c9bcb4e63e2e6ed049a77b26e09eb5f15897
Nicolas Dufour <ndufour@fb.com>
2022-08-15 17:56:16 +0100

    Added dm_control check

--- examples/dreamer/dreamer.py
+++ examples/dreamer/dreamer.py
@@ -235,7 +235,7 @@ def main(cfg: "DictConfig"):
     with torch.profiler.profile(
         activities=[
         torch.profiler.ProfilerActivity.CPU, torch.profiler.ProfilerActivity.CUDA], 
-        schedule=torch.profiler.schedule(skip_first=125, wait=1, warmup=1, active=1, repeat=3),
+        schedule=torch.profiler.schedule(skip_first=125, wait=0, warmup=1, active=1, repeat=1),
         on_trace_ready=torch.profiler.tensorboard_trace_handler('./log/dreamer'),
         record_shapes=True,
         profile_memory=True,
