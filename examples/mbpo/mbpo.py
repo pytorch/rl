85c8053fd0969c58280859495b787d537a5f9407
Nicolas Dufour <ndufour@fb.com>
2022-08-24 16:41:31 +0100

    Added profiler

--- examples/mbpo/mbpo.py
+++ examples/mbpo/mbpo.py
@@ -327,94 +327,101 @@ def main(cfg: "DictConfig"):
 
         if collected_frames >= cfg.init_random_frames:
             # Train model on current replay buffer
+            with torch.profiler.profile(
+                    schedule=torch.profiler.schedule(wait=1, warmup=1, active=2, repeat=1),
+                    on_trace_ready=torch.profiler.tensorboard_trace_handler('./log/resnet18'),
+                    record_shapes=True,
+                    profile_memory=True,
+                    with_stack=True
+            ) as prof:
+                for j in range(cfg.optim_steps_per_batch):
+                    # Train Model
+                    # Sample data from model and buffer it
+                    if j % cfg.train_model_every_k_optim_step == 0:
+                        for _ in range(len(real_replay_buffer) // cfg.model_batch_size):
+                            model_sampled_tensordict = real_replay_buffer.sample(
+                                cfg.model_batch_size
+                            ).to(device)
 
-            for j in range(cfg.optim_steps_per_batch):
-                # Train Model
-                # Sample data from model and buffer it
-                if j % cfg.train_model_every_k_optim_step == 0:
-                    for _ in range(len(real_replay_buffer) // cfg.model_batch_size):
-                        model_sampled_tensordict = real_replay_buffer.sample(
-                            cfg.model_batch_size
+                            with autocast(dtype=torch.float16):
+                                model_loss_td = world_model_loss(model_sampled_tensordict)
+                            scaler1.scale(model_loss_td["loss_world_model"]).backward()
+                            scaler1.unscale_(world_model_opt)
+                            clip_grad_norm_(world_model_loss.parameters(), cfg.grad_clip)
+                            scaler1.step(world_model_opt)
+                            world_model_opt.zero_grad()
+                            scaler1.update()
+                    with torch.no_grad(), set_exploration_mode("random"):
+                        for _ in range(cfg.num_model_rollouts):
+                            model_sampled_tensordict = real_replay_buffer.sample(
+                                cfg.model_batch_size
+                            ).to(device)
+                            fake_traj_tensordict = model_based_env.rollout(
+                                max_steps=cfg.imagination_horizon,
+                                policy=policy,
+                                auto_reset=False,
+                                tensordict=model_sampled_tensordict,
+                            )
+                            fake_traj_tensordict = fake_traj_tensordict.select(
+                                *original_keys
+                            )
+                            fake_replay_buffer.extend(
+                                fake_traj_tensordict.view(-1).cpu()
+                            )
+
+                    for _ in range(cfg.num_sac_training_steps_per_optim_step):
+
+                        num_real_samples = int(cfg.sac_batch_size * cfg.real_data_ratio)
+
+                        num_fake_samples = cfg.sac_batch_size - num_real_samples
+
+                        # agent_sampled_tensordict = fake_replay_buffer.sample(cfg.sac_batch_size)
+
+                        fake_sampled_tensordict = fake_replay_buffer.sample(
+                            num_fake_samples
+                        )
+
+                        real_sampled_tensordict = real_replay_buffer.sample(
+                            num_real_samples
+                        )
+
+                        agent_sampled_tensordict = torch.cat(
+                            [
+                                fake_sampled_tensordict,
+                                real_sampled_tensordict,
+                            ],
+                            dim=0,
                         ).to(device)
 
+                        ### Train agent
                         with autocast(dtype=torch.float16):
-                            model_loss_td = world_model_loss(model_sampled_tensordict)
-                        scaler1.scale(model_loss_td["loss_world_model"]).backward()
-                        scaler1.unscale_(world_model_opt)
-                        clip_grad_norm_(world_model_loss.parameters(), cfg.grad_clip)
-                        scaler1.step(world_model_opt)
-                        world_model_opt.zero_grad()
-                        scaler1.update()
-                with torch.no_grad(), set_exploration_mode("random"):
-                    for _ in range(cfg.num_model_rollouts):
-                        model_sampled_tensordict = real_replay_buffer.sample(
-                            cfg.model_batch_size
-                        ).to(device)
-                        fake_traj_tensordict = model_based_env.rollout(
-                            max_steps=cfg.imagination_horizon,
-                            policy=policy,
-                            auto_reset=False,
-                            tensordict=model_sampled_tensordict,
-                        )
-                        fake_traj_tensordict = fake_traj_tensordict.select(
-                            *original_keys
-                        )
-                        fake_replay_buffer.extend(
-                            fake_traj_tensordict.view(-1).cpu()
-                        )
+                            sac_loss_td = sac_loss(agent_sampled_tensordict)
+                            sac_loss_sum = (
+                                sac_loss_td["loss_actor"]
+                                + sac_loss_td["loss_qvalue"]
+                                + sac_loss_td["loss_value"]
+                                + sac_loss_td["loss_alpha"]
+                            )
+                        scaler2.scale(sac_loss_sum).backward()
+                        scaler2.unscale_(sac_opt)
+                        clip_grad_norm_(sac_loss.parameters(), cfg.grad_clip)
+                        scaler2.step(sac_opt)
+                        sac_opt.zero_grad()
+                        scaler2.update()
 
-                for _ in range(cfg.num_sac_training_steps_per_optim_step):
+                        target_net_updater.step()
+                    prof.step()
 
-                    num_real_samples = int(cfg.sac_batch_size * cfg.real_data_ratio)
-
-                    num_fake_samples = cfg.sac_batch_size - num_real_samples
-
-                    # agent_sampled_tensordict = fake_replay_buffer.sample(cfg.sac_batch_size)
-
-                    fake_sampled_tensordict = fake_replay_buffer.sample(
-                        num_fake_samples
-                    )
-
-                    real_sampled_tensordict = real_replay_buffer.sample(
-                        num_real_samples
-                    )
-
-                    agent_sampled_tensordict = torch.cat(
-                        [
-                            fake_sampled_tensordict,
-                            real_sampled_tensordict,
-                        ],
-                        dim=0,
-                    ).to(device)
-
-                ### Train agent
-                with autocast(dtype=torch.float16):
-                    sac_loss_td = sac_loss(agent_sampled_tensordict)
-                    sac_loss_sum = (
-                        sac_loss_td["loss_actor"]
-                        + sac_loss_td["loss_qvalue"]
-                        + sac_loss_td["loss_value"]
-                        + sac_loss_td["loss_alpha"]
-                    )
-                scaler2.scale(sac_loss_sum).backward()
-                scaler2.unscale_(sac_opt)
-                clip_grad_norm_(sac_loss.parameters(), cfg.grad_clip)
-                scaler2.step(sac_opt)
-                sac_opt.zero_grad()
-                scaler2.update()
-
-                target_net_updater.step()
-
-                with torch.no_grad(), set_exploration_mode("mode"):
-                    td_record = record(None)
-                    if td_record is not None and logger is not None:
-                        for key, value in td_record.items():
-                            if key in ["r_evaluation", "total_r_evaluation"]:
-                                logger.log_scalar(
-                                    key,
-                                    value.detach().cpu().numpy(),
-                                    step=collected_frames,
-                                )
+                    with torch.no_grad(), set_exploration_mode("mode"):
+                        td_record = record(None)
+                        if td_record is not None and logger is not None:
+                            for key, value in td_record.items():
+                                if key in ["r_evaluation", "total_r_evaluation"]:
+                                    logger.log_scalar(
+                                        key,
+                                        value.detach().cpu().numpy(),
+                                        step=collected_frames,
+                                    )
 
 
 if __name__ == "__main__":
