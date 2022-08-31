acb5eac7bc520d69e4e8a754c0c8c57f677953db
vmoens <vincentmoens@gmail.com>
2022-08-30 21:53:23 -0400

    download weights from s3

--- torchrl/envs/transforms/r3m.py
+++ torchrl/envs/transforms/r3m.py
@@ -1,6 +1,8 @@
 import torch
+from torch.hub import load_state_dict_from_url
 from torch.nn import Identity
 
+from torchrl.data import TensorDict
 from torchrl.envs.transforms import (
     ToTensorImage,
     Compose,
@@ -30,12 +32,15 @@ class _R3MNet(Transform):
                 "torchvision installed in your environment."
             )
         if model_name == "resnet18":
+            self.model_name = "r3m_18"
             self.outdim = 512
             convnet = models.resnet18(pretrained=False)
-        elif model_name == "resnet18":
+        elif model_name == "resnet34":
+            self.model_name = "r3m_34"
             self.outdim = 512
             convnet = models.resnet34(pretrained=False)
         elif model_name == "resnet50":
+            self.model_name = "r3m_50"
             self.outdim = 2048
             convnet = models.resnet50(pretrained=False)
         else:
@@ -62,8 +67,23 @@ class _R3MNet(Transform):
         return out
 
     @staticmethod
-    def _load_weights(convnet, model_name):
-        raise NotImplementedError
+    def _load_weights(model_name, r3m_instance):
+        if model_name not in ("r3m_50", "r3m_34", "r3m_18"):
+            raise ValueError(
+                "model_name should be one of 'r3m_50', 'r3m_34' or 'r3m_18'"
+            )
+        # url = "https://download.pytorch.org/models/rl/r3m/" + model_name
+        url = "https://pytorch.s3.amazonaws.com/models/rl/r3m/" + model_name + ".pt"
+        d = load_state_dict_from_url(
+            url, progress=True, map_location=next(r3m_instance.parameters()).device
+        )
+        td = TensorDict(d["r3m"], []).unflatten_keys(".")
+        td_flatten = td["module"]["convnet"].flatten_keys(".")
+        state_dict = td_flatten.to_dict()
+        r3m_instance.convnet.load_state_dict(state_dict)
+
+    def load_weights(self):
+        self._load_weights(self.model_name, self)
 
 
 class R3MTransform(Compose):
@@ -71,7 +91,10 @@ class R3MTransform(Compose):
     TODO
     """
 
-    def __init__(self, model_name, keys_in=None, keys_out=None, size=244):
+    def __init__(
+        self, model_name, keys_in=None, keys_out=None, size=244, download=False
+    ):
+        self.download = download
         # ToTensor
         totensor = ToTensorImage(unsqueeze=False, keys_in=keys_in, keys_out=keys_out)
         keys_out = totensor.keys_out
@@ -90,3 +113,5 @@ class R3MTransform(Compose):
         network = _R3MNet(in_keys=keys_out, out_keys=keys_out, model_name=model_name)
         transforms = [totensor, resize, normalize, network]
         super().__init__(*transforms)
+        if self.download:
+            self[-1].load_weights()
