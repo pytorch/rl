6ee530f515ffba7bdc43b051598474c2facbe652
vmoens <vincentmoens@gmail.com>
2022-08-31 22:19:54 -0400

    amend -y

--- .circleci/unittest/linux_olddeps/scripts/install.sh
+++ .circleci/unittest/linux_olddeps/scripts/install.sh
@@ -34,9 +34,9 @@ git submodule sync && git submodule update --init --recursive
 
 printf "Installing PyTorch with %s\n" "${CU_VERSION}"
 if [ "${CU_VERSION:-}" == cpu ] ; then
-    conda install pytorch==1.10.0 torchvision==0.11.0 torchaudio==0.10.0 cpuonly -c pytorch
+    conda install pytorch==1.10.0 torchvision==0.11.0 torchaudio==0.10.0 cpuonly -c pytorch -y
 else
-    conda install pytorch==1.10.0 torchvision==0.11.0 torchaudio==0.10.0 cudatoolkit=11.3 -c pytorch -c conda-forge
+    conda install pytorch==1.10.0 torchvision==0.11.0 torchaudio==0.10.0 cudatoolkit=11.3 -c pytorch -c conda-forge -y
 fi
 
 printf "* Installing torchrl\n"
