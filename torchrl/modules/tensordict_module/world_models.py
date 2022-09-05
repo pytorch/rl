c5b597cb0c0af2d72d719d7bc685323f4f1728fa
Nicolas Dufour <ndufour@fb.com>
2022-09-05 10:50:24 +0100

    Small fixes and renaming

--- torchrl/modules/tensordict_module/world_models.py
+++ torchrl/modules/tensordict_module/world_models.py
@@ -13,29 +13,29 @@ class WorldModelWrapper(TensorDictSequence):
     """
     World model wrapper.
     This module wraps together a world model and a reward model.
-    The world model is used to predict an imaginary world state.
+    The world state model is used to predict an imaginary world state.
     The reward model is used to predict the reward of the imaginary world state.
 
     Args:
-        world_model (TensorDictModule): a world model that generates a world state.
+        world_state_model (TensorDictModule): a world state model that generates a new world state.
         reward_model (TensorDictModule): a reward model, that reads the world state and returns a reward
 
     """
 
     def __init__(
         self,
-        world_model: TensorDictModule,
+        world_state_model: TensorDictModule,
         reward_model: TensorDictModule,
     ):
         super().__init__(
-            world_model,
+            world_state_model,
             reward_model,
         )
 
-    def get_world_modeler_operator(self) -> TensorDictSequence:
+    def get_world_state_model_operator(self) -> TensorDictSequence:
         """
 
-        Returns a world modeler operator that maps either an observation to a world state or a world state to the next world state.
+        Returns a world state operator that maps either an observation to a world state or a world state to the next world state.
 
         """
         return self.module[0]
