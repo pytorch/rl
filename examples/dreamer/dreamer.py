c19153172f4434560d811bea4aa3bba838448d9b
Nicolas Dufour <ndufour@fb.com>
2022-09-09 16:31:46 +0100

    reference with no model training

--- examples/dreamer/dreamer.py
+++ examples/dreamer/dreamer.py
@@ -204,7 +204,7 @@ def main(cfg: "DictConfig"):
         logger = WandbLogger(
             f"dreamer/{exp_name}",
             project="torchrl",
-            group=f"Dreamer_{cfg.env_name}",
+            group=f"Dreamer_{cfg.env_name}_ref",
             offline=cfg.offline_logging,
         )
     elif cfg.logger == "csv":
