22d15a46d495b47d47db9d9b065d3ee441b67124
Nicolas Dufour <ndufour@fb.com>
2022-09-12 16:03:15 +0100

    Fixed string handling

--- torchrl/modules/utils/mappings.py
+++ torchrl/modules/utils/mappings.py
@@ -81,7 +81,7 @@ def mappings(key: str) -> Callable:
     if key in _mappings:
         return _mappings[key]
     elif key.startswith("biased_softplus"):
-        stripped_key = key.replace("biased_softplus", "").split("_")
+        stripped_key = key.replace("biased_softplus_", "").split("_")
         if len(stripped_key) == 1:
             return biased_softplus(float(stripped_key[-1]))
         elif len(stripped_key) == 2:
