d372043dd10da5079fef2bbd443bb0df2535005c
vmoens <vincentmoens@gmail.com>
2022-09-19 13:54:45 +0100

    _run_checks=False

--- torchrl/envs/model_based/common.py
+++ torchrl/envs/model_based/common.py
@@ -113,7 +113,7 @@ class ModelBasedEnv(EnvBase, metaclass=abc.ABCMeta):
     @classmethod
     def __new__(cls, *args, **kwargs):
         return super().__new__(
-            cls, *args, _inplace_update=False, _batch_locked=False, **kwargs
+            cls, *args, _inplace_update=False, _batch_locked=False, _run_checks=False, **kwargs
         )
 
     def set_specs_from_env(self, env: EnvBase):
