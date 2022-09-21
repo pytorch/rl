f5844a3f1545fd31b7aa596ef1e6d8f4e228f62d
vmoens <vincentmoens@gmail.com>
2022-09-21 13:26:00 +0100

    amend

--- test/test_configs.py
+++ test/test_configs.py
@@ -1,40 +1,66 @@
 import argparse
 
 import pytest
-from hydra.utils import instantiate
-from omegaconf import OmegaConf
-from torch import nn
 
+try:
+    import hydra
+    from hydra.core.global_hydra import GlobalHydra
+    from hydra.utils import instantiate
+
+    _has_hydra = True
+except ImportError:
+    _has_hydra = False
+from mocking_classes import ContinuousActionVecMockEnv
+from torch import nn
 from torchrl.modules import TensorDictModule
 
-from mocking_classes import ContinuousActionVecMockEnv
 
 def make_env():
     def fun():
         return ContinuousActionVecMockEnv()
+
     return fun
 
-@pytest.mark.parametrize("file", [
-    "examples/configs/collectors/async_sync.yaml",
-    "examples/configs/collectors/sync_single.yaml",
-    "examples/configs/collectors/sync_sync.yaml",
-])
+
+@pytest.fixture(scope="session", autouse=True)
+def init_hydra(request):
+    hydra.initialize("../examples/configs/")
+    request.addfinalizer(GlobalHydra.instance().clear)
+
+
+@pytest.mark.skipif(not _has_hydra, reason="No hydra found")
+@pytest.mark.parametrize(
+    "file",
+    [
+        "async_sync",
+        "sync_single",
+        "sync_sync",
+    ],
+)
 def test_collector_configs(file):
     create_env = make_env()
     policy = TensorDictModule(
-        nn.Linear(7, 7),
-        in_keys=["observation"],
-        out_keys=["action"]
+        nn.Linear(7, 7), in_keys=["observation"], out_keys=["action"]
     )
 
-    yaml_read = OmegaConf.load(file)
-    cfg = OmegaConf.create(yaml_read)
+    cfg = hydra.compose(
+        "config",
+    )  # overrides=[f"collectors={file}"])
+    # yaml_read = OmegaConf.load("examples/configs/config.yaml")
+    # cfg = OmegaConf.create(yaml_read, flags={"collector": file})
+    print(cfg)
 
     if cfg.num_workers == 0:
         create_env_fn = create_env
     else:
-        create_env_fn = [create_env, ] * cfg.num_workers
-    instantiate(cfg.collector, policy=policy, create_env_fn=create_env_fn)
+        create_env_fn = [
+            create_env,
+        ] * cfg.num_workers
+    collector = instantiate(cfg.collectors, policy=policy, create_env_fn=create_env_fn)
+    for data in collector:
+        assert data.numel() == 200
+        break
+    collector.shutdown()
 
 
 if __name__ == "__main__":
--- torchrl/trainers/helpers/envs.py
+++ torchrl/trainers/helpers/envs.py
@@ -3,9 +3,7 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-from dataclasses import dataclass
-from dataclasses import field as dataclass_field
-from typing import Callable, Optional, Union, Any, Sequence
+from typing import Callable, Optional, Union, Sequence
 
 import torch
 
