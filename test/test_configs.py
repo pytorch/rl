d192e99ce19e46ffea33660c187c14317d0bd76c
vmoens <vincentmoens@gmail.com>
2022-09-22 18:02:46 +0100

    transforms

--- test/test_configs.py
+++ test/test_configs.py
@@ -1,6 +1,7 @@
 import argparse
 
 import pytest
+from torchrl.envs import TransformedEnv, Compose
 
 try:
     import hydra
@@ -31,55 +32,78 @@ def init_hydra(request):
 
 
 @pytest.mark.skipif(not _has_hydra, reason="No hydra found")
-@pytest.mark.parametrize(
-    "file,num_workers",
-    [
-        ("async_sync", 2),
-        ("sync_single", 0),
-        ("sync_sync", 2),
-    ],
-)
-def test_collector_configs(file, num_workers):
-    create_env = make_env()
-    policy = TensorDictModule(
-        nn.Linear(7, 7), in_keys=["observation"], out_keys=["action"]
+class TestConfigs:
+    @pytest.mark.parametrize(
+        "file,num_workers",
+        [
+            ("async_sync", 2),
+            ("sync_single", 0),
+            ("sync_sync", 2),
+        ],
     )
+    def test_collector_configs(self, file, num_workers):
+        create_env = make_env()
+        policy = TensorDictModule(
+            nn.Linear(7, 7), in_keys=["observation"], out_keys=["action"]
+        )
 
-    cfg = hydra.compose(
-        "config", overrides=[f"collector={file}", f"num_workers={num_workers}"]
+        cfg = hydra.compose(
+            "config", overrides=[f"collector={file}", f"num_workers={num_workers}"]
+        )
+
+        if cfg.num_workers == 0:
+            create_env_fn = create_env
+        else:
+            create_env_fn = [
+                create_env,
+            ] * cfg.num_workers
+        collector = instantiate(
+            cfg.collector, policy=policy, create_env_fn=create_env_fn
+        )
+        for data in collector:
+            assert data.numel() == 200
+            break
+        collector.shutdown()
+
+    @pytest.mark.skipif(not _has_gym, reason="No gym found")
+    @pytest.mark.skipif(not _has_dmc, reason="No gym found")
+    @pytest.mark.parametrize(
+        "file",
+        [
+            "dmcontrol_pixels",
+            "dmcontrol_state",
+            "gym_pixels",
+            "gym_state",
+        ],
     )
+    def test_env_configs(self, file):
+        cfg = hydra.compose("config", overrides=[f"env={file}"])
 
-    if cfg.num_workers == 0:
-        create_env_fn = create_env
-    else:
-        create_env_fn = [
-            create_env,
-        ] * cfg.num_workers
-    collector = instantiate(cfg.collector, policy=policy, create_env_fn=create_env_fn)
-    for data in collector:
-        assert data.numel() == 200
-        break
-    collector.shutdown()
+        env = instantiate(cfg.env)
+        env.rollout(3)
+        env.close()
 
+    @pytest.mark.skipif(not _has_gym, reason="No gym found")
+    @pytest.mark.skipif(not _has_dmc, reason="No gym found")
+    @pytest.mark.parametrize(
+        "env_file,transform_file",
+        [
+            ["dmcontrol_pixels", "pixels"],
+            ["dmcontrol_state", "state"],
+            ["gym_pixels", "pixels"],
+            ["gym_state", "state"],
+        ],
+    )
+    def test_transforms_configs(self, env_file, transform_file):
+        cfg = hydra.compose(
+            "config", overrides=[f"env={env_file}", f"transforms={transform_file}"]
+        )
 
-@pytest.mark.skipif(not _has_hydra, reason="No hydra found")
-@pytest.mark.skipif(not _has_gym, reason="No gym found")
-@pytest.mark.skipif(not _has_dmc, reason="No gym found")
-@pytest.mark.parametrize(
-    "file",
-    [
-        "dmcontrol_pixels",
-        "dmcontrol_state",
-        "gym_pixels",
-        "gym_state",
-    ],
-)
-def test_env_configs(file):
-    cfg = hydra.compose("config", overrides=[f"env={file}"])
-
-    env = instantiate(cfg.env)
-    env.rollout(50)
-    env.close()
+        base_env = instantiate(cfg.env)
+        transforms = [instantiate(transform) for transform in cfg.transforms]
+        env = TransformedEnv(base_env, Compose(*transforms))
+        env.rollout(3)
+        env.close()
 
 
 if __name__ == "__main__":
