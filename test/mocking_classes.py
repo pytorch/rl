5934220bd08982e5b8737b1e41675f87a061ddf6
Nicolas Dufour <ndufour@fb.com>
2022-09-23 12:19:40 +0100

    linting

--- test/mocking_classes.py
+++ test/mocking_classes.py
@@ -216,7 +216,9 @@ class MockBatchedLockedEnv(EnvBase):
         )
 
         return TensorDict(
-            {"reward": n, "done": done, "next_observation": n}, tensordict.batch_size, device=self.device
+            {"reward": n, "done": done, "next_observation": n},
+            tensordict.batch_size,
+            device=self.device,
         )
 
     def _reset(self, tensordict: TensorDictBase, **kwargs) -> TensorDictBase:
@@ -235,7 +237,9 @@ class MockBatchedLockedEnv(EnvBase):
         done = torch.full(batch_size, done, dtype=torch.bool, device=self.device)
 
         return TensorDict(
-            {"reward": n, "done": done, "next_observation": n}, batch_size, device=self.device
+            {"reward": n, "done": done, "next_observation": n},
+            batch_size,
+            device=self.device,
         )
 
 
