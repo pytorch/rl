29b401cba04f67dce03ff2bdacd218c5c5855331
silvestrebahi <silvestre.bahi@gmail.com>
2022-10-07 17:58:52 +0200

    Update run_test.sh
    lint end of file

--- .circleci/unittest/linux_stable/scripts/run_test.sh
+++ .circleci/unittest/linux_stable/scripts/run_test.sh
@@ -22,4 +22,3 @@ coverage run -m pytest test/smoke_test.py -v --durations 20
 coverage run -m pytest test/smoke_test_deps.py -v --durations 20
 coverage run -m pytest --instafail -v --durations 20
 coverage xml
-
