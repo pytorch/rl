07254ec35b1fa3db5c0ebeb3f56c58deffdc4a11
vmoens <vincentmoens@gmail.com>
2022-10-12 15:16:56 +0100

    init

--- .circleci/unittest/linux/scripts/run_test.sh
+++ .circleci/unittest/linux/scripts/run_test.sh
@@ -18,7 +18,7 @@ lib_dir="${env_dir}/lib"
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$lib_dir
 export MKL_THREADING_LAYER=GNU
 
-coverage run -m pytest test/smoke_test.py -v --durations 20
-coverage run -m pytest test/smoke_test_deps.py -v --durations 20
-coverage run -m pytest --instafail -v --durations 20
+MP_START_METHOD=fork coverage run -m pytest test/smoke_test.py -v --durations 20
+MP_START_METHOD=fork coverage run -m pytest test/smoke_test_deps.py -v --durations 20
+MP_START_METHOD=fork coverage run -m pytest --instafail -v --durations 20
 coverage xml
