f8ebf330cc7ce18e17392d89d833fc501330193d
Vincent Moens <vincentmoens@gmail.com>
2022-10-18 16:00:17 +0100

    [Doc] Publishing on pytorch.org (#582)

--- torchrl/objectives/__init__.py
+++ torchrl/objectives/__init__.py
@@ -15,5 +15,6 @@ from .utils import (
     distance_loss,
     hold_out_params,
     next_state_value,
+    hold_out_net,
 )
 from .value import bellman_max, c_val, dv_val, vtrace, GAE, TDLambdaEstimate, TDEstimate
