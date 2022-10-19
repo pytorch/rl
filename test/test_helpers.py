8c3164664849af43b3071c1537a00a41153d5fd5
vmoens <vincentmoens@gmail.com>
2022-10-19 21:35:06 +0100

    amend

--- test/test_helpers.py
+++ test/test_helpers.py
@@ -529,7 +529,7 @@ def test_dreamer_make(device, tanh_loc, exploration):
     sys.path.append(os.path.dirname(__file__) + "/../examples/dreamer/")
     from dreamer_utils import transformed_env_constructor
 
-    flags = ["from_pixels=True", "catframes=1"]
+    flags = ["from_pixels=True", "catframes=1", f"collector_devices={device}"]
 
     config_fields = [
         (config_field.name, config_field.type, config_field)
