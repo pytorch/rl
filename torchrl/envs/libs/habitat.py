1254345ac6dff0e601bfe89b758641d83b549e53
vmoens <vincentmoens@gmail.com>
2022-10-21 17:31:33 +0100

    time to impact: 5 mins

--- torchrl/envs/libs/habitat.py
+++ torchrl/envs/libs/habitat.py
@@ -1,12 +1,15 @@
 from torchrl.envs.libs.gym import GymEnv
 
 try:
+    import habitat
     import habitat.utils.gym_definitions  # noqa
 
     _has_habitat = True
 except ImportError:
     _has_habitat = False
 
+import habitat
+import habitat.utils.gym_definitions  # noqa
 
 class HabitatEnv(GymEnv):
     """A wrapper for habitat envs.
