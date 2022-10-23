72d3aeb8d7d24bb6351284fdb0daf596229af3fc
vmoens <vincentmoens@gmail.com>
2022-10-23 14:16:45 +0100

    amend

--- .circleci/unittest/linux_libs/scripts/install_habitat.sh
+++ .circleci/unittest/linux_libs/scripts/install_habitat.sh
@@ -10,8 +10,14 @@ set -e
 eval "$(./conda/bin/conda shell.bash hook)"
 conda activate ./env
 
-conda install -c anaconda mesa-libgl-cos6-x86_64 -y
-conda install -c anaconda mesa-libegl-cos6-x86_64 -y
+yum makecache
+yum install -y glfw
+yum install -y glew
+yum install -y mesa-libGL
+yum install -y mesa-libGL-devel
+yum install -y mesa-libOSMesa-devel
+yum -y install egl-utils
+yum -y install freeglut
 
 # suggested by habitat team: do we need this?
 #conda create -n $MY_TEST_ENV python=3.7 cmake=3.14.0 -y
@@ -22,10 +28,6 @@ conda install habitat-sim withbullet headless -c conda-forge -c aihabitat-nightl
 conda run python -m pip install install git+https://github.com/facebookresearch/habitat-lab.git#subdirectory=habitat-lab
 conda run python -m pip install install gym[accept-rom-license]
 conda run python -m pip install install gym[atari]
-
-# This is to reduce verbosity
-conda env config vars set MAGNUM_LOG=quiet HABITAT_SIM_LOG=quiet \
-  LD_PRELOAD="/home/circleci/project/conda/pkgs/mesa-libegl-cos6-x86_64-11.0.7-4/x86_64-conda_cos6-linux-gnu/sysroot/usr/lib64/libEGL.so.1 /home/circleci/project/conda/pkgs/mesa-libgl-cos6-x86_64-11.0.7-4/x86_64-conda_cos6-linux-gnu/sysroot/usr/lib64/libGL.so.1"
 
 conda deactivate
 conda activate ./env
