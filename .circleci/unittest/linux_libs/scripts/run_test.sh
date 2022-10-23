bcb2348ce67d4421eb297690bf93d3d5ecd9c2f4
vmoens <vincentmoens@gmail.com>
2022-10-23 14:50:19 +0100

    amend

--- .circleci/unittest/linux_libs/scripts/run_test.sh
+++ .circleci/unittest/linux_libs/scripts/run_test.sh
@@ -9,10 +9,14 @@ export PYTORCH_TEST_WITH_SLOW='1'
 python -m torch.utils.collect_env
 # Avoid error: "fatal: unsafe repository"
 git config --global --add safe.directory '*'
-root_dir="$(git rev-parse --show-toplevel)"
 
-#export DISPLAY=unix:0.0
-#export MKL_THREADING_LAYER=GNU
+root_dir="$(git rev-parse --show-toplevel)"
+env_dir="${root_dir}/env"
+lib_dir="${env_dir}/lib"
+
+# solves ImportError: /lib64/libstdc++.so.6: version `GLIBCXX_3.4.21' not found
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$lib_dir
+export MKL_THREADING_LAYER=GNU
 
 # this workflow only tests the libs
 python -c "import habitat;import habitat.utils.gym_definitions"
