7829d6f27d5f114b999c5a40b68c62bccdf376d9
vmoens <vincentmoens@gmail.com>
2022-10-23 15:39:33 +0100

    amend

--- .circleci/unittest/linux_libs/scripts/install_habitat.sh
+++ .circleci/unittest/linux_libs/scripts/install_habitat.sh
@@ -10,14 +10,10 @@ set -e
 eval "$(./conda/bin/conda shell.bash hook)"
 conda activate ./env
 
-yum makecache
-yum install -y glfw
-yum install -y glew
-yum install -y mesa-libGL
-yum install -y mesa-libGL-devel
-yum install -y mesa-libOSMesa-devel
-yum -y install egl-utils
-yum -y install freeglut
+sudo apt-get install -y libglfw3
+sudo apt-get install -y libglfw3-dev
+sudo apt-get install -y freeglut3-dev
+sudo apt-get install -y libgl1-mesa-glx
 
 # suggested by habitat team: do we need this?
 #conda create -n $MY_TEST_ENV python=3.7 cmake=3.14.0 -y
