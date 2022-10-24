54e256da469f59866101e16614453da8211c7c8e
Artem Korenev <artk@fb.com>
2022-10-24 22:01:41 +0100

    Fix format

--- test/test_actors.py
+++ test/test_actors.py
@@ -1,7 +1,10 @@
 import pytest
 import torch
+from torchrl.modules.tensordict_module.actors import (
+    QValueHook,
+    DistributionalQValueHook,
+)
 
-from torchrl.modules.tensordict_module.actors import QValueHook, DistributionalQValueHook
 
 def test_qvalue_hook_wrong_action_space():
     with pytest.raises(ValueError):
@@ -12,19 +15,22 @@ def test_distributional_qvalue_hook_wrong_action_space():
     with pytest.raises(ValueError):
         DistributionalQValueHook(action_space="wrong_value", support=None)
 
+
 @pytest.mark.parametrize(
     "action_space, expected_action",
     (
         ("one_hot", [0, 0, 1, 0, 0]),
         ("categorical", 2),
-    )
+    ),
 )
 def test_qvalue_hook_0_dim_batch(action_space, expected_action):
     hook = QValueHook(action_space=action_space)
-    
+
     in_values = torch.tensor([1.0, -1.0, 100.0, -2.0, -3.0])
-    action, values, chosen_action_value = hook(net=None, observation=None, values=in_values)
-    
+    action, values, chosen_action_value = hook(
+        net=None, observation=None, values=in_values
+    )
+
     assert (torch.tensor(expected_action, dtype=torch.long) == action).all()
     assert (values == in_values).all()
     assert (torch.tensor([100.0]) == chosen_action_value).all()
@@ -35,17 +41,21 @@ def test_qvalue_hook_0_dim_batch(action_space, expected_action):
     (
         ("one_hot", [[0, 0, 1, 0, 0], [1, 0, 0, 0, 0]]),
         ("categorical", [2, 0]),
-    )
+    ),
 )
 def test_qvalue_hook_1_dim_batch(action_space, expected_action):
     hook = QValueHook(action_space=action_space)
-    
-    in_values = torch.tensor([
-        [1.0, -1.0, 100.0, -2.0, -3.0],
-        [5.0, 4.0, 3.0, 2.0, -5.0],
-    ])
-    action, values, chosen_action_value = hook(net=None, observation=None, values=in_values)
-    
+
+    in_values = torch.tensor(
+        [
+            [1.0, -1.0, 100.0, -2.0, -3.0],
+            [5.0, 4.0, 3.0, 2.0, -5.0],
+        ]
+    )
+    action, values, chosen_action_value = hook(
+        net=None, observation=None, values=in_values
+    )
+
     assert (torch.tensor(expected_action, dtype=torch.long) == action).all()
     assert (values == in_values).all()
     assert (torch.tensor([[100.0], [5.0]]) == chosen_action_value).all()
@@ -56,20 +66,24 @@ def test_qvalue_hook_1_dim_batch(action_space, expected_action):
     (
         ("one_hot", [0, 0, 1, 0, 0]),
         ("categorical", 2),
-    )
+    ),
 )
-def test_distributional_qvalue_hook_0_dim_batch(action_space, expected_action):    
+def test_distributional_qvalue_hook_0_dim_batch(action_space, expected_action):
     support = torch.tensor([-2.0, 0.0, 2.0])
     hook = DistributionalQValueHook(action_space=action_space, support=support)
-    
-    in_values = torch.nn.LogSoftmax(dim=-1)(torch.tensor([
-            [1.0, -1.0, 11.0, -2.0, 30.0],
-            [1.0, -1.0, 1.0, -2.0, -3.0],
-            [1.0, -1.0, 10.0, -2.0, -3.0],
-    ]))
+
+    in_values = torch.nn.LogSoftmax(dim=-1)(
+        torch.tensor(
+            [
+                [1.0, -1.0, 11.0, -2.0, 30.0],
+                [1.0, -1.0, 1.0, -2.0, -3.0],
+                [1.0, -1.0, 10.0, -2.0, -3.0],
+            ]
+        )
+    )
     action, values = hook(net=None, observation=None, values=in_values)
     expected_action = torch.tensor(expected_action, dtype=torch.long)
-    
+
     assert action.shape == expected_action.shape
     assert (action == expected_action).all()
     assert values.shape == in_values.shape
@@ -81,27 +95,31 @@ def test_distributional_qvalue_hook_0_dim_batch(action_space, expected_action):
     (
         ("one_hot", [[0, 0, 1, 0, 0], [1, 0, 0, 0, 0]]),
         ("categorical", [2, 0]),
-    )
+    ),
 )
 def test_qvalue_hook_categorical_1_dim_batch(action_space, expected_action):
     support = torch.tensor([-2.0, 0.0, 2.0])
     hook = DistributionalQValueHook(action_space=action_space, support=support)
-    
-    in_values = torch.nn.LogSoftmax(dim=-1)(torch.tensor([
-        [
-            [1.0, -1.0, 11.0, -2.0, 30.0],
-            [1.0, -1.0, 1.0, -2.0, -3.0],
-            [1.0, -1.0, 10.0, -2.0, -3.0],
-        ],
-        [
-            [11.0, -1.0, 7.0, -1.0, 20.0],
-            [10.0, 19.0, 1.0, -2.0, -3.0],
-            [1.0, -1.0, 0.0, -2.0, -3.0],
-        ],
-    ]))
+
+    in_values = torch.nn.LogSoftmax(dim=-1)(
+        torch.tensor(
+            [
+                [
+                    [1.0, -1.0, 11.0, -2.0, 30.0],
+                    [1.0, -1.0, 1.0, -2.0, -3.0],
+                    [1.0, -1.0, 10.0, -2.0, -3.0],
+                ],
+                [
+                    [11.0, -1.0, 7.0, -1.0, 20.0],
+                    [10.0, 19.0, 1.0, -2.0, -3.0],
+                    [1.0, -1.0, 0.0, -2.0, -3.0],
+                ],
+            ]
+        )
+    )
     action, values = hook(net=None, observation=None, values=in_values)
     expected_action = torch.tensor(expected_action, dtype=torch.long)
-            
+
     assert action.shape == expected_action.shape
     assert (action == expected_action).all()
     assert values.shape == in_values.shape
--- test/test_utils.py
+++ test/test_utils.py
@@ -1,4 +1,5 @@
 import os
+
 import pytest
 from torchrl._utils import get_binary_env_var
 
@@ -7,12 +8,12 @@ from torchrl._utils import get_binary_env_var
 def test_get_binary_env_var_positive(value):
     try:
         key = "SOME_ENVIRONMENT_VARIABLE_UNLIKELY_TO_BE_IN_ENVIRONMENT"
-        
+
         assert key not in os.environ
-        
+
         os.environ[key] = value
         assert get_binary_env_var(key)
-        
+
     finally:
         if key in os.environ:
             del os.environ[key]
@@ -22,14 +23,14 @@ def test_get_binary_env_var_positive(value):
 def test_get_binary_env_var_negative(value):
     try:
         key = "SOME_ENVIRONMENT_VARIABLE_UNLIKELY_TO_BE_IN_ENVIRONMENT"
-        
+
         assert key not in os.environ
-        
+
         os.environ[key] = "True"
         assert get_binary_env_var(key)
         os.environ[key] = value
         assert not get_binary_env_var(key)
-        
+
     finally:
         if key in os.environ:
             del os.environ[key]
@@ -38,10 +39,10 @@ def test_get_binary_env_var_negative(value):
 def test_get_binary_env_var_missing():
     try:
         key = "SOME_ENVIRONMENT_VARIABLE_UNLIKELY_TO_BE_IN_ENVIRONMENT"
-        
+
         assert key not in os.environ
         assert not get_binary_env_var(key)
-        
+
     finally:
         if key in os.environ:
             del os.environ[key]
@@ -50,12 +51,12 @@ def test_get_binary_env_var_missing():
 def test_get_binary_env_var_wrong_value():
     try:
         key = "SOME_ENVIRONMENT_VARIABLE_UNLIKELY_TO_BE_IN_ENVIRONMENT"
-        
+
         assert key not in os.environ
         os.environ[key] = "smthwrong"
         with pytest.raises(ValueError):
             get_binary_env_var(key)
-        
+
     finally:
         if key in os.environ:
             del os.environ[key]
