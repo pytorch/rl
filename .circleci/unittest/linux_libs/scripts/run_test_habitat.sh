ad43e03b1ed13e89a2196b796451511ca7e19879
vmoens <vincentmoens@gmail.com>
2022-10-24 13:35:20 +0100

    amend

--- .circleci/unittest/linux_libs/scripts/run_test_habitat.sh
+++ .circleci/unittest/linux_libs/scripts/run_test_habitat.sh
@@ -15,14 +15,14 @@ root_dir="$(git rev-parse --show-toplevel)"
 env_dir="${root_dir}/env"
 lib_dir="${env_dir}/lib"
 
-## install habitat deps
-#yum install -y glfw
-#yum install -y glew
-#yum install -y mesa-libGL
-#yum install -y mesa-libGL-devel
-#yum install -y mesa-libOSMesa-devel
-#yum -y install egl-utils
-#yum -y install freeglut
+# install habitat deps
+yum install -y glfw
+yum install -y glew
+yum install -y mesa-libGL
+yum install -y mesa-libGL-devel
+yum install -y mesa-libOSMesa-devel
+yum -y install egl-utils
+yum -y install freeglut
 
 # If you already have an environment you want to use, you can just run the following:
 conda install habitat-sim withbullet headless -c conda-forge -c aihabitat-nightly -y
@@ -35,6 +35,11 @@ python -c "import habitat;import habitat.utils.gym_definitions"
 # solves ImportError: /lib64/libstdc++.so.6: version `GLIBCXX_3.4.21' not found
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$lib_dir
 export MKL_THREADING_LAYER=GNU
+# more logging
+export MAGNUM_LOG=verbose MAGNUM_GPU_VALIDATION=ON
+
+wget https://github.com/openai/mujoco-py/blob/master/vendor/10_nvidia.json
+mv 10_nvidia.json /usr/share/glvnd/egl_vendor.d/10_nvidia.json
 
 # this workflow only tests the libs
 python -c "import habitat;import habitat.utils.gym_definitions"
