a65c78ad71b7ee3a610065516648aa5b96fc8a60
vmoens <vincentmoens@gmail.com>
2022-10-24 15:14:19 +0100

    amend

--- .circleci/unittest/linux_libs/scripts/install_habitat.sh
+++ .circleci/unittest/linux_libs/scripts/install_habitat.sh
@@ -10,14 +10,6 @@ set -e
 eval "$(./conda/bin/conda shell.bash hook)"
 conda activate ./env
 
-#yum install -y glfw
-#yum install -y glew
-#yum install -y mesa-libGL
-#yum install -y mesa-libGL-devel
-#yum install -y mesa-libOSMesa-devel
-#yum -y install egl-utils
-#yum -y install freeglut
-
 # If you already have an environment you want to use, you can just run the following:
 conda install habitat-sim withbullet headless -c conda-forge -c aihabitat-nightly -y
 conda run python -m pip install install git+https://github.com/facebookresearch/habitat-lab.git#subdirectory=habitat-lab
