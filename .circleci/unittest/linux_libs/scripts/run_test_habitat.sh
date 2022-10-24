292d554831c392a6eb3e85bbbee11b75d0dacff2
vmoens <vincentmoens@gmail.com>
2022-10-24 13:26:28 +0100

    amend

--- .circleci/unittest/linux_libs/scripts/run_test_habitat.sh
+++ .circleci/unittest/linux_libs/scripts/run_test_habitat.sh
@@ -15,14 +15,14 @@ root_dir="$(git rev-parse --show-toplevel)"
 env_dir="${root_dir}/env"
 lib_dir="${env_dir}/lib"
 
-# install habitat deps
-yum install -y glfw
-yum install -y glew
-yum install -y mesa-libGL
-yum install -y mesa-libGL-devel
-yum install -y mesa-libOSMesa-devel
-yum -y install egl-utils
-yum -y install freeglut
+## install habitat deps
+#yum install -y glfw
+#yum install -y glew
+#yum install -y mesa-libGL
+#yum install -y mesa-libGL-devel
+#yum install -y mesa-libOSMesa-devel
+#yum -y install egl-utils
+#yum -y install freeglut
 
 # If you already have an environment you want to use, you can just run the following:
 conda install habitat-sim withbullet headless -c conda-forge -c aihabitat-nightly -y
@@ -38,4 +38,5 @@ export MKL_THREADING_LAYER=GNU
 
 # this workflow only tests the libs
 python -c "import habitat;import habitat.utils.gym_definitions"
-pytest test/test_libs.py --instafail -v --durations 20
+#pytest test/test_libs.py --instafail -v --durations 20
+python test/test_libs.py -k test_habitat
