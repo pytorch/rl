ee066b79890429b2287834b368a408f911e6176d
vmoens <vincentmoens@gmail.com>
2022-10-24 14:46:23 +0100

    amend

--- .circleci/unittest/linux_libs/scripts/setup_env.sh
+++ .circleci/unittest/linux_libs/scripts/setup_env.sh
@@ -10,7 +10,7 @@ set -e
 this_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 # Avoid error: "fatal: unsafe repository"
 apt-get update
-apt-get install -y git
+apt-get install -y git wget
 
 git config --global --add safe.directory '*'
 root_dir="$(git rev-parse --show-toplevel)"
