2930abce794f48320faf75d46c5a8f36d30cb981
vmoens <vincentmoens@gmail.com>
2022-10-25 13:59:17 +0100

    amend

--- .circleci/unittest/linux_libs/scripts_habitat/run_test.sh
+++ .circleci/unittest/linux_libs/scripts_habitat/run_test.sh
@@ -31,4 +31,5 @@ export MAGNUM_LOG=verbose MAGNUM_GPU_VALIDATION=ON
 # this workflow only tests the libs
 python -c "import habitat;import habitat.utils.gym_definitions"
 
-coverage run -m pytest test/test_libs.py --instafail -v --durations 20
+#coverage run -m pytest test/test_libs.py --instafail -v --durations 20
+coverage run -m python test/test_libs.py
