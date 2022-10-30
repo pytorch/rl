57d6679e338cac13caaac3b79e616c3ba3f408ed
vmoens <vincentmoens@gmail.com>
2022-10-30 07:01:49 +0000

    init

--- .circleci/unittest/linux_olddeps/scripts_gym_0_13/run_test.sh
+++ .circleci/unittest/linux_olddeps/scripts_gym_0_13/run_test.sh
@@ -22,7 +22,8 @@ export MKL_THREADING_LAYER=GNU
 
 coverage run -m pytest test/smoke_test.py -v --durations 20
 coverage run -m pytest test/smoke_test_deps.py -v --durations 20 -k 'test_gym or test_dm_control_pixels or test_dm_control'
-MUJOCO_GL=egl coverage run -m xvfb-run -a pytest --instafail -v --durations 20
+#MUJOCO_GL=egl coverage run -m xvfb-run -a pytest --instafail -v --durations 20
+MUJOCO_GL=egl coverage run -m pytest --instafail -v --durations 20
 #pytest --instafail -v --durations 20
 #python test/test_libs.py
 coverage xml -i
