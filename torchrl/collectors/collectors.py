3f4efdac3429196b97d56ea8c01891db685784f1
adityagandhamal <adityagandhamal1327@gmail.com>
2022-11-02 21:15:18 +0530

    run linting

--- torchrl/collectors/collectors.py
+++ torchrl/collectors/collectors.py
@@ -633,9 +633,8 @@ class SyncDataCollector(_DataCollector):
                 tensordict_out.append(self._tensordict.clone())
 
                 self._reset_if_necessary()
-                self._tensordict.update(
-                    step_mdp(self._tensordict), 
-                    inplace=True
+                self._tensordict.update(step_mdp(self._tensordict), 
+                inplace=True
                 )
             if self.return_in_place and len(self._tensordict_out.keys()) > 0:
                 tensordict_out = torch.stack(tensordict_out, len(self.env.batch_size))
