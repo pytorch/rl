d95e79f628b843f0933c51ae14f936c5ddff4336
vmoens <vincentmoens@gmail.com>
2022-11-11 11:11:15 +0000

    amend

--- test/test_libs.py
+++ test/test_libs.py
@@ -82,66 +82,85 @@ else:
         [True, False],
     ],
 )
-def test_gym(env_name, frame_skip, from_pixels, pixels_only):
-    if env_name == PONG_VERSIONED and not from_pixels:
-        raise pytest.skip("already pixel")
-    elif (
-        env_name != PONG_VERSIONED
-        and from_pixels
-        and (not torch.has_cuda or not torch.cuda.device_count())
-    ):
-        raise pytest.skip("no cuda device")
+class TestGym:
+    def test_gym(self, env_name, frame_skip, from_pixels, pixels_only):
+        if env_name == PONG_VERSIONED and not from_pixels:
+            raise pytest.skip("already pixel")
+        elif (
+            env_name != PONG_VERSIONED
+            and from_pixels
+            and (not torch.has_cuda or not torch.cuda.device_count())
+        ):
+            raise pytest.skip("no cuda device")
 
-    tdreset = []
-    tdrollout = []
-    final_seed = []
-    for _ in range(2):
-        env0 = GymEnv(
+        tdreset = []
+        tdrollout = []
+        final_seed = []
+        for _ in range(2):
+            env0 = GymEnv(
+                env_name,
+                frame_skip=frame_skip,
+                from_pixels=from_pixels,
+                pixels_only=pixels_only,
+            )
+            torch.manual_seed(0)
+            np.random.seed(0)
+            final_seed.append(env0.set_seed(0))
+            tdreset.append(env0.reset())
+            tdrollout.append(env0.rollout(max_steps=50))
+            assert env0.from_pixels is from_pixels
+            env0.close()
+            env_type = type(env0._env)
+            del env0
+
+        assert_allclose_td(*tdreset)
+        assert_allclose_td(*tdrollout)
+        final_seed0, final_seed1 = final_seed
+        assert final_seed0 == final_seed1
+
+        if env_name == PONG_VERSIONED:
+            base_env = gym.make(env_name, frameskip=frame_skip)
+            frame_skip = 1
+        else:
+            if gym_version < version.parse("0.26.0"):
+                base_env = gym.make(env_name)
+            else:
+                base_env = gym.make(env_name, render_mode="rgb_array")
+
+        if from_pixels and not _is_from_pixels(base_env):
+            base_env = PixelObservationWrapper(base_env, pixels_only=pixels_only)
+        assert type(base_env) is env_type
+        env1 = GymWrapper(base_env, frame_skip=frame_skip)
+        torch.manual_seed(0)
+        np.random.seed(0)
+        final_seed2 = env1.set_seed(0)
+        tdreset2 = env1.reset()
+        rollout2 = env1.rollout(max_steps=50)
+        assert env1.from_pixels is from_pixels
+        env1.close()
+        del env1, base_env
+
+        assert_allclose_td(tdreset[0], tdreset2, rtol=1e-4, atol=1e-4)
+        assert final_seed0 == final_seed2
+        assert_allclose_td(tdrollout[0], rollout2, rtol=1e-4, atol=1e-4)
+
+    def test_gym_fake_td(self, env_name, frame_skip, from_pixels, pixels_only):
+        if env_name == PONG_VERSIONED and not from_pixels:
+            raise pytest.skip("already pixel")
+        elif (
+            env_name != PONG_VERSIONED
+            and from_pixels
+            and (not torch.has_cuda or not torch.cuda.device_count())
+        ):
+            raise pytest.skip("no cuda device")
+
+        env = GymEnv(
             env_name,
             frame_skip=frame_skip,
             from_pixels=from_pixels,
             pixels_only=pixels_only,
         )
-        torch.manual_seed(0)
-        np.random.seed(0)
-        final_seed.append(env0.set_seed(0))
-        tdreset.append(env0.reset())
-        tdrollout.append(env0.rollout(max_steps=50))
-        assert env0.from_pixels is from_pixels
-        env0.close()
-        env_type = type(env0._env)
-        del env0
-
-    assert_allclose_td(*tdreset)
-    assert_allclose_td(*tdrollout)
-    final_seed0, final_seed1 = final_seed
-    assert final_seed0 == final_seed1
-
-    if env_name == PONG_VERSIONED:
-        base_env = gym.make(env_name, frameskip=frame_skip)
-        frame_skip = 1
-    else:
-        if gym_version < version.parse("0.26.0"):
-            base_env = gym.make(env_name)
-        else:
-            base_env = gym.make(env_name, render_mode="rgb_array")
-
-    if from_pixels and not _is_from_pixels(base_env):
-        base_env = PixelObservationWrapper(base_env, pixels_only=pixels_only)
-    assert type(base_env) is env_type
-    env1 = GymWrapper(base_env, frame_skip=frame_skip)
-    torch.manual_seed(0)
-    np.random.seed(0)
-    final_seed2 = env1.set_seed(0)
-    tdreset2 = env1.reset()
-    rollout2 = env1.rollout(max_steps=50)
-    assert env1.from_pixels is from_pixels
-    env1.close()
-    del env1, base_env
-
-    assert_allclose_td(tdreset[0], tdreset2, rtol=1e-4, atol=1e-4)
-    assert final_seed0 == final_seed2
-    assert_allclose_td(tdrollout[0], rollout2, rtol=1e-4, atol=1e-4)
+        _test_fake_tensordict(env)
 
 
 @pytest.mark.skipif(not _has_dmc, reason="no dm_control library found")
@@ -155,77 +174,91 @@ def test_gym(env_name, frame_skip, from_pixels, pixels_only):
         [False, False],
     ],
 )
-def test_dmcontrol(env_name, task, frame_skip, from_pixels, pixels_only):
-    if from_pixels and (not torch.has_cuda or not torch.cuda.device_count()):
-        raise pytest.skip("no cuda device")
+class TestDMControl:
+    def test_dmcontrol(self, env_name, task, frame_skip, from_pixels, pixels_only):
+        if from_pixels and (not torch.has_cuda or not torch.cuda.device_count()):
+            raise pytest.skip("no cuda device")
 
-    tds = []
-    tds_reset = []
-    final_seed = []
-    for _ in range(2):
-        env0 = DMControlEnv(
+        tds = []
+        tds_reset = []
+        final_seed = []
+        for _ in range(2):
+            env0 = DMControlEnv(
+                env_name,
+                task,
+                frame_skip=frame_skip,
+                from_pixels=from_pixels,
+                pixels_only=pixels_only,
+            )
+            torch.manual_seed(0)
+            np.random.seed(0)
+            final_seed0 = env0.set_seed(0)
+            tdreset0 = env0.reset()
+            rollout0 = env0.rollout(max_steps=50)
+            env0.close()
+            del env0
+            tds_reset.append(tdreset0)
+            tds.append(rollout0)
+            final_seed.append(final_seed0)
+
+        tdreset1, tdreset0 = tds_reset
+        rollout0, rollout1 = tds
+        final_seed0, final_seed1 = final_seed
+
+        assert_allclose_td(tdreset1, tdreset0)
+        assert final_seed0 == final_seed1
+        assert_allclose_td(rollout0, rollout1)
+
+        env1 = DMControlEnv(
             env_name,
             task,
             frame_skip=frame_skip,
             from_pixels=from_pixels,
             pixels_only=pixels_only,
         )
+        torch.manual_seed(1)
+        np.random.seed(1)
+        final_seed1 = env1.set_seed(1)
+        tdreset1 = env1.reset()
+        rollout1 = env1.rollout(max_steps=50)
+        env1.close()
+        del env1
+
+        with pytest.raises(AssertionError):
+            assert_allclose_td(tdreset1, tdreset0)
+            assert final_seed0 == final_seed1
+            assert_allclose_td(rollout0, rollout1)
+
+        base_env = suite.load(env_name, task)
+        if from_pixels:
+            render_kwargs = {"camera_id": 0}
+            base_env = pixels.Wrapper(
+                base_env, pixels_only=pixels_only, render_kwargs=render_kwargs
+            )
+        env2 = DMControlWrapper(base_env, frame_skip=frame_skip)
         torch.manual_seed(0)
         np.random.seed(0)
-        final_seed0 = env0.set_seed(0)
-        tdreset0 = env0.reset()
-        rollout0 = env0.rollout(max_steps=50)
-        env0.close()
-        del env0
-        tds_reset.append(tdreset0)
-        tds.append(rollout0)
-        final_seed.append(final_seed0)
+        final_seed2 = env2.set_seed(0)
+        tdreset2 = env2.reset()
+        rollout2 = env2.rollout(max_steps=50)
 
-    tdreset1, tdreset0 = tds_reset
-    rollout0, rollout1 = tds
-    final_seed0, final_seed1 = final_seed
+        assert_allclose_td(tdreset0, tdreset2)
+        assert final_seed0 == final_seed2
+        assert_allclose_td(rollout0, rollout2)
 
-    assert_allclose_td(tdreset1, tdreset0)
-    assert final_seed0 == final_seed1
-    assert_allclose_td(rollout0, rollout1)
+    def test_faketd(self, env_name, task, frame_skip, from_pixels, pixels_only):
+        if from_pixels and (
+            not torch.has_cuda or not torch.cuda.device_count()):
+            raise pytest.skip("no cuda device")
 
-    env1 = DMControlEnv(
-        env_name,
-        task,
-        frame_skip=frame_skip,
-        from_pixels=from_pixels,
-        pixels_only=pixels_only,
-    )
-    torch.manual_seed(1)
-    np.random.seed(1)
-    final_seed1 = env1.set_seed(1)
-    tdreset1 = env1.reset()
-    rollout1 = env1.rollout(max_steps=50)
-    env1.close()
-    del env1
-
-    with pytest.raises(AssertionError):
-        assert_allclose_td(tdreset1, tdreset0)
-        assert final_seed0 == final_seed1
-        assert_allclose_td(rollout0, rollout1)
-
-    base_env = suite.load(env_name, task)
-    if from_pixels:
-        render_kwargs = {"camera_id": 0}
-        base_env = pixels.Wrapper(
-            base_env, pixels_only=pixels_only, render_kwargs=render_kwargs
+        env = DMControlEnv(
+            env_name,
+            task,
+            frame_skip=frame_skip,
+            from_pixels=from_pixels,
+            pixels_only=pixels_only,
         )
-    env2 = DMControlWrapper(base_env, frame_skip=frame_skip)
-    torch.manual_seed(0)
-    np.random.seed(0)
-    final_seed2 = env2.set_seed(0)
-    tdreset2 = env2.reset()
-    rollout2 = env2.rollout(max_steps=50)
-
-    assert_allclose_td(tdreset0, tdreset2)
-    assert final_seed0 == final_seed2
-    assert_allclose_td(rollout0, rollout2)
-
+        _test_fake_tensordict(env)
 
 @pytest.mark.skipif(
     IS_OSX,
@@ -314,9 +347,7 @@ class TestCollectorLib:
 class TestHabitat:
     def test_habitat(self, envname):
         env = HabitatEnv(envname)
-        print([_env for _env in env.available_envs if _env.startswith("Habitat")])
         rollout = env.rollout(3)
-        print(rollout)
         _test_fake_tensordict(env)
 
 
