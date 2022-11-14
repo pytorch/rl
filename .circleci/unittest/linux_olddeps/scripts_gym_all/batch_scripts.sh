ecdfff01d9601e88dd58e8181d760bc2cec741ff
Sergey Ordinskiy <ordinskiy@fb.com>
2022-11-14 16:38:50 +0100

    amend

--- .circleci/unittest/linux_olddeps/scripts_gym_all/batch_scripts.sh
+++ .circleci/unittest/linux_olddeps/scripts_gym_all/batch_scripts.sh
@@ -16,17 +16,7 @@ $DIR/install.sh
 # Extracted from run_test.sh to run once.
 yum makecache && yum install libglvnd-devel mesa-libGL mesa-libGL-devel mesa-libEGL mesa-libEGL-devel glfw mesa-libOSMesa-devel glew glew-devel egl-utils freeglut xorg-x11-server-Xvfb -y
 
-# gym[atari]==[0.20;0.25] installs ale-py==0.8 by default, but this version is not compatible with gym<0.26
-# pip3 install ale-py==0.7
-
-#for GYM_VERSION in '0.13' '0.19' '0.20' '0.25' '0.26'
-#do
-#  echo "Installing gym version: ${GYM_VERSION}"
-#  pip3 install gym==$GYM_VERSION
-#  echo "Running tests for gym version: ${GYM_VERSION}"
-#  $DIR/run_test.sh
-#done
-
+# This version is installed initially (see environment.yml)
 for GYM_VERSION in '0.13'
 do
   echo "Installing gym version: ${GYM_VERSION}"
@@ -35,6 +25,7 @@ do
   $DIR/run_test.sh
 done
 
+# gym[atari]==0.19 is broken, so we install only gym without dependencies.
 for GYM_VERSION in '0.19'
 do
   echo "Installing gym version: ${GYM_VERSION}"
@@ -43,6 +34,7 @@ do
   $DIR/run_test.sh
 done
 
+# gym[atari]==0.20 installs ale-py==0.8, but this version is not compatible with gym<0.26, so we downgrade it.
 for GYM_VERSION in '0.20'
 do
   echo "Installing gym version: ${GYM_VERSION}"
@@ -52,9 +44,18 @@ do
   $DIR/run_test.sh
 done
 
-for GYM_VERSION in  '0.25' '0.26'
+for GYM_VERSION in '0.25'
 do
   echo "Installing gym version: ${GYM_VERSION}"
+  pip3 install 'gym[atari]'==$GYM_VERSION
+  echo "Running tests for gym version: ${GYM_VERSION}"
+  $DIR/run_test.sh
+done
+
+for GYM_VERSION in '0.26'
+do
+  echo "Installing gym version: ${GYM_VERSION}"
+  pip3 install 'gym[accept-rom-license]'==$GYM_VERSION
   pip3 install 'gym[atari]'==$GYM_VERSION
   echo "Running tests for gym version: ${GYM_VERSION}"
   $DIR/run_test.sh
