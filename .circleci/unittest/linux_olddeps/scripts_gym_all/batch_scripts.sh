5a11ca5052cb8bd3bd53674f2c836ca13fc5e850
Sergey Ordinskiy <ordinskiy@fb.com>
2022-11-14 17:13:58 +0100

    amend

--- .circleci/unittest/linux_olddeps/scripts_gym_all/batch_scripts.sh
+++ .circleci/unittest/linux_olddeps/scripts_gym_all/batch_scripts.sh
@@ -19,44 +19,40 @@ yum makecache && yum install libglvnd-devel mesa-libGL mesa-libGL-devel mesa-lib
 # This version is installed initially (see environment.yml)
 for GYM_VERSION in '0.13'
 do
-  echo "Installing gym version: ${GYM_VERSION}"
+  echo "Testing gym version: ${GYM_VERSION}"
   pip3 install 'gym[atari]'==$GYM_VERSION
-  echo "Running tests for gym version: ${GYM_VERSION}"
   $DIR/run_test.sh
 done
 
 # gym[atari]==0.19 is broken, so we install only gym without dependencies.
 for GYM_VERSION in '0.19'
 do
-  echo "Installing gym version: ${GYM_VERSION}"
+  echo "Testing gym version: ${GYM_VERSION}"
   pip3 install gym==$GYM_VERSION
-  echo "Running tests for gym version: ${GYM_VERSION}"
   $DIR/run_test.sh
 done
 
 # gym[atari]==0.20 installs ale-py==0.8, but this version is not compatible with gym<0.26, so we downgrade it.
 for GYM_VERSION in '0.20'
 do
-  echo "Installing gym version: ${GYM_VERSION}"
+  echo "Testing gym version: ${GYM_VERSION}"
   pip3 install 'gym[atari]'==$GYM_VERSION
   pip3 install ale-py==0.7
-  echo "Running tests for gym version: ${GYM_VERSION}"
   $DIR/run_test.sh
 done
 
 for GYM_VERSION in '0.25'
 do
-  echo "Installing gym version: ${GYM_VERSION}"
+  echo "Testing gym version: ${GYM_VERSION}"
   pip3 install 'gym[atari]'==$GYM_VERSION
-  echo "Running tests for gym version: ${GYM_VERSION}"
   $DIR/run_test.sh
 done
 
+# For this version "gym[accept-rom-license]" is required.
 for GYM_VERSION in '0.26'
 do
-  echo "Installing gym version: ${GYM_VERSION}"
+  echo "Testing gym version: ${GYM_VERSION}"
   pip3 install 'gym[accept-rom-license]'==$GYM_VERSION
   pip3 install 'gym[atari]'==$GYM_VERSION
-  echo "Running tests for gym version: ${GYM_VERSION}"
   $DIR/run_test.sh
 done
