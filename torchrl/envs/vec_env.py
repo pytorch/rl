594a9a4de85212b069b4ff7f5fcbd9e42a3f16a9
vmoens <vincentmoens@gmail.com>
2022-11-15 10:20:02 +0000

    strict=False

--- torchrl/envs/vec_env.py
+++ torchrl/envs/vec_env.py
@@ -989,7 +989,7 @@ def _run_worker_pipe_shared_mem(
                 step_keys = set(_td.keys()) - set(env_input_keys)
             if pin_memory:
                 _td.pin_memory()
-            tensordict.update_(_td.select(*step_keys))
+            tensordict.update_(_td.select(*step_keys, strict=False))
             if _td.get("done"):
                 msg = "done"
             else:
