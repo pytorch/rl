51757baf389cdd331330614992cdd9895d2c42c2
Ying-Chen Lin <yclin@fb.com>
2022-11-18 23:06:44 +0000

    refactor

--- torchrl/envs/libs/jumanji.py
+++ torchrl/envs/libs/jumanji.py
@@ -2,7 +2,8 @@ from typing import Optional, Dict, Union
 
 import numpy as np
 import torch
-from tensordict.tensordict import TensorDict, TensorDictBase
+from tensordict.tensordict import TensorDict, TensorDictBase, make_tensordict
+import dataclasses
 
 from torchrl.data import (
     DEVICE_TYPING,
@@ -26,6 +27,47 @@ try:
 except ImportError as err:
     _has_jumanji = False
     IMPORT_ERR = str(err)
+
+
+def _ndarray_to_tensor(value, device) -> torch.Tensor:
+    value = np.asarray(value)
+    if value.dtype == np.uint16:
+        value = value.astype(np.int16)
+    elif value.dtype == np.uint32:
+        value = value.astype(np.int32)
+    elif value.dtype == np.uint64:
+        value = value.astype(np.int64)
+    return torch.tensor(value).to(device)
+
+
+def _object_to_tensordict(obj, device, batch_size) -> TensorDictBase:
+    t = {}
+    if isinstance(obj, tuple) and hasattr(obj, "_fields"):
+        _iter = obj._fields
+    elif dataclasses.is_dataclass(obj):
+        _iter = (field.name for field in dataclasses.fields(obj))
+    else:
+        raise NotImplementedError(f"unsupported data type {type(obj)}")
+    for name in _iter:
+        value = getattr(obj, name)
+        if isinstance(value, (list, jnp.ndarray, np.ndarray)):
+            t[name] = _ndarray_to_tensor(value, device=device)
+        else:
+            t[name] = _object_to_tensordict(value, device, batch_size)
+    return make_tensordict(**t, device=device, batch_size=batch_size)
+
+
+def _tensordict_to_object(tensordict: TensorDictBase, object_example):
+    object_type = type(object_example)
+    t = {}
+    for name in tensordict.keys():
+        value = tensordict[name]
+        if isinstance(value, TensorDictBase):
+            t[name] = _tensordict_to_object(value, getattr(object_example, name))
+        else:
+            value_example = getattr(object_example, name)
+            t[name] = value.detach().numpy().reshape(value_example.shape).astype(value_example.dtype)
+    return object_type(**t)
 
 
 def _jumanji_to_torchrl_spec_transform(
@@ -80,135 +122,22 @@ def _jumanji_to_torchrl_spec_transform(
         raise TypeError(f"Unsupported spec type {type(spec)}")
 
 
-def _data_to_spec_transform(state) -> TensorSpec:
-    if isinstance(state, torch.Tensor):
-        if state.dtype in (torch.float, torch.double, torch.half):
+def _torchrl_data_to_spec_transform(data) -> TensorSpec:
+    if isinstance(data, torch.Tensor):
+        if data.dtype in (torch.float, torch.double, torch.half):
             return NdUnboundedContinuousTensorSpec(
-                shape=state.shape, dtype=state.dtype, device=state.device
+                shape=data.shape, dtype=data.dtype, device=data.device
             )
         else:
             return NdUnboundedDiscreteTensorSpec(
-                shape=state.shape, dtype=state.dtype, device=state.device
+                shape=data.shape, dtype=data.dtype, device=data.device
             )
-    elif isinstance(state, TensorDict):
+    elif isinstance(data, TensorDict):
         return CompositeSpec(
-            **{key: _data_to_spec_transform(value) for key, value in state.items()}
+            **{key: _torchrl_data_to_spec_transform(value) for key, value in data.items()}
         )
     else:
-        raise TypeError(f"Unsupported state type {type(state)}")
-
-
-def _jumanji_to_torchrl_data_transform(val, device, batch_size):
-    if isinstance(val, (jax.Array, np.ndarray)):
-        if isinstance(val, jax.Array):
-            val = np.array(val)
-        if val.dtype == np.uint16:
-            val = val.astype(np.int16)
-        if val.dtype == np.uint32:
-            val = val.astype(np.int32)
-        if val.dtype == np.uint64:
-            val = val.astype(np.int64)
-        return torch.tensor(val, device=device)
-    elif isinstance(val, tuple) and hasattr(val, "_fields"):  # named tuples
-        return TensorDict(
-            {
-                k: _jumanji_to_torchrl_data_transform(v, device, batch_size)
-                for k, v in zip(val._fields, val)
-            },
-            device=device,
-            batch_size=batch_size,
-        )
-    elif hasattr(val, "__dict__"):
-        return TensorDict(
-            {
-                k: _jumanji_to_torchrl_data_transform(v, device, batch_size)
-                for k, v in val.__dict__.items()
-            },
-            device=device,
-            batch_size=batch_size,
-        )
-    else:
-        raise TypeError(f"Unsupported data type {type(val)}")
-
-
-def _torchrl_to_jumanji_state_transform(tensordict: TensorDict, env):
-    if isinstance(env, jumanji.environments.games.snake.env.Snake):
-        return jumanji.environments.games.snake.types.State(
-            key=tensordict.get("key").numpy().astype(np.uint32),
-            body_state=tensordict.get("body_state").numpy(),
-            head_pos=jumanji.environments.games.snake.types.Position(
-                tensordict.get("head_pos").get("row").squeeze(-1).numpy(),
-                tensordict.get("head_pos").get("col").squeeze(-1).numpy(),
-            ),
-            fruit_pos=jumanji.environments.games.snake.types.Position(
-                tensordict.get("fruit_pos").get("row").squeeze(-1).numpy(),
-                tensordict.get("fruit_pos").get("col").squeeze(-1).numpy(),
-            ),
-            length=tensordict.get("length").squeeze(-1).numpy(),
-            step=tensordict.get("step").squeeze(-1).numpy(),
-        )
-    if isinstance(env, jumanji.environments.games.connect4.env.Connect4):
-        return jumanji.environments.games.connect4.types.State(
-            current_player=tensordict.get("current_player").squeeze(-1).numpy(),
-            board=tensordict.get("board").numpy(),
-        )
-    if isinstance(env, jumanji.environments.combinatorial.tsp.env.TSP):
-        return jumanji.environments.combinatorial.tsp.types.State(
-            problem=tensordict.get("problem").numpy(),
-            position=tensordict.get("position").squeeze(-1).numpy(),
-            visited_mask=tensordict.get("visited_mask").numpy(),
-            order=tensordict.get("order").numpy(),
-            num_visited=tensordict.get("num_visited").squeeze(-1).numpy(),
-        )
-    if isinstance(env, jumanji.environments.combinatorial.knapsack.env.Knapsack):
-        return jumanji.environments.combinatorial.knapsack.types.State(
-            problem=tensordict.get("problem").numpy(),
-            last_item=tensordict.get("last_item").squeeze(-1).numpy(),
-            first_item=tensordict.get("first_item").squeeze(-1).numpy(),
-            used_mask=tensordict.get("used_mask").numpy(),
-            num_steps=tensordict.get("num_steps").squeeze(-1).numpy(),
-            remaining_budget=tensordict.get("remaining_budget").squeeze(-1).numpy(),
-        )
-    if isinstance(env, jumanji.environments.combinatorial.binpack.env.BinPack):
-
-        def get_fields(tensordict, fields):
-            return {key: tensordict.get(key).numpy() for key in fields}
-
-        def get_squeezed_fields(tensordict, fields):
-            return {key: tensordict.get(key).squeeze(-1).numpy() for key in fields}
-
-        return jumanji.environments.combinatorial.binpack.types.State(
-            container=jumanji.environments.combinatorial.binpack.space.Space(
-                **get_squeezed_fields(
-                    tensordict.get("container"), ["x1", "x2", "y1", "y2", "z1", "z2"]
-                )
-            ),
-            ems=jumanji.environments.combinatorial.binpack.space.Space(
-                **get_fields(
-                    tensordict.get("ems"), ["x1", "x2", "y1", "y2", "z1", "z2"]
-                )
-            ),
-            ems_mask=tensordict.get("ems_mask").numpy(),
-            items=jumanji.environments.combinatorial.binpack.types.Item(
-                **get_fields(tensordict.get("items"), ["x_len", "y_len", "z_len"])
-            ),
-            items_mask=tensordict.get("items_mask").numpy(),
-            items_placed=tensordict.get("items_placed").numpy(),
-            items_location=jumanji.environments.combinatorial.binpack.types.Location(
-                **get_fields(tensordict.get("items_location"), ["x", "y", "z"])
-            ),
-            action_mask=tensordict.get("action_mask").numpy(),
-            sorted_ems_indexes=tensordict.get("sorted_ems_indexes").numpy(),
-            key=tensordict.get("key").numpy(),
-        )
-    if isinstance(env, jumanji.environments.combinatorial.routing.env.Routing):
-        return jumanji.environments.combinatorial.routing.types.State(
-            key=tensordict.get("key").numpy().astype(np.uint32),
-            grid=tensordict.get("grid").numpy(),
-            step=tensordict.get("step").squeeze(-1).numpy(),
-            finished_agents=tensordict.get("finished_agents").numpy(),
-        )
-    raise NotImplementedError(f"Unsupported Jumanji environment {type(env)}")
+        raise TypeError(f"Unsupported data type {type(data)}")
 
 
 class JumanjiWrapper(GymLikeEnv):
@@ -252,15 +181,17 @@ class JumanjiWrapper(GymLikeEnv):
             raise NotImplementedError("TODO")
         return env
 
-    def _make_state_spec(self, env) -> TensorSpec:
+    def _make_state_example(self, env):
         # generate a sample state object to build state spec from.
         key = jax.random.PRNGKey(0)
-        state, _ = env.reset(key)
+        keys = jax.random.split(key, self.numel())
+        state, _ = jax.vmap(self._env.reset)(jnp.stack(keys))
+        state = self._reshape(state)
+        return state
 
-        state_dict = _jumanji_to_torchrl_data_transform(
-            state, self.device, batch_size=()
-        )
-        state_spec = _data_to_spec_transform(state_dict)
+    def _make_state_spec(self, state) -> TensorSpec:
+        state_dict = _object_to_tensordict(state, self.device, self.batch_size)
+        state_spec = _torchrl_data_to_spec_transform(state_dict)
         return state_spec
 
     def _make_input_spec(self, env) -> TensorSpec:
@@ -284,13 +215,17 @@ class JumanjiWrapper(GymLikeEnv):
         return _jumanji_to_torchrl_spec_transform(env.reward_spec(), device=self.device)
 
     def _make_specs(self, env: "jumanji.env.Environment") -> None:  # noqa: F821
+
+        # extract spec from jumanji definition
         self._input_spec = self._make_input_spec(env)
         self._observation_spec = self._make_observation_spec(env)
         self._reward_spec = self._make_reward_spec(env)
 
-        state_spec = self._make_state_spec(env)
-        self._input_spec["state"] = state_spec
-        self._observation_spec["state"] = state_spec
+        # extract state spec from instance
+        self._state_example = self._make_state_example(env)
+        self._state_spec = self._make_state_spec(self._state_example)
+        self._input_spec["state"] = self._state_spec
+        self._observation_spec["state"] = self._state_spec
 
     def _check_kwargs(self, kwargs: Dict):
         if "env" not in kwargs:
@@ -308,30 +243,35 @@ class JumanjiWrapper(GymLikeEnv):
         self.key = jax.random.PRNGKey(seed)
 
     def read_state(self, state):
-        state = _jumanji_to_torchrl_data_transform(
-            state, device=self.device, batch_size=self.batch_size
-        )
-        state = self.input_spec["state"].encode(state)
-        return state
+        state_dict = _object_to_tensordict(state, self.device, self.batch_size)
+        return self._state_spec.encode(state_dict)
 
     def read_obs(self, obs):
-        obs = _jumanji_to_torchrl_data_transform(
-            obs, device=self.device, batch_size=self.batch_size
-        )
-        return super().read_obs(obs)
+        if isinstance(obs, (list, jnp.ndarray, np.ndarray)):
+            obs_dict = _ndarray_to_tensor(obs, self.device)
+        else:
+            obs_dict = _object_to_tensordict(obs, self.device, self.batch_size)
+        return super().read_obs(obs_dict)
 
     def _step(self, tensordict: TensorDictBase) -> TensorDictBase:
 
-        state = _torchrl_to_jumanji_state_transform(tensordict.get("state"), self._env)
+        # prepare inputs
+        state = _tensordict_to_object(tensordict.get("state"), self._state_example)
         action = self.read_action(tensordict.get("action"))
         reward = self.reward_spec.zero(self.batch_size)
 
+        # flatten batch size into vector
         state = self._flatten(state)
         action = self._flatten(action)
+
+        # jax vectorizing map on env.step
         state, timestep = jax.vmap(self._env.step)(state, action)
+
+        # reshape batch size from vector
         state = self._reshape(state)
         timestep = self._reshape(timestep)
 
+        # collect outputs
         state_dict = self.read_state(state)
         obs_dict = self.read_obs(timestep.observation)
         reward = self.read_reward(reward, np.asarray(timestep.reward))
@@ -341,6 +281,7 @@ class JumanjiWrapper(GymLikeEnv):
 
         self._is_done = done
 
+        # build results
         tensordict_out = TensorDict(
             source=obs_dict,
             batch_size=tensordict.batch_size,
@@ -356,17 +297,24 @@ class JumanjiWrapper(GymLikeEnv):
         self, tensordict: Optional[TensorDictBase] = None, **kwargs
     ) -> TensorDictBase:
 
+        # generate random keys
         self.key, *keys = jax.random.split(self.key, self.numel() + 1)
+
+        # jax vectorizing map on env.reset
         state, timestep = jax.vmap(self._env.reset)(jnp.stack(keys))
+
+        # reshape batch size from vector
         state = self._reshape(state)
         timestep = self._reshape(timestep)
 
+        # collect outputs
         state_dict = self.read_state(state)
         obs_dict = self.read_obs(timestep.observation)
         done = torch.zeros(self.batch_size, dtype=torch.bool)
 
         self._is_done = done
 
+        # build results
         tensordict_out = TensorDict(
             source=obs_dict,
             batch_size=self.batch_size,
