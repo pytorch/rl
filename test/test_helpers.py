9a81a97f4165bc16d8e9691230adaef85dcc87f0
albertbou92 <albertbou92@users.noreply.github.com>
2022-11-23 12:23:41 +0100

    [Feature] A2C objective class and train example (#680)
    * a2c
    
    * a2c
    
    * a2c config
    
    * a2c config
    
    * fix imports
    
    * latest
    
    * simplified config
    
    * simplified config
    
    * Update config.yaml
    
    * [BugFix] Use GitHub for flake8 pre-commit hook (#679)
    
    * [BugFix] Update to strict select (#675)
    
    * init
    
    * strict=False
    
    * amend
    
    * amend
    
    * [Feature] Auto-compute stats for ObservationNorm (#669)
    
    * Add auto-compute stats feature for ObservationNorm
    
    * Fix issue in ObservNorm init function
    
    * Quick refactor of ObservationNorm init method
    
    * Minor refactoring and adding more tests for ObservationNorm
    
    * lint
    
    * docstring
    
    * docstring
    
    Co-authored-by: vmoens <vincentmoens@gmail.com>
    
    * [Doc] _make_collector helper function (#678)
    
    * [Doc] BatchSubSampler class docstrings example (#677)
    
    * [BugFix] PPO objective crashes if advantage_module is None (#676)
    
    * Minor: lint
    
    * [Refactor] Refactor 'next_' into nested tensordicts (#649)
    
    * init
    
    * [Feature] Nested composite spec (#654)
    
    * [Feature] Move `transform.forward` to `transform.step` (#660)
    
    * transform step function
    
    * amend
    
    * amend
    
    * amend
    
    * amend
    
    * amend
    
    * fixing key names
    
    * fixing key names
    
    * [Refactor] Transform next remove (#661)
    
    * Refactor "next_" into ("next", ) (#673)
    
    * amend
    
    * amend
    
    * bugfix
    
    * init
    
    * strict=False
    
    * strict=False
    
    * minor
    
    * amend
    
    * [BugFix] Use GitHub for flake8 pre-commit hook (#679)
    
    * amend
    
    * [BugFix] Update to strict select (#675)
    
    * init
    
    * strict=False
    
    * amend
    
    * amend
    
    * [Feature] Auto-compute stats for ObservationNorm (#669)
    
    * Add auto-compute stats feature for ObservationNorm
    
    * Fix issue in ObservNorm init function
    
    * Quick refactor of ObservationNorm init method
    
    * Minor refactoring and adding more tests for ObservationNorm
    
    * lint
    
    * docstring
    
    * docstring
    
    Co-authored-by: vmoens <vincentmoens@gmail.com>
    
    * amend
    
    * amend
    
    * lint
    
    * bf
    
    * bf
    
    * amend
    
    Co-authored-by: Romain Julien <romainjulien@fb.com>
    
    Co-authored-by: Romain Julien <romainjulien@fb.com>
    
    * adapted to nested next td
    
    * [Refactor] Refactor 'next_' into nested tensordicts (#649)
    
    * init
    
    * [Feature] Nested composite spec (#654)
    
    * [Feature] Move `transform.forward` to `transform.step` (#660)
    
    * transform step function
    
    * amend
    
    * amend
    
    * amend
    
    * amend
    
    * amend
    
    * fixing key names
    
    * fixing key names
    
    * [Refactor] Transform next remove (#661)
    
    * Refactor "next_" into ("next", ) (#673)
    
    * amend
    
    * amend
    
    * bugfix
    
    * init
    
    * strict=False
    
    * strict=False
    
    * minor
    
    * amend
    
    * [BugFix] Use GitHub for flake8 pre-commit hook (#679)
    
    * amend
    
    * [BugFix] Update to strict select (#675)
    
    * init
    
    * strict=False
    
    * amend
    
    * amend
    
    * [Feature] Auto-compute stats for ObservationNorm (#669)
    
    * Add auto-compute stats feature for ObservationNorm
    
    * Fix issue in ObservNorm init function
    
    * Quick refactor of ObservationNorm init method
    
    * Minor refactoring and adding more tests for ObservationNorm
    
    * lint
    
    * docstring
    
    * docstring
    
    Co-authored-by: vmoens <vincentmoens@gmail.com>
    
    * amend
    
    * amend
    
    * lint
    
    * bf
    
    * bf
    
    * amend
    
    Co-authored-by: Romain Julien <romainjulien@fb.com>
    
    Co-authored-by: Romain Julien <romainjulien@fb.com>
    
    * [Doc] More doc about environments (#683)
    
    * amend
    
    * amend
    
    * amend
    
    * amend
    
    * amend
    
    * amend
    
    * [Doc] Fix missing tensordict install for doc (#685)
    
    * model config fix
    
    * formatting
    
    * formatting
    
    * a2c runtime error comment change
    
    * a2c test
    
    * a2c test
    
    * a2c test
    
    * make a2c model test
    
    * increase a2c tests coverage
    
    * formatting
    
    * fix bug a2c testing
    
    * minor fixes
    
    Co-authored-by: Vincent Moens <vincentmoens@gmail.com>
    Co-authored-by: Romain Julien <romainjulien@fb.com>

--- test/test_helpers.py
+++ test/test_helpers.py
@@ -32,19 +32,22 @@ from torchrl.envs.utils import set_exploration_mode
 from torchrl.modules.tensordict_module.common import _has_functorch
 from torchrl.trainers.helpers import transformed_env_constructor
 from torchrl.trainers.helpers.envs import EnvConfig
+from torchrl.trainers.helpers.losses import A2CLossConfig, make_a2c_loss
 from torchrl.trainers.helpers.models import (
+    A2CModelConfig,
     DDPGModelConfig,
     DiscreteModelConfig,
+    DreamerConfig,
+    make_a2c_model,
     make_ddpg_actor,
     make_dqn_actor,
+    make_dreamer,
     make_ppo_model,
     make_redq_model,
     make_sac_model,
     PPOModelConfig,
     REDQModelConfig,
     SACModelConfig,
-    DreamerConfig,
-    make_dreamer,
 )
 
 TORCH_VERSION = version.parse(torch.__version__)
@@ -353,6 +356,138 @@ def test_ppo_maker(device, from_pixels, shared_mapping, gsde, exploration):
             raise
         proof_environment.close()
         del proof_environment
+
+
+@pytest.mark.skipif(not _has_hydra, reason="No hydra library found")
+@pytest.mark.skipif(not _has_gym, reason="No gym library found")
+@pytest.mark.parametrize("device", get_available_devices())
+@pytest.mark.parametrize("from_pixels", [tuple(), ("from_pixels=True", "catframes=4")])
+@pytest.mark.parametrize("gsde", [tuple(), ("gSDE=True",)])
+@pytest.mark.parametrize("shared_mapping", [tuple(), ("shared_mapping=True",)])
+@pytest.mark.parametrize("exploration", ["random", "mode"])
+def test_a2c_maker(device, from_pixels, shared_mapping, gsde, exploration):
+    A2CModelConfig.advantage_in_loss = False
+    if not gsde and exploration != "random":
+        pytest.skip("no need to test this setting")
+    flags = list(from_pixels + shared_mapping + gsde)
+    config_fields = [
+        (config_field.name, config_field.type, config_field)
+        for config_cls in (
+            EnvConfig,
+            A2CLossConfig,
+            A2CModelConfig,
+        )
+        for config_field in dataclasses.fields(config_cls)
+    ]
+
+    Config = dataclasses.make_dataclass(cls_name="Config", fields=config_fields)
+    cs = ConfigStore.instance()
+    cs.store(name="config", node=Config)
+
+    with initialize(version_base=None, config_path=None):
+        cfg = compose(config_name="config", overrides=flags)
+        # if gsde and from_pixels:
+        #     pytest.skip("gsde and from_pixels are incompatible")
+
+        env_maker = (
+            ContinuousActionConvMockEnvNumpy
+            if from_pixels
+            else ContinuousActionVecMockEnv
+        )
+        env_maker = transformed_env_constructor(
+            cfg, use_env_creator=False, custom_env_maker=env_maker
+        )
+        proof_environment = env_maker()
+
+        if cfg.from_pixels and not cfg.shared_mapping:
+            with pytest.raises(
+                RuntimeError,
+                match="A2C learnt from pixels require the shared_mapping to be set to True",
+            ):
+                actor_value = make_a2c_model(
+                    proof_environment,
+                    device=device,
+                    cfg=cfg,
+                )
+            return
+
+        actor_value = make_a2c_model(
+            proof_environment,
+            device=device,
+            cfg=cfg,
+        )
+        actor = actor_value.get_policy_operator()
+        expected_keys = [
+            "done",
+            "pixels" if len(from_pixels) else "observation_vector",
+            "pixels_orig" if len(from_pixels) else "observation_orig",
+            "action",
+            "sample_log_prob",
+            "loc",
+            "scale",
+        ]
+        if shared_mapping:
+            expected_keys += ["hidden"]
+        if len(gsde):
+            expected_keys += ["_eps_gSDE"]
+
+        td = proof_environment.reset().to(device)
+        td_clone = td.clone()
+        with set_exploration_mode(exploration):
+            if UNSQUEEZE_SINGLETON and not td_clone.ndimension():
+                # Linear and conv used to break for non-batched data
+                actor(td_clone.unsqueeze(0))
+            else:
+                actor(td_clone)
+
+        try:
+            _assert_keys_match(td_clone, expected_keys)
+        except AssertionError:
+            proof_environment.close()
+            raise
+
+        if cfg.gSDE:
+            if cfg.shared_mapping:
+                tsf_loc = actor[-1].module[-1].module.transform(td_clone.get("loc"))
+            else:
+                tsf_loc = actor.module[-1].module.transform(td_clone.get("loc"))
+
+            if exploration == "random":
+                with pytest.raises(AssertionError):
+                    torch.testing.assert_close(td_clone.get("action"), tsf_loc)
+            else:
+                torch.testing.assert_close(td_clone.get("action"), tsf_loc)
+
+        value = actor_value.get_value_operator()
+        expected_keys = [
+            "done",
+            "pixels" if len(from_pixels) else "observation_vector",
+            "pixels_orig" if len(from_pixels) else "observation_orig",
+            "state_value",
+        ]
+        if shared_mapping:
+            expected_keys += ["hidden"]
+        if len(gsde):
+            expected_keys += ["_eps_gSDE"]
+
+        td_clone = td.clone()
+        if UNSQUEEZE_SINGLETON and not td_clone.ndimension():
+            # Linear and conv used to break for non-batched data
+            value(td_clone.unsqueeze(0))
+        else:
+            value(td_clone)
+        try:
+            _assert_keys_match(td_clone, expected_keys)
+        except AssertionError:
+            proof_environment.close()
+            raise
+        proof_environment.close()
+        del proof_environment
+
+        cfg.advantage_in_loss = False
+        loss_fn = make_a2c_loss(actor_value, cfg)
+        cfg.advantage_in_loss = True
+        loss_fn = make_a2c_loss(actor_value, cfg)
 
 
 @pytest.mark.skipif(not _has_hydra, reason="No hydra library found")
