23e3d85ae732c217f97bf821d3a1d82881cbfd1b
vmoens <vincentmoens@gmail.com>
2022-11-25 07:55:20 +0000

    amend

--- tutorials/sphinx-tutorials/torch_envs.py
+++ tutorials/sphinx-tutorials/torch_envs.py
@@ -639,9 +639,9 @@ from torchrl.envs.transforms import VecNorm, TransformedEnv
 
 make_env = EnvCreator(lambda: TransformedEnv(GymEnv("CartPole-v1"), VecNorm(decay=1.0)))
 env = ParallelEnv(3, make_env)
-make_env.state_dict()["_extra_state"]["td"]["next", "observation_count"].fill_(0.0)
-make_env.state_dict()["_extra_state"]["td"]["next", "observation_ssq"].fill_(0.0)
-make_env.state_dict()["_extra_state"]["td"]["next", "observation_sum"].fill_(0.0)
+make_env.state_dict()["_extra_state"]["td"]["observation_count"].fill_(0.0)
+make_env.state_dict()["_extra_state"]["td"]["observation_ssq"].fill_(0.0)
+make_env.state_dict()["_extra_state"]["td"]["observation_sum"].fill_(0.0)
 
 tensordict = env.rollout(max_steps=5)
 
