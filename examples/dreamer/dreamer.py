5b697e0e0bb986cdd4f96970ff0d927e5c902dfa
Romain Julien <romainjulien@fb.com>
2022-12-01 11:04:56 +0000

    lint

--- examples/dreamer/dreamer.py
+++ examples/dreamer/dreamer.py
@@ -117,9 +117,8 @@ def main(cfg: "DictConfig"):  # noqa: F821
     if not cfg.vecnorm and cfg.norm_stats:
         key = ("next", "pixels") if cfg.from_pixels else ("next", "observation_vector")
         _, stats = generate_stats_from_observation_norms(
-            cfg,
-            proof_environment=transformed_env_constructor(cfg)(),
-            key=key)[0]
+            cfg, proof_environment=transformed_env_constructor(cfg)(), key=key
+        )[0]
     elif cfg.from_pixels:
         stats = {"loc": 0.5, "scale": 0.5}
     else:
