535c2bdd1b12ccea9da6bbd8ba7cf5404e92c50d
albert bou <albertbou92@gmail.com>
2022-12-02 08:30:00 +0100

    format

--- torchrl/trainers/helpers/losses.py
+++ torchrl/trainers/helpers/losses.py
@@ -3,7 +3,7 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-from dataclasses import asdict, dataclass
+from dataclasses import dataclass
 from typing import Any, Optional, Tuple
 
 from torchrl.modules import ActorCriticOperator, ActorValueOperator
