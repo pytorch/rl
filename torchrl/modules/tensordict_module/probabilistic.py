306873fb73c0d2ef796120a06d55e476a2278641
Tom Begley <tomcbegley@gmail.com>
2022-12-07 16:24:40 +0000

    Add docstrings

--- torchrl/modules/tensordict_module/probabilistic.py
+++ torchrl/modules/tensordict_module/probabilistic.py
@@ -6,22 +6,93 @@
 import warnings
 from typing import Optional, Sequence, Type, Union
 
-from tensordict import TensorDictBase
 from tensordict.nn import TensorDictModule
 from tensordict.nn.prototype import (
     ProbabilisticTensorDictModule,
     ProbabilisticTensorDictSequential,
 )
+from tensordict.tensordict import TensorDictBase
 
 from torchrl.data import CompositeSpec, TensorSpec
+from torchrl.modules.distributions import Delta
 from torchrl.modules.tensordict_module.common import _forward_hook_safe_action
 from torchrl.modules.tensordict_module.sequence import SafeSequential
-from torchrl.modules.distributions import Delta
 
 
-class SafeProbabilisticModule(
-    ProbabilisticTensorDictModule,
-):
+class SafeProbabilisticModule(ProbabilisticTensorDictModule):
+    """A :obj:``SafeProbabilisticModule`` is an :obj:``tensordict.nn.prototype.ProbabilisticTensorDictModule`` subclass that accepts a :obj:``TensorSpec`` as argument to control the output domain.
+
+    `SafeProbabilisticModule` is a non-parametric module representing a
+    probability distribution. It reads the distribution parameters from an input
+    TensorDict using the specified `in_keys`. The output is sampled given some rule,
+    specified by the input :obj:`default_interaction_mode` argument and the
+    :obj:`interaction_mode()` global function.
+
+    :obj:`SafeProbabilisticModule` can be used to construct the distribution
+    (through the :obj:`get_dist()` method) and/or sampling from this distribution
+    (through a regular :obj:`__call__()` to the module).
+
+    A :obj:`SafeProbabilisticModule` instance has two main features:
+    - It reads and writes TensorDict objects
+    - It uses a real mapping R^n -> R^m to create a distribution in R^d from
+    which values can be sampled or computed.
+
+    When the :obj:`__call__` / :obj:`forward` method is called, a distribution is
+    created, and a value computed (using the 'mean', 'mode', 'median' attribute or
+    the 'rsample', 'sample' method). The sampling step is skipped if the supplied
+    TensorDict has all of the desired key-value pairs already.
+
+    By default, SafeProbabilisticModule distribution class is a Delta
+    distribution, making SafeProbabilisticModule a simple wrapper around
+    a deterministic mapping function.
+
+    Args:
+        in_keys (str or iterable of str or dict): key(s) that will be read from the
+            input TensorDict and used to build the distribution. Importantly, if it's an
+            iterable of string or a string, those keys must match the keywords used by
+            the distribution class of interest, e.g. :obj:`"loc"` and :obj:`"scale"` for
+            the Normal distribution and similar. If in_keys is a dictionary,, the keys
+            are the keys of the distribution and the values are the keys in the
+            tensordict that will get match to the corresponding distribution keys.
+        out_keys (str or iterable of str): keys where the sampled values will be
+            written. Importantly, if these keys are found in the input TensorDict, the
+            sampling step will be skipped.
+        spec (TensorSpec): specs of the first output tensor. Used when calling
+            td_module.random() to generate random values in the target space.
+        safe (bool, optional): if True, the value of the sample is checked against the
+            input spec. Out-of-domain sampling can occur because of exploration policies
+            or numerical under/overflow issues. As for the :obj:`spec` argument, this
+            check will only occur for the distribution sample, but not the other tensors
+            returned by the input module. If the sample is out of bounds, it is
+            projected back onto the desired space using the `TensorSpec.project` method.
+            Default is :obj:`False`.
+        default_interaction_mode (str, optional): default method to be used to retrieve
+            the output value. Should be one of: 'mode', 'median', 'mean' or 'random'
+            (in which case the value is sampled randomly from the distribution). Default
+            is 'mode'.
+            Note: When a sample is drawn, the :obj:`ProbabilisticTDModule` instance will
+            fist look for the interaction mode dictated by the `interaction_mode()`
+            global function. If this returns `None` (its default value), then the
+            `default_interaction_mode` of the `ProbabilisticTDModule` instance will be
+            used. Note that DataCollector instances will use `set_interaction_mode` to
+            `"random"` by default.
+        distribution_class (Type, optional): a torch.distributions.Distribution class to
+            be used for sampling. Default is Delta.
+        distribution_kwargs (dict, optional): kwargs to be passed to the distribution.
+        return_log_prob (bool, optional): if True, the log-probability of the
+            distribution sample will be written in the tensordict with the key
+            `'sample_log_prob'`. Default is `False`.
+        cache_dist (bool, optional): EXPERIMENTAL: if True, the parameters of the
+            distribution (i.e. the output of the module) will be written to the
+            tensordict along with the sample. Those parameters can be used to re-compute
+            the original distribution later on (e.g. to compute the divergence between
+            the distribution used to sample the action and the updated distribution in
+            PPO). Default is `False`.
+        n_empirical_estimate (int, optional): number of samples to compute the empirical
+            mean when it is not available. Default is 1000
+
+    """
+
     def __init__(
         self,
         in_keys: Union[str, Sequence[str], dict],
@@ -119,6 +190,26 @@ class SafeProbabilisticModule(
 
 
 class SafeProbabilisticSequential(ProbabilisticTensorDictSequential, SafeSequential):
+    """A :obj:``SafeProbabilisticSequential`` is an :obj:``tensordict.nn.prototype.ProbabilisticTensorDictSequential`` subclass that accepts a :obj:``TensorSpec`` as argument to control the output domain.
+
+    Similarly to :obj:`TensorDictSequential`, but enforces that the final module in the
+    sequence is an :obj:`ProbabilisticTensorDictModule` and also exposes ``get_dist``
+    method to recover the distribution object from the ``ProbabilisticTensorDictModule``
+
+    Args:
+         modules (iterable of TensorDictModules): ordered sequence of TensorDictModule
+            instances, terminating in ProbabilisticTensorDictModule, to be run
+            sequentially.
+         partial_tolerant (bool, optional): if True, the input tensordict can miss some
+            of the input keys. If so, the only module that will be executed are those
+            who can be executed given the keys that are present. Also, if the input
+            tensordict is a lazy stack of tensordicts AND if partial_tolerant is
+            :obj:`True` AND if the stack does not have the required keys, then
+            TensorDictSequential will scan through the sub-tensordicts looking for those
+            that have the required keys, if any.
+
+    """
+
     def __init__(
         self,
         *modules: Union[TensorDictModule, ProbabilisticTensorDictModule],
