316d7d9411ba65ad0349d37130a3df5e592d70b4
vmoens <vincentmoens@gmail.com>
2022-12-08 16:38:35 +0000

    init

--- torchrl/modules/tensordict_module/actors.py
+++ torchrl/modules/tensordict_module/actors.py
@@ -85,7 +85,6 @@ class ProbabilisticActor(SafeProbabilisticModule):
         >>> action_spec = NdBoundedTensorSpec(shape=torch.Size([4]),
         ...    minimum=-1, maximum=1)
         >>> module = NormalParamWrapper(torch.nn.Linear(4, 8))
-        >>> params = make_functional(module)
         >>> tensordict_module = SafeModule(module, in_keys=["observation"], out_keys=["loc", "scale"])
         >>> td_module = ProbabilisticActor(
         ...    module=tensordict_module,
@@ -93,6 +92,7 @@ class ProbabilisticActor(SafeProbabilisticModule):
         ...    dist_in_keys=["loc", "scale"],
         ...    distribution_class=TanhNormal,
         ...    )
+        >>> params = make_functional(td_module)
         >>> td = td_module(td, params=params)
         >>> td
         TensorDict(
