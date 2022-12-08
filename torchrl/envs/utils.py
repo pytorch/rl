27a12e33f8cd425844058d3ace519c7c64fe0f3e
Ying-Chen Lin <yclin@fb.com>
2022-12-08 13:23:25 +0000

    add autograd support for step method of Brax envs

--- torchrl/envs/utils.py
+++ torchrl/envs/utils.py
@@ -182,7 +182,7 @@ def check_env_specs(env):
         assert fake_tensordict[key].shape == real_tensordict[key].shape
 
     # test dtypes
-    real_tensordict = env.rollout(3) # keep empty structures, i.e. dict()
+    real_tensordict = env.rollout(3) # keep empty structures, for example dict()
     for key, value in real_tensordict.items():
         _check_dtype(key, value, env.observation_spec, env.input_spec)
 
