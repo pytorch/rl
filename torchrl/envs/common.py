c15b0569995bdd299176d090790839c6b4000d0e
Vincent Moens <vincentmoens@gmail.com>
2022-12-27 07:40:19 +0100

    Revert "Minor cleaning in BaseEnv classes (#767)" (#768)
    This reverts commit 7c2889563ad29129d32c8caaa762b7303a33ce45.

--- torchrl/envs/common.py
+++ torchrl/envs/common.py
@@ -470,7 +470,7 @@ class EnvBase(nn.Module, metaclass=abc.ABCMeta):
     def numel(self) -> int:
         return prod(self.batch_size)
 
-    def set_seed(self, seed: Optional[int] = None, static_seed: bool = False) -> Optional[int]:
+    def set_seed(self, seed: int, static_seed: bool = False) -> int:
         """Sets the seed of the environment and returns the next seed to be used (which is the input seed if a single environment is present).
 
         Args:
@@ -491,7 +491,6 @@ class EnvBase(nn.Module, metaclass=abc.ABCMeta):
             seed = new_seed
         return seed
 
-    @abc.abstractmethod
     def _set_seed(self, seed: Optional[int]):
         raise NotImplementedError
 
@@ -788,7 +787,6 @@ class _EnvWrapper(EnvBase, metaclass=abc.ABCMeta):
             f"env not set in {self.__class__.__name__}, cannot access {attr}"
         )
 
-    @abc.abstractmethod
     def _init_env(self) -> Optional[int]:
         """Runs all the necessary steps such that the environment is ready to use.
 
@@ -823,7 +821,22 @@ class _EnvWrapper(EnvBase, metaclass=abc.ABCMeta):
         except AttributeError:
             pass
 
-        
+    def set_seed(
+        self, seed: Optional[int] = None, static_seed: bool = False
+    ) -> Optional[int]:
+        if seed is not None:
+            torch.manual_seed(seed)
+        self._set_seed(seed)
+        if seed is not None and not static_seed:
+            new_seed = seed_generator(seed)
+            seed = new_seed
+        return seed
+
+    @abc.abstractmethod
+    def _set_seed(self, seed: Optional[int]):
+        raise NotImplementedError
+
+
 def make_tensordict(
     env: _EnvWrapper,
     policy: Optional[Callable[[TensorDictBase, ...], TensorDictBase]] = None,
