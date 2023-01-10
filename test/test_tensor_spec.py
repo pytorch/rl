839515bee32f7b77c9a27053b3f183ee4cb7d8cb
Waris Radji <wradji64@gmail.com>
2023-01-10 21:51:39 +0100

    [CodeQuality] call all() without making a list (#821)

--- test/test_tensor_spec.py
+++ test/test_tensor_spec.py
@@ -973,13 +973,13 @@ class TestSpec:
         actions_numpy = [action_spec.to_numpy(a) for a in actions_tensors]
         actions_tensors_2 = [action_spec.encode(a) for a in actions_numpy]
         assert all(
-            [(a1 == a2).all() for a1, a2 in zip(actions_tensors, actions_tensors_2)]
+            (a1 == a2).all() for a1, a2 in zip(actions_tensors, actions_tensors_2)
         )
 
         actions_numpy = [int(np.random.randint(0, 10, (1,))) for a in actions_tensors]
         actions_tensors = [action_spec.encode(a) for a in actions_numpy]
         actions_numpy_2 = [action_spec.to_numpy(a) for a in actions_tensors]
-        assert all([(a1 == a2) for a1, a2 in zip(actions_numpy, actions_numpy_2)])
+        assert all((a1 == a2) for a1, a2 in zip(actions_numpy, actions_numpy_2))
 
     def test_mult_discrete_action_spec_reconstruct(self):
         torch.manual_seed(0)
@@ -1000,7 +1000,7 @@ class TestSpec:
         ]
         actions_tensors = [action_spec.encode(a) for a in actions_numpy]
         actions_numpy_2 = [action_spec.to_numpy(a) for a in actions_tensors]
-        assert all([(a1 == a2).all() for a1, a2 in zip(actions_numpy, actions_numpy_2)])
+        assert all((a1 == a2).all() for a1, a2 in zip(actions_numpy, actions_numpy_2))
 
     def test_one_hot_discrete_action_spec_rand(self):
         torch.manual_seed(0)
