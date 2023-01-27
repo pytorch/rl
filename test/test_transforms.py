835cd40a5ee1514e46d685c0486a1c83736e9379
Vincent Moens <vincentmoens@gmail.com>
2023-01-27 16:50:02 +0000

    [Refactor] Removing inplace transform attribute (#871)

--- test/test_transforms.py
+++ test/test_transforms.py
@@ -1259,31 +1259,6 @@ class TestTransforms:
         with pytest.raises(RuntimeError, match=err_msg):
             transform._apply_transform(torch.Tensor([1]))
 
-    @pytest.mark.parametrize("device", get_available_devices())
-    def test_observationnorm_infinite_stats_error(self, device):
-        base_env = ContinuousActionVecMockEnv(
-            observation_spec=CompositeSpec(
-                observation=BoundedTensorSpec(
-                    minimum=1, maximum=1, shape=torch.Size([1])
-                ),
-                observation_orig=BoundedTensorSpec(
-                    minimum=1, maximum=1, shape=torch.Size([1])
-                ),
-            ),
-            action_spec=BoundedTensorSpec(minimum=1, maximum=1, shape=torch.Size((1,))),
-            seed=0,
-        )
-        base_env.out_key = "observation"
-        t_env = TransformedEnv(
-            base_env,
-            transform=ObservationNorm(in_keys="observation"),
-        )
-        t_env.append_transform(ObservationNorm(in_keys="observation"))
-        err_msg = "Non-finite values found in"
-        with pytest.raises(RuntimeError, match=err_msg):
-            for transform in t_env.transform:
-                transform.init_stats(num_iter=100)
-
     def test_catframes_transform_observation_spec(self):
         N = 4
         key1 = "first key"
@@ -1649,7 +1624,6 @@ class TestTransforms:
             device=device,
         )
         br(td)
-        assert td["reward"] is reward
         assert (td["reward"] != reward_copy).all()
         assert (td["misc"] == misc_copy).all()
         assert (torch.count_nonzero(td["reward"]) == torch.sum(reward_copy > 0)).all()
