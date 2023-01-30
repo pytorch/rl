420058a6a5c18262e93e55a0c46e94d7f5525f44
Grigory Sizov <grigorysizov@fb.com>
2023-01-30 18:59:54 +0100

    Run linter

--- test/_utils_internal.py
+++ test/_utils_internal.py
@@ -264,6 +264,7 @@ def get_transform_out(env_name, transformed_in):
 
     return t_out
 
+
 @contextlib.contextmanager
 def set_global_var(module, var_name, value):
     old_value = getattr(module, var_name)
