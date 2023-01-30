78a18efcfc8e1a7225604b7bc4cda9d666a157c7
vmoens <vincentmoens@gmail.com>
2023-01-30 14:10:31 +0000

    amend

--- test/test_rb.py
+++ test/test_rb.py
@@ -813,7 +813,12 @@ def test_smoke_replay_buffer_transform(transform):
 
     td = TensorDict({"observation": torch.randn(3, 3, 3, 16, 1)}, [])
     rb.add(td)
-    rb.sample(1)
+    if not isinstance(rb._transform[0], (CatFrames, StepCounter)):
+        rb.sample(1)
+    else:
+        with pytest.raises(NotImplementedError):
+            rb.sample(1)
+        return
 
     rb._transform = mock.MagicMock()
     rb.sample(1)
