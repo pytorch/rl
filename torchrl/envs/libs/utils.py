da9cb599c0142d432fdca023ee03ac740f02c801
vmoens <vincentmoens@gmail.com>
2023-02-07 16:10:01 +0000

    amend

--- torchrl/envs/libs/utils.py
+++ torchrl/envs/libs/utils.py
@@ -10,7 +10,21 @@ import copy
 from collections.abc import MutableMapping
 
 import numpy as np
-from gym import ObservationWrapper, spaces
+
+IMPORT_ERROR = None
+_has_gym = False
+try:
+    from gymnasium import ObservationWrapper, spaces
+
+    _has_gym = True
+except ImportError as err:
+    IMPORT_ERROR = err
+    try:
+        from gym import ObservationWrapper, spaces
+
+        _has_gym = True
+    except ImportError as err:
+        IMPORT_ERROR = err
 
 STATE_KEY = "state"
 
@@ -42,6 +56,8 @@ class GymPixelObservationWrapper(ObservationWrapper):
     def __init__(
         self, env, pixels_only=True, render_kwargs=None, pixel_keys=("pixels",)
     ):
+        if not _has_gym:
+            raise IMPORT_ERROR
         env.reset()
         super().__init__(env)
 
