a2a3f3e03c03ca924e55b97083cd097e4dee6b0b
vmoens <vincentmoens@gmail.com>
2023-02-20 22:13:49 +0100

    d4rl

--- torchrl/data/__init__.py
+++ torchrl/data/__init__.py
@@ -3,6 +3,7 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+from .datasets import D4RLExperienceReplay
 from .postprocs import MultiStep
 from .replay_buffers import (
     LazyMemmapStorage,
---
+++ torchrl/data/datasets/d4rl.py
@@ -0,0 +1,68 @@
+from typing import Callable, Optional
+
+import d4rl  # noqa
+
+import gym  # noqa
+import torch
+from tensordict.tensordict import make_tensordict
+
+from torchrl.data import CompositeSpec, LazyMemmapStorage
+from torchrl.data.replay_buffers import TensorDictReplayBuffer
+from torchrl.data.replay_buffers.samplers import Sampler
+from torchrl.data.replay_buffers.writers import Writer
+from torchrl.envs.libs.gym import GymWrapper
+
+
+class D4RLExperienceReplay(TensorDictReplayBuffer):
+    """An Experience replay class for D4RL.
+
+    Examples:
+        >>> from torchrl.data.datasets.d4rl import D4RLExperienceReplay
+        >>> from torchrl.envs import ObservationNorm
+        >>> data = D4RLExperienceReplay("maze2d-umaze-v1")
+        >>> # we can append transforms to the dataset
+        >>> data.append_transform(ObservationNorm(loc=-1, scale=1.0))
+        >>> data.sample(128)
+
+    """
+
+    def __init__(
+        self,
+        name,
+        sampler: Optional[Sampler] = None,
+        writer: Optional[Writer] = None,
+        collate_fn: Optional[Callable] = None,
+        pin_memory: bool = False,
+        prefetch: Optional[int] = None,
+        transform: Optional["Transform"] = None,  # noqa-F821
+        **env_kwargs,
+    ):
+        env = GymWrapper(gym.make(name, **env_kwargs))
+        dataset = make_tensordict(
+            {
+                k: torch.tensor(item)
+                for k, item in d4rl.qlearning_dataset(env._env).items()
+            }
+        )
+        dataset.rename_key("terminals", "done")
+        dataset.rename_key("observations", "observation")
+        dataset.rename_key("next_observations", "next/observation")
+        dataset.rename_key("rewards", "reward")
+        dataset.rename_key("actions", "action")
+        dataset = dataset.unflatten_keys("/")
+        storage = LazyMemmapStorage(dataset.shape[0])
+        super().__init__(
+            storage=storage,
+            sampler=sampler,
+            writer=writer,
+            collate_fn=collate_fn,
+            pin_memory=pin_memory,
+            prefetch=prefetch,
+            transform=transform,
+        )
+        self.extend(dataset)
+        self.specs = CompositeSpec(
+            input_spec=env.input_spec,
+            observation_spec=env.observation_spec,
+            reward_spec=env.reward_spec,
+        )
