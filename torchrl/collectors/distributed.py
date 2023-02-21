46ec988d7bf49cca0f19de35941f251acdd307e3
vmoens <vincentmoens@gmail.com>
2023-02-21 16:56:28 +0100

    amend

--- torchrl/collectors/distributed.py
+++ torchrl/collectors/distributed.py
@@ -1,13 +1,14 @@
 import os
-import time
-import submitit
 import socket
+import time
+
+import submitit
 
 from torch.distributed import rpc
 
 from torchrl.collectors import MultiaSyncDataCollector
 from torchrl.collectors.collectors import RandomPolicy
-from torchrl.envs import EnvCreator
+from torchrl.envs import EnvCreator, EnvBase
 from torchrl.envs.vec_env import _BatchedEnv
 
 
@@ -34,10 +35,12 @@ def collect(rank, rank0_ip):
 
 
 class DistributedDataCollector:
-    def __init__(self, env_makers, policy, frames_per_batch, total_frames):
+    def __init__(self, env_makers, policy, num_envs_per_collector, frames_per_batch, total_frames):
         self.env_constructors = env_makers
+        self.policy = policy
         self.num_workers = len(env_makers)
         self.frames_per_batch = frames_per_batch
+        self.num_envs_per_collector =  num_envs_per_collector
         self.total_frames = total_frames
 
         hostname = socket.gethostname()
@@ -63,15 +66,14 @@ class DistributedDataCollector:
     def _init_workers(self):
         self.collector_infos = []
         self.collector_rrefs = []
+        self.futures = []
         for i in range(self.num_workers):
             print("Submitting job")
             executor = submitit.AutoExecutor(folder="log_test")
             executor.update_parameters(
-                timeout_min=10,
-                slurm_partition="train",
-                slurm_cpus_per_task=32
-                )
-            job = executor.submit(collect, i+1, self.IPAddr)  # will compute add(5, 7)
+                timeout_min=10, slurm_partition="train", slurm_cpus_per_task=32
+            )
+            job = executor.submit(collect, i + 1, self.IPAddr)  # will compute add(5, 7)
             print("job id", job.job_id)  # ID of your job
 
             print("creating the collector")
@@ -90,23 +92,31 @@ class DistributedDataCollector:
             collector_rref = rpc.remote(
                 collector_info,
                 MultiaSyncDataCollector,
-                args=([env_make] * self.num_envs_per_collector,
-                      self.policy),
+                args=([env_make] * self.num_envs_per_collector, self.policy),
                 kwargs={
                     "frames_per_batch": self.frames_per_batch,
-                        "total_frames": self.total_frames, 
-                        "split_trajs": False
-                        },
+                    "total_frames": self.total_frames,
+                    "split_trajs": False,
+                },
             )
+            future = rpc.rpc_async(
+                collector_info, MultiaSyncDataCollector.next, args=(collector_rref,)
+            )
+
             self.collector_infos.append(collector_info)
             self.collector_rrefs.append(collector_rref)
+            self.futures.append(future)
 
     def iterator(self):
         total_frames = 0
         while total_frames < self.total_frames:
-            future = rpc.rpc_async(
-                self.collector_infos[i],
-                MultiaSyncDataCollector.next,
-                args=(collector_rref,)
-            )
-            total_frames += data.numel()
+            for i, future in enumerate(self.futures):
+                if future.done():
+                    data = future.value()
+                    total_frames += data.numel()
+                    self.futures[i] = rpc.rpc_async(
+                        self.collector_infos[i],
+                        MultiaSyncDataCollector.next,
+                        args=(self.collector_rrefs[i],),
+                    )
+                    yield data
