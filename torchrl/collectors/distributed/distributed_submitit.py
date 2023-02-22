f7e58cb6d21f092c2a00a1db43834b4e57c56b8d
vmoens <vincentmoens@gmail.com>
2023-02-22 11:56:15 +0100

    amend

--- torchrl/collectors/distributed/distributed_submitit.py
+++ torchrl/collectors/distributed/distributed_submitit.py
@@ -6,6 +6,7 @@ from typing import OrderedDict
 SUBMITIT_ERR = None
 try:
     import submitit
+
     _has_submitit = False
 except ModuleNotFoundError as err:
     _has_submitit = False
@@ -31,91 +32,6 @@ DEFAULT_SLURM_CONF = {
     "slurm_gpus_per_node": 0,
 }
 
-def init_master(world_size, env_constructors, collector_class, num_workers_per_collector, policy, frames_per_batch, total_frames, collector_kwargs):
-    hostname = socket.gethostname()
-    IPAddr = socket.gethostbyname(hostname)
-
-    print("IP address", IPAddr)
-
-    os.environ["MASTER_ADDR"] = str(IPAddr)
-    os.environ["MASTER_PORT"] = "29500"
-    os.environ["TORCH_DISTRIBUTED_DEBUG"] = "DETAIL"
-
-    options = rpc.TensorPipeRpcBackendOptions(
-        num_worker_threads=16,
-        init_method=f"tcp://{IPAddr}:10003",
-        rpc_timeout=10_000,
-        _transports=["uv"],
-        # Currently fails when nodes have more than 0 gpus avail,
-        # even when no device is made visible
-        devices=[],
-    )
-    print("init rpc")
-    rpc.init_rpc(
-        "TRAINER_NODE",
-        rank=0,
-        backend=rpc.BackendType.TENSORPIPE,
-        rpc_backend_options=options,
-        world_size=world_size,
-    )
-
-    num_workers = world_size-1
-    time_interval = 1.0
-    collector_infos = []
-    for i in range(num_workers):
-        counter = 0
-        while True:
-            counter += 1
-            time.sleep(time_interval)
-            try:
-                print(f"trying to connect to collector node {i + 1}")
-                collector_info = rpc.get_worker_info(f"COLLECTOR_NODE_{i + 1}")
-                break
-            except RuntimeError as err:
-                if counter * time_interval > MAX_TIME_TO_CONNECT:
-                    raise RuntimeError(
-                        "Could not connect to remote node"
-                        ) from err
-                continue
-        collector_infos.append(collector_info)
-
-    collector_rrefs = []
-    for i in range(num_workers):
-        env_make = env_constructors[i]
-        if not isinstance(env_make, (EnvBase, EnvCreator)):
-            env_make = EnvCreator(env_make)
-        print("Making collector in remote node")
-        collector_rref = rpc.remote(
-            collector_infos[i],
-            collector_class,
-            args=(
-                [env_make] * num_workers_per_collector
-                if collector_class is not SyncDataCollector
-                else env_make,
-                policy,
-            ),
-            kwargs={
-                "frames_per_batch": frames_per_batch,
-                "total_frames": total_frames,
-                "split_trajs": False,
-                **collector_kwargs,
-            },
-        )
-        collector_rrefs.append(collector_rref)
-
-    futures = []
-    for i in range(num_workers):
-        print("Asking for the first batch")
-        future = rpc.rpc_async(
-            collector_infos[i],
-            collector_class.next,
-            args=(collector_rrefs[i],),
-        )
-
-        futures.append(future)
-
-    for f in futures:
-        print(f.wait())
 
 def init_collection_node(rank, rank0_ip, world_size):
     """Sets up RPC on the distant node.
@@ -128,7 +44,7 @@ def init_collection_node(rank, rank0_ip, world_size):
     """
     os.environ["MASTER_ADDR"] = str(rank0_ip)
     os.environ["MASTER_PORT"] = "29500"
-    os.environ["TORCH_DISTRIBUTED_DEBUG"] = "DETAIL"
+    # os.environ["TORCH_DISTRIBUTED_DEBUG"] = "DETAIL"
     # os.environ['TP_SOCKET_IFNAME']='lo'
     options = rpc.TensorPipeRpcBackendOptions(
         num_worker_threads=16,
@@ -198,55 +114,131 @@ class DistributedDataCollector(_DataCollector):
 
         self._init_workers()
 
+    def _init_master(
+        self,
+        world_size,
+        env_constructors,
+        collector_class,
+        num_workers_per_collector,
+        policy,
+        frames_per_batch,
+        total_frames,
+        collector_kwargs,
+    ):
+
+        options = rpc.TensorPipeRpcBackendOptions(
+            num_worker_threads=16,
+            init_method=f"tcp://{self.IPAddr}:10003",
+            rpc_timeout=10_000,
+            _transports=["uv"],
+            # Currently fails when nodes have more than 0 gpus avail,
+            # even when no device is made visible
+            devices=[],
+        )
+        print("init rpc")
+        rpc.init_rpc(
+            "TRAINER_NODE",
+            rank=0,
+            backend=rpc.BackendType.TENSORPIPE,
+            rpc_backend_options=options,
+            world_size=world_size,
+        )
+
+        num_workers = world_size - 1
+        time_interval = 1.0
+        collector_infos = []
+        for i in range(num_workers):
+            counter = 0
+            while True:
+                counter += 1
+                time.sleep(time_interval)
+                try:
+                    print(f"trying to connect to collector node {i + 1}")
+                    collector_info = rpc.get_worker_info(f"COLLECTOR_NODE_{i + 1}")
+                    break
+                except RuntimeError as err:
+                    if counter * time_interval > MAX_TIME_TO_CONNECT:
+                        raise RuntimeError("Could not connect to remote node") from err
+                    continue
+            collector_infos.append(collector_info)
+
+        collector_rrefs = []
+        for i in range(num_workers):
+            env_make = env_constructors[i]
+            if not isinstance(env_make, (EnvBase, EnvCreator)):
+                env_make = EnvCreator(env_make)
+            print("Making collector in remote node")
+            collector_rref = rpc.remote(
+                collector_infos[i],
+                collector_class,
+                args=(
+                    [env_make] * num_workers_per_collector
+                    if collector_class is not SyncDataCollector
+                    else env_make,
+                    policy,
+                ),
+                kwargs={
+                    "frames_per_batch": frames_per_batch,
+                    "total_frames": total_frames,
+                    "split_trajs": False,
+                    **collector_kwargs,
+                },
+            )
+            collector_rrefs.append(collector_rref)
+
+        futures = []
+        for i in range(num_workers):
+            print("Asking for the first batch")
+            future = rpc.rpc_async(
+                collector_infos[i],
+                collector_class.next,
+                args=(collector_rrefs[i],),
+            )
+
+            futures.append(future)
+        self.futures = futures
+        self.collector_rrefs = collector_rrefs
+        self.collector_infos = collector_infos
+
     def _init_workers(self):
-        self.collector_infos = []
-        self.collector_rrefs = []
-        self.futures = []
-        self.executors = []
         executor = submitit.AutoExecutor(folder="log_test")
         executor.update_parameters(**self.slurm_kwargs)
-        job_master = executor.submit(
-            init_master, self.num_workers + 1,
+
+        hostname = socket.gethostname()
+        IPAddr = socket.gethostbyname(hostname)
+        self.IPAddr = IPAddr
+        os.environ["MASTER_ADDR"] = str(self.IPAddr)
+        os.environ["MASTER_PORT"] = "29500"
+        # os.environ["TORCH_DISTRIBUTED_DEBUG"] = "DETAIL"
+
+        for i in range(self.num_workers):
+            print("Submitting job")
+            job = executor.submit(
+                init_collection_node, i + 1, self.IPAddr, self.num_workers + 1
+            )
+            print("job id", job.job_id)  # ID of your job
+            self.executors.append(executor)
+
+        self._init_master(
+            self.num_workers + 1,
             self.env_constructors,
             self.collector_class,
             self.num_workers_per_collector,
             self.policy,
             self.frames_per_batch,
             self.total_frames,
-            self.collector_kwargs
+            self.collector_kwargs,
         )
-        # let's wait until we figure out what the IP address is
-        IPAddr = None
-        count = 0
-        while IPAddr is None and count < 100:
-            count += 1
-            stdout_master = job_master.stdout()
-            if stdout_master is None:
-                time.sleep(1.0)
-                continue
-            for line in stdout_master.split("\n"):
-                if "IP address" in line:
-                    IPAddr = line.split("IP address ")[-1]
-                    break
-            else:
-                time.sleep(1.0)
-        if count >= 100:
-            raise RuntimeError("Failed to get the IP address of master node.")
-        print("IP address:", IPAddr)
-        for i in range(self.num_workers):
-            print("Submitting job")
-            job = executor.submit(
-                init_collection_node, i + 1, IPAddr, self.num_workers + 1
-            )
-            print("job id", job.job_id)  # ID of your job
-            self.executors.append(executor)
-
 
     def iterator(self):
-        # placeholder
-        for i in range(100000):
-            time.sleep(1.0)
-            yield i
+        total_frames = 0
+        while total_frames < self.total_frames:
+            if self._sync:
+                data = self._next_sync()
+            else:
+                data = self._next_async()
+
+            total_frames += data.numel()
 
     def _next_async(self):
         for i, future in enumerate(self.futures):
