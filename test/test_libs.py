e9aeabaa0df045414946e47f0ef01d642dafa821
vmoens <vincentmoens@gmail.com>
2023-03-10 14:34:37 +0000

    select(*keys)

--- test/test_libs.py
+++ test/test_libs.py
@@ -1151,8 +1151,9 @@ class TestD4RL:
         print("f", data_false._storage._storage)
         data_from_env = D4RLExperienceReplay(task, split_trajs=True, from_env=True)
         print("fe", data_from_env._storage._storage)
-        keys = data_from_env._storage._storage.keys(True)
-        assert_allclose_td(data_true._storage._storage.select(*keys), data_from_env._storage._storage)
+        keys = set(data_from_env._storage._storage.keys(True))
+        keys = keys.intersection(data_true._storage._storage.keys(True))
+        assert_allclose_td(data_true._storage._storage.select(*keys), data_from_env._storage._storage.select(*keys))
 
 if __name__ == "__main__":
     args, unknown = argparse.ArgumentParser().parse_known_args()
