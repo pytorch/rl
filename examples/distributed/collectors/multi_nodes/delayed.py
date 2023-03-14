00e67e17021336101a42b45722b90fac8aa2a1d6
vmoens <vincentmoens@gmail.com>
2023-03-14 14:39:40 +0000

    more params

--- examples/distributed/collectors/multi_nodes/delayed.py
+++ examples/distributed/collectors/multi_nodes/delayed.py
@@ -15,21 +15,28 @@ from torchrl.collectors.distributed.generic import (
 from torchrl.envs import EnvCreator
 
 parser = ArgumentParser()
-parser.add_argument("--partition", help="slurm partition to use")
+parser.add_argument("--partition", "-p", help="slurm partition to use")
 parser.add_argument("--num_jobs", type=int, default=8, help="Number of jobs")
 parser.add_argument("--tcp_port", type=int, default=1234, help="TCP port")
 parser.add_argument("--num_workers", type=int, default=1, help="Number of workers per node")
+parser.add_argument("--gpus_per_node", "--gpus-per-node", "-G", type=int, default=0, help="Number of GPUs per node. If greater than 0, the backend used will be NCCL.")
+parser.add_argument("--cpus_per_task", "--cpus-per-task", "-c", type=int, default=32, help="Number of CPUs per node.")
+
 
 args = parser.parse_args()
 
-DEFAULT_SLURM_CONF["slurm_partition"] = args.partition
+slurm_gpus_per_node = args.gpus_per_node
+
+DEFAULT_SLURM_CONF["slurm_gpus_per_node"] = slurm_gpus_per_node
+DEFAULT_SLURM_CONF["slurm_cpus_per_task"] = args.cpus_per_task
+DEFAULT_SLURM_CONF_MAIN["slurm_partition"] = args.partition
 DEFAULT_SLURM_CONF_MAIN["slurm_partition"] = args.partition
 
 num_jobs = args.num_jobs
 tcp_port = args.tcp_port
 num_workers = args.num_workers
 
-@submitit_delayed_launcher(num_jobs=num_jobs, tcpport=tcp_port)
+@submitit_delayed_launcher(num_jobs=num_jobs, backend="nccl" if slurm_gpus_per_node else "gloo", tcpport=tcp_port)
 def main():
     from torchrl.collectors.collectors import RandomPolicy
     from torchrl.data import BoundedTensorSpec
@@ -37,6 +44,7 @@ def main():
     from torchrl.collectors import MultiSyncDataCollector, SyncDataCollector
 
     collector_class = SyncDataCollector if num_workers == 1 else MultiSyncDataCollector
+    device_str = "device" if num_workers == 1 else "devices"
     collector = DistributedDataCollector(
         [EnvCreator(lambda: GymEnv("ALE/Pong-v5"))] * num_jobs,
         policy=RandomPolicy(BoundedTensorSpec(-1, 1, shape=(1,))),
@@ -46,6 +54,9 @@ def main():
         tcp_port=tcp_port,
         collector_class=collector_class,
         num_workers_per_collector=args.num_workers,
+        collector_kwargs={device_str: "cuda:0" if slurm_gpus_per_node else "cpu"},
+        storing_device="cuda:0" if slurm_gpus_per_node else "cpu",
+        backend="nccl" if slurm_gpus_per_node else "gloo",
     )
     pbar = tqdm.tqdm(total=1_000_000)
     for data in collector:
