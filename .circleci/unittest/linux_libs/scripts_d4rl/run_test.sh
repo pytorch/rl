2af1f6c8d521e277200e396ca6f313d52db8822a
vmoens <vincentmoens@gmail.com>
2023-03-16 12:44:39 +0000

    amend

--- .circleci/unittest/linux_libs/scripts_d4rl/run_test.sh
+++ .circleci/unittest/linux_libs/scripts_d4rl/run_test.sh
@@ -10,7 +10,7 @@ apt-get update && apt-get install -y git gcc patchelf libosmesa6-dev libgl1-mesa
 git clone https://github.com/Farama-Foundation/d4rl.git
 cd d4rl
 pip3 install -U 'mujoco-py<2.1,>=2.0'
-pip3 install -U "gym[all]"==0.23
+pip3 install -U "gym[classic_control,box2d,atari,accept-rom-license]"==0.23
 pip install -e .
 cd ..
 
