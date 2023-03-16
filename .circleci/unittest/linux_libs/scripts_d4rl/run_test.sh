ef6164bea534fa7bdee108d2482dd309c1bed87d
vmoens <vincentmoens@gmail.com>
2023-03-16 09:34:32 +0000

    amend

--- .circleci/unittest/linux_libs/scripts_d4rl/run_test.sh
+++ .circleci/unittest/linux_libs/scripts_d4rl/run_test.sh
@@ -4,7 +4,7 @@ set -e
 
 eval "$(./conda/bin/conda shell.bash hook)"
 conda activate ./env
-apt-get update && apt-get install -y git
+apt-get update && apt-get install -y git gcc
 
 export PYTORCH_TEST_WITH_SLOW='1'
 python -m torch.utils.collect_env
