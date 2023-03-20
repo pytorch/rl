06dadfa29c304c2c9d5798a4c800fae4d123e0ba
Matteo Bettini <mb2389@cl.cam.ac.uk>
2023-03-20 13:31:13 +0000

    lint

--- test/test_libs.py
+++ test/test_libs.py
@@ -10,8 +10,6 @@ from typing import Optional, Union
 import numpy as np
 import pytest
 import torch
-from packaging import version
-from tensordict.tensordict import assert_allclose_td, TensorDict
 
 from _utils_internal import (
     _make_multithreaded_env,
@@ -21,6 +19,8 @@ from _utils_internal import (
     PENDULUM_VERSIONED,
     PONG_VERSIONED,
 )
+from packaging import version
+from tensordict.tensordict import assert_allclose_td, TensorDict
 from torchrl._utils import implement_for
 from torchrl.collectors import MultiaSyncDataCollector
 from torchrl.collectors.collectors import RandomPolicy
