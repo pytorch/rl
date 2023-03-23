7ac9fc0972145a62526ee55c197dd0bcb6340247
vmoens <vincentmoens@gmail.com>
2023-03-23 16:02:54 +0000

    lint

--- torchrl/collectors/__init__.py
+++ torchrl/collectors/__init__.py
@@ -4,8 +4,8 @@
 # LICENSE file in the root directory of this source tree.
 
 from .collectors import (
-    DataCollectorBase,
     aSyncDataCollector,
+    DataCollectorBase,
     MultiaSyncDataCollector,
     MultiSyncDataCollector,
     SyncDataCollector,
