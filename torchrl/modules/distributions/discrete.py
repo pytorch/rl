8d79cb5ef7e0a4e60a544cdd9c531dacf5df12c6
Xiaomeng Yang <bit.yangxm@gmail.com>
2023-04-05 23:21:53 +0000

    Fix lint

--- torchrl/modules/distributions/discrete.py
+++ torchrl/modules/distributions/discrete.py
@@ -3,7 +3,6 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-from math import prod
 from typing import Optional, Sequence, Union
 
 import torch
