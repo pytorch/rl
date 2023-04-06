e0e69c5dbd60945d30171316785f5e75f414ada1
vmoens <vincentmoens@gmail.com>
2023-04-06 10:36:36 +0100

    amend

---
+++ benchmarks/collectors.py
@@ -0,0 +1,169 @@
+import torch.cuda
+import tqdm
+
+from torchrl.collectors import SyncDataCollector
+from torchrl.collectors.collectors import (
+    MultiaSyncDataCollector,
+    MultiSyncDataCollector,
+    RandomPolicy,
+)
+from torchrl.envs import EnvCreator, StepCounter, TransformedEnv
+from torchrl.envs.libs.dm_control import DMControlEnv
+
+
+def single_collector_setup():
+    device = "cuda:0" if torch.cuda.device_count() else "cpu"
+    env = TransformedEnv(DMControlEnv("cheetah", "run", device=device), StepCounter(50))
+    c = SyncDataCollector(
+        env,
+        RandomPolicy(env.action_spec),
+        total_frames=10_000,
+        frames_per_batch=100,
+        device=device,
+    )
+    c = iter(c)
+    for _ in c:
+        break
+    return ((c,), {})
+
+
+def sync_collector_setup():
+    device = "cuda:0" if torch.cuda.device_count() else "cpu"
+    env = EnvCreator(
+        lambda: TransformedEnv(
+            DMControlEnv("cheetah", "run", device=device), StepCounter(50)
+        )
+    )
+    c = MultiSyncDataCollector(
+        [env, env],
+        RandomPolicy(env().action_spec),
+        total_frames=10_000,
+        frames_per_batch=100,
+        device=device,
+    )
+    c = iter(c)
+    for _ in c:
+        break
+    return ((c,), {})
+
+
+def async_collector_setup():
+    device = "cuda:0" if torch.cuda.device_count() else "cpu"
+    env = EnvCreator(
+        lambda: TransformedEnv(
+            DMControlEnv("cheetah", "run", device=device), StepCounter(50)
+        )
+    )
+    c = MultiaSyncDataCollector(
+        [env, env],
+        RandomPolicy(env().action_spec),
+        total_frames=10_000,
+        frames_per_batch=100,
+        device=device,
+    )
+    c = iter(c)
+    for _ in c:
+        break
+    return ((c,), {})
+
+
+def single_collector_setup_pixels():
+    device = "cuda:0" if torch.cuda.device_count() else "cpu"
+    env = TransformedEnv(
+        DMControlEnv("cheetah", "run", device=device, from_pixels=True), StepCounter(50)
+    )
+    c = SyncDataCollector(
+        env,
+        RandomPolicy(env.action_spec),
+        total_frames=10_000,
+        frames_per_batch=100,
+        device=device,
+    )
+    c = iter(c)
+    for _ in c:
+        break
+    return ((c,), {})
+
+
+def sync_collector_setup_pixels():
+    device = "cuda:0" if torch.cuda.device_count() else "cpu"
+    env = EnvCreator(
+        lambda: TransformedEnv(
+            DMControlEnv("cheetah", "run", device=device, from_pixels=True),
+            StepCounter(50),
+        )
+    )
+    c = MultiSyncDataCollector(
+        [env, env],
+        RandomPolicy(env().action_spec),
+        total_frames=10_000,
+        frames_per_batch=100,
+        device=device,
+    )
+    c = iter(c)
+    for _ in c:
+        break
+    return ((c,), {})
+
+
+def async_collector_setup_pixels():
+    device = "cuda:0" if torch.cuda.device_count() else "cpu"
+    env = EnvCreator(
+        lambda: TransformedEnv(
+            DMControlEnv("cheetah", "run", device=device, from_pixels=True),
+            StepCounter(50),
+        )
+    )
+    c = MultiaSyncDataCollector(
+        [env, env],
+        RandomPolicy(env().action_spec),
+        total_frames=10_000,
+        frames_per_batch=100,
+        device=device,
+    )
+    c = iter(c)
+    for _ in c:
+        break
+    return ((c,), {})
+
+
+def execute_collector(c):
+    ## will run for 9 iterations (1 during setup)
+    for _ in c:
+        continue
+
+
+def test_single(benchmark):
+    benchmark.pedantic(
+        execute_collector, setup=single_collector_setup, iterations=1, rounds=5
+    )
+
+
+def test_sync(benchmark):
+    benchmark.pedantic(
+        execute_collector, setup=sync_collector_setup, iterations=1, rounds=5
+    )
+
+
+def test_async(benchmark):
+    benchmark.pedantic(
+        execute_collector, setup=async_collector_setup, iterations=1, rounds=5
+    )
+
+
+def test_single_pixels(benchmark):
+    benchmark.pedantic(
+        execute_collector, setup=single_collector_setup_pixels, iterations=1, rounds=5
+    )
+
+
+def test_sync_pixels(benchmark):
+    benchmark.pedantic(
+        execute_collector, setup=sync_collector_setup_pixels, iterations=1, rounds=5
+    )
+
+
+def test_async_pixels(benchmark):
+    benchmark.pedantic(
+        execute_collector, setup=async_collector_setup_pixels, iterations=1, rounds=5
+    )
---
+++ benchmarks/envs.py
@@ -0,0 +1,52 @@
+import torch
+
+from torchrl.envs import ParallelEnv, SerialEnv, StepCounter, TransformedEnv
+from torchrl.envs.libs.dm_control import DMControlEnv
+
+
+def make_simple_env():
+    device = "cuda:0" if torch.cuda.device_count() else "cpu"
+    env = DMControlEnv("cheetah", "run", device=device)
+    env.rollout(3)
+    return ((env,), {})
+
+
+def make_transformed_env():
+    device = "cuda:0" if torch.cuda.device_count() else "cpu"
+    env = TransformedEnv(DMControlEnv("cheetah", "run", device=device), StepCounter(50))
+    env.rollout(3)
+    return ((env,), {})
+
+
+def make_serial_env():
+    device = "cuda:0" if torch.cuda.device_count() else "cpu"
+    env = SerialEnv(3, lambda: DMControlEnv("cheetah", "run", device=device))
+    env.rollout(3)
+    return ((env,), {})
+
+
+def make_parallel_env():
+    device = "cuda:0" if torch.cuda.device_count() else "cpu"
+    env = ParallelEnv(3, lambda: DMControlEnv("cheetah", "run", device=device))
+    env.rollout(3)
+    return ((env,), {})
+
+
+def execute_env(env):
+    env.rollout(1000, break_when_any_done=False)
+
+
+def test_simple(benchmark):
+    benchmark.pedantic(execute_env, setup=make_simple_env, iterations=1, rounds=5)
+
+
+def test_transformed(benchmark):
+    benchmark.pedantic(execute_env, setup=make_transformed_env, iterations=1, rounds=5)
+
+
+def test_serial(benchmark):
+    benchmark.pedantic(execute_env, setup=make_serial_env, iterations=1, rounds=5)
+
+
+def test_parallel(benchmark):
+    benchmark.pedantic(execute_env, setup=make_parallel_env, iterations=1, rounds=5)
