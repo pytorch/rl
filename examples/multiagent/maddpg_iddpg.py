46415faaf18b7b4c561eeb37db9b22e57ffc6b70
Matteo Bettini <mb2389@cl.cam.ac.uk>
2023-04-17 12:02:03 +0200

    maddpg

--- examples/multiagent/maddpg_iddpg.py
+++ examples/multiagent/maddpg_iddpg.py
@@ -2,25 +2,31 @@ import time
 
 import numpy as np
 import torch
-
-import wandb
-from models.mlp import MultiAgentMLP
 from tensordict.nn import TensorDictModule
 from tensordict.nn.distributions import NormalParamExtractor
 from torch import nn
+
+import wandb
+from models.mlp import MultiAgentMLP
 from torchrl.collectors import SyncDataCollector
 from torchrl.data.replay_buffers import ReplayBuffer
-from torchrl.data.replay_buffers.samplers import SamplerWithoutReplacement
+from torchrl.data.replay_buffers.samplers import RandomSampler
 from torchrl.data.replay_buffers.storages import LazyTensorStorage
 from torchrl.envs.libs.vmas import VmasEnv
-from torchrl.modules import ProbabilisticActor, TanhNormal, ValueOperator
-from torchrl.objectives import ClipPPOLoss, ValueEstimators
+from torchrl.envs.utils import set_exploration_mode
+from torchrl.modules import (
+    ProbabilisticActor,
+    TanhDelta,
+    ValueOperator,
+    OrnsteinUhlenbeckProcessWrapper, AdditiveGaussianWrapper,
+)
+from torchrl.objectives import DDPGLoss, ValueEstimators
 from torchrl.record.loggers import generate_exp_name
 from torchrl.record.loggers.wandb import WandbLogger
 
 
 def rendering_callback(env, td):
-    env.frames.append(env_test.render(mode="rgb_array", agent_index_focus=None))
+    env.frames.append(env.render(mode="rgb_array", agent_index_focus=None))
 
 
 if __name__ == "__main__":
@@ -41,6 +47,7 @@ if __name__ == "__main__":
     vmas_envs = frames_per_batch // max_steps
     n_iters = 500  # Number of sampling/training iterations
     total_frames = frames_per_batch * n_iters
+    memory_size = total_frames
 
     scenario_name = "balance"
     env_config = {
@@ -51,21 +58,18 @@ if __name__ == "__main__":
         # RL
         "gamma": 0.9,
         "seed": seed,
-        # PPO
-        "lmbda": 0.9,
-        "entropy_eps": 0,
-        "clip_epsilon": 0.2,
         # Sampling,
         "frames_per_batch": frames_per_batch,
         "max_steps": max_steps,
         "vmas_envs": vmas_envs,
         "n_iters": n_iters,
         "total_frames": total_frames,
+        "memory_size": memory_size,
         "vmas_device": vmas_device,
         # Training
         "num_epochs": 45,  # optimization steps per batch of data collected
         "minibatch_size": 4096,  # size of minibatches used in each epoch
-        "lr": 5e-5,
+        "lr": 5e-4,
         "max_grad_norm": 40.0,
         "training_device": training_device,
         # Evaluation
@@ -101,11 +105,10 @@ if __name__ == "__main__":
     )
     env_config.update({"n_agents": env.n_agents, "scenario_name": scenario_name})
 
-    # Policy
     actor_net = nn.Sequential(
         MultiAgentMLP(
             n_agent_inputs=env.observation_spec["observation"].shape[-1],
-            n_agent_outputs=2 * env.action_spec.shape[-1],
+            n_agent_outputs=env.action_spec.shape[-1],
             n_agents=env.n_agents,
             centralised=False,
             share_params=model_config["shared_parameters"],
@@ -114,26 +117,28 @@ if __name__ == "__main__":
             num_cells=256,
             activation_class=nn.Tanh,
         ),
-        NormalParamExtractor(),
     )
     policy_module = TensorDictModule(
-        actor_net, in_keys=["observation"], out_keys=["loc", "scale"]
+        actor_net, in_keys=["observation"], out_keys=["param"]
     )
     policy = ProbabilisticActor(
         module=policy_module,
         spec=env.unbatched_input_spec["action"],
-        in_keys=["loc", "scale"],
-        distribution_class=TanhNormal,
+        in_keys=["param"],
+        distribution_class=TanhDelta,
         distribution_kwargs={
             "min": env.unbatched_input_spec["action"].space.minimum,
             "max": env.unbatched_input_spec["action"].space.maximum,
         },
-        return_log_prob=True,
+        return_log_prob=False,
+    )
+    policy = AdditiveGaussianWrapper(
+        policy, annealing_num_steps=total_frames
     )
 
     # Critic
     module = MultiAgentMLP(
-        n_agent_inputs=env.observation_spec["observation"].shape[-1],
+        n_agent_inputs=env.observation_spec["observation"].shape[-1] + env.action_spec.shape[-1], # Q critic takes action and value
         n_agent_outputs=1,
         n_agents=env.n_agents,
         centralised=model_config["centralised_critic"],
@@ -145,10 +150,11 @@ if __name__ == "__main__":
     )
     value_module = ValueOperator(
         module=module,
-        in_keys=["observation"],
+        in_keys=["observation", "action"],
     )
 
-    value_module(policy(env.reset().to(training_device)))
+    with set_exploration_mode("random"):
+        value_module(policy(env.reset().to(training_device)))
 
     collector = SyncDataCollector(
         env,
@@ -158,34 +164,24 @@ if __name__ == "__main__":
     )
 
     replay_buffer = ReplayBuffer(
-        storage=LazyTensorStorage(frames_per_batch, device=training_device),
-        sampler=SamplerWithoutReplacement(),
+        storage=LazyTensorStorage(memory_size, device=training_device),
+        sampler=RandomSampler(),
         batch_size=config["minibatch_size"],
-        collate_fn=lambda x: x,  # Make it not clone when sampling
+        collate_fn=lambda x: x, # Make it not clone when sampling
     )
 
-    # Loss
-    loss_module = ClipPPOLoss(
-        actor=policy,
-        critic=value_module,
-        advantage_key="advantage",
-        clip_epsilon=config["clip_epsilon"],
-        entropy_coef=config["entropy_eps"],
-        normalize_advantage=False,
-    )
+    loss_module = DDPGLoss(actor_network=policy, value_network=value_module)
+    loss_module.make_value_estimator(ValueEstimators.TD0, gamma=config["gamma"])
 
-    loss_module.make_value_estimator(
-        ValueEstimators.GAE, gamma=config["gamma"], lmbda=config["lmbda"]
-    )
     optim = torch.optim.Adam(loss_module.parameters(), config["lr"])
 
     # Logging
     if log:
         config.update({"model": model_config, "env": env_config})
         model_name = (
-            ("Het" if not model_config["shared_parameters"] else "")
-            + ("MA" if model_config["centralised_critic"] else "I")
-            + "PPO"
+                ("Het" if not model_config["shared_parameters"] else "")
+                + ("MA" if model_config["centralised_critic"] else "I")
+                + "DDPG"
         )
         logger = WandbLogger(
             exp_name=generate_exp_name(env_config["scenario_name"], model_name),
@@ -199,17 +195,10 @@ if __name__ == "__main__":
     total_time = 0
     sampling_start = time.time()
     for i, tensordict_data in enumerate(collector):
-        print(f"\nIteration {i}")
+        print(f"Iteration {i}")
 
         sampling_time = time.time() - sampling_start
         print(f"Sampling took {sampling_time}")
-
-        with torch.no_grad():
-            loss_module.value_estimator(
-                tensordict_data,
-                params=loss_module.critic_params.detach(),
-                target_params=loss_module.target_critic_params,
-            )
 
         data_view = tensordict_data.reshape(-1)
         replay_buffer.extend(data_view)
@@ -217,26 +206,21 @@ if __name__ == "__main__":
         training_tds = []
         training_start = time.time()
         for _ in range(config["num_epochs"]):
-            for _ in range(frames_per_batch // config["minibatch_size"]):
-                subdata = replay_buffer.sample()
-                loss_vals = loss_module(subdata)
-                training_tds.append(loss_vals.detach())
+            subdata = replay_buffer.sample()
+            loss_vals = loss_module(subdata)
+            training_tds.append(loss_vals.detach())
 
-                loss_value = (
-                    loss_vals["loss_objective"]
-                    + loss_vals["loss_critic"]
-                    + loss_vals["loss_entropy"]
-                )
+            loss_value = loss_vals["loss_actor"] + loss_vals["loss_value"]
 
-                loss_value.backward()
+            loss_value.backward()
 
-                total_norm = torch.nn.utils.clip_grad_norm_(
-                    loss_module.parameters(), config["max_grad_norm"]
-                )
-                training_tds[-1]["grad_norm"] = total_norm.mean()
+            total_norm = torch.nn.utils.clip_grad_norm_(
+                loss_module.parameters(), config["max_grad_norm"]
+            )
+            training_tds[-1]["grad_norm"] = total_norm.mean()
 
-                optim.step()
-                optim.zero_grad()
+            optim.step()
+            optim.zero_grad()
 
         training_time = time.time() - training_start
         print(f"Training took: {training_time}")
@@ -280,14 +264,13 @@ if __name__ == "__main__":
                 },
                 commit=False,
             )
-
         if (
             config["evaluation_episodes"] > 0
             and i % config["evaluation_interval"] == 0
             and log
         ):
             evaluation_start = time.time()
-            with torch.no_grad():
+            with torch.no_grad() and set_exploration_mode("mean"):
                 rollouts = []
                 for _ in range(config["evaluation_episodes"] - 1):
                     rollouts.append(
