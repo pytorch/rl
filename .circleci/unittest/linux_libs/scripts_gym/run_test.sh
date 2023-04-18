efd233d3f7ce05975a0ee83281e36ad549449cd5
vmoens <vincentmoens@gmail.com>
2023-04-18 20:56:04 +0100

    amend

--- .circleci/unittest/linux_libs/scripts_gym/run_test.sh
+++ .circleci/unittest/linux_libs/scripts_gym/run_test.sh
@@ -14,12 +14,11 @@ root_dir="$(git rev-parse --show-toplevel)"
 env_dir="${root_dir}/env"
 lib_dir="${env_dir}/lib"
 
-# solves ImportError: /lib64/libstdc++.so.6: version `GLIBCXX_3.4.21' not found
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$lib_dir
 export MKL_THREADING_LAYER=GNU
 
 python .circleci/unittest/helpers/coverage_run_parallel.py -m pytest test/smoke_test.py -v --durations 20
 python .circleci/unittest/helpers/coverage_run_parallel.py -m pytest test/smoke_test_deps.py -v --durations 20 -k 'test_gym'
-DISPLAY=:1 MUJOCO_GL=egl python .circleci/unittest/helpers/coverage_run_parallel.py -m pytest test/test_libs.py --instafail -v --durations 20 -k "gym" --error-for-skips
+DISPLAY=:1 MUJOCO_GL=egl xvfb-run -s "-screen 0 1400x900x24" python .circleci/unittest/helpers/coverage_run_parallel.py -m pytest test/test_libs.py --instafail -v --durations 20 -k "gym" --error-for-skips
 coverage combine
 coverage xml -i
