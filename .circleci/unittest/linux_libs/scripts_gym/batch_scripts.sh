33d23b74eafeb9452d187f01023a72359f6bc8d3
vmoens <vincentmoens@gmail.com>
2023-04-19 11:40:36 +0100

    amend

--- .circleci/unittest/linux_libs/scripts_gym/batch_scripts.sh
+++ .circleci/unittest/linux_libs/scripts_gym/batch_scripts.sh
@@ -14,7 +14,21 @@ conda activate ./env
 $DIR/install.sh
 
 # Extracted from run_test.sh to run once.
-yum makecache && yum install libglvnd-devel zlib-devel mesa-libGL mesa-libGL-devel mesa-libEGL mesa-libEGL-devel glfw mesa-libOSMesa-devel glew glew-devel egl-utils freeglut xorg-x11-server-Xvfb -y
+yum makecache && yum install \
+#  libglvnd-devel \
+#  zlib-devel \
+#  mesa-libGL \
+#  mesa-libGL-devel \
+#  mesa-libEGL \
+#  mesa-libEGL-devel \
+#  glfw \
+#  mesa-libOSMesa-devel \
+#  glew \
+#  glew-devel \
+#  egl-utils \
+  freeglut-devel \
+  xorg-x11-server-Xvfb -y
+
 #yum makecache && yum install libglvnd-devel glew xorg-x11-server-Xvfb zlib-devel egl-utils freeglut mesa-libEGL -y
 
 
