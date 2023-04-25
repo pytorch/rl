58f5292a4fcc19a1d0adc68efdf8c63524335fe3
vmoens <vincentmoens@gmail.com>
2023-04-25 10:48:12 +0100

    amend

--- .circleci/unittest/linux_libs/scripts_gym/setup_env.sh
+++ .circleci/unittest/linux_libs/scripts_gym/setup_env.sh
@@ -75,6 +75,7 @@ conda env config vars set \
   SDL_VIDEODRIVER=dummy \
   DISPLAY=unix:0.0 \
   PYOPENGL_PLATFORM=egl \
+  NVIDIA_PATH=/usr/src/nvidia-470.63.01 \
   MUJOCO_PY_MJKEY_PATH=${root_dir}/mujoco-py/mujoco_py/binaries/mjkey.txt
 
 #  MUJOCO_PY_MUJOCO_PATH=$root_dir/.mujoco/mujoco200_linux \
