a11c8e6ceaae67bb15c851c4c5103fb91b503b0c
vmoens <vincentmoens@gmail.com>
2023-04-25 07:58:42 +0100

    get_nvidia_lib_dir

--- .circleci/unittest/linux_libs/scripts_gym/run_test.sh
+++ .circleci/unittest/linux_libs/scripts_gym/run_test.sh
@@ -19,8 +19,12 @@ export MKL_THREADING_LAYER=GNU
 
 python .circleci/unittest/helpers/coverage_run_parallel.py -m pytest test/smoke_test.py -v --durations 20
 python .circleci/unittest/helpers/coverage_run_parallel.py -m pytest test/smoke_test_deps.py -v --durations 20 -k 'test_gym'
-#export DISPLAY=':99.0'
-#Xvfb :99 -screen 0 1400x900x24 > /dev/null 2>&1 &
-MUJOCO_GL=egl python .circleci/unittest/helpers/coverage_run_parallel.py -m pytest test/test_libs.py --instafail -v --durations 20 -k "gym" --error-for-skips
+
+python -c """
+from mujoco_py.builder import get_nvidia_lib_dir
+print('NVIDIA LIB DIR', get_nvidia_lib_dir())
+"""
+
+python .circleci/unittest/helpers/coverage_run_parallel.py -m pytest test/test_libs.py --instafail -v --durations 20 -k "gym" --error-for-skips
 coverage combine
 coverage xml -i
