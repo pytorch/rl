04dc12e6546568b0a322c65357eb2d369e068043
vmoens <vincentmoens@gmail.com>
2023-04-27 21:28:34 +0100

    LD_PRELOAD

--- .circleci/unittest/linux_libs/scripts_gym/setup_env.sh
+++ .circleci/unittest/linux_libs/scripts_gym/setup_env.sh
@@ -69,7 +69,19 @@ cat "${this_dir}/environment.yml"
 
 
 conda install -c conda-forge gcc -y
-#conda install -c conda-forge glew -y
+conda install -c conda-forge glew -y
+
+# Search for the glew.h file
+glew_path=$(find ./conda -name glew.h 2>/dev/null)
+
+# Check if glew.h file is found
+if [ -z "$glew_path" ]; then
+    echo "glew.h file not found"
+    exit 1
+fi
+
+echo "glew.h path: $glew_path"
+
 conda install -c anaconda mesa-libegl-cos6-x86_64 -y
 
 echo "Installing X11 with conda"
@@ -87,6 +99,7 @@ conda env config vars set \
   SDL_VIDEODRIVER=dummy \
   DISPLAY=unix:0.0 \
   PYOPENGL_PLATFORM=egl \
+  LD_PRELOAD=$glew_path \
   NVIDIA_PATH=/usr/src/nvidia-470.63.01 \
   MUJOCO_PY_MJKEY_PATH=${root_dir}/mujoco-py/mujoco_py/binaries/mjkey.txt \
   MUJOCO_PY_MUJOCO_PATH=${root_dir}/mujoco-py/mujoco_py/binaries/linux/mujoco210 \
