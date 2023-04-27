7a2c96fdbada2e631a57556fbb7b3c2a052b746c
vmoens <vincentmoens@gmail.com>
2023-04-27 14:14:43 +0100

    amend

--- .circleci/unittest/linux_libs/scripts_gym/run_test.sh
+++ .circleci/unittest/linux_libs/scripts_gym/run_test.sh
@@ -20,13 +20,6 @@ export MKL_THREADING_LAYER=GNU
 python .circleci/unittest/helpers/coverage_run_parallel.py -m pytest test/smoke_test.py -v --durations 20
 python .circleci/unittest/helpers/coverage_run_parallel.py -m pytest test/smoke_test_deps.py -v --durations 20 -k 'test_gym'
 
-python -c """
-import os
-print('NVIDIA_PATH', os.environ.get('NVIDIA_PATH'))
-print('LD_LIBRARY_PATH', os.environ.get('LD_LIBRARY_PATH'))
-from mujoco_py.builder import get_nvidia_lib_dir
-print('NVIDIA LIB DIR', get_nvidia_lib_dir())
-"""
 export DISPLAY=':99.0'
 Xvfb :99 -screen 0 1400x900x24 > /dev/null 2>&1 &
 python .circleci/unittest/helpers/coverage_run_parallel.py -m pytest test/test_libs.py --instafail -v --durations 20 -k "gym" --error-for-skips
