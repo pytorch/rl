2f289415302d5e34a6052e23b34c66586f474b54
vmoens <vincentmoens@gmail.com>
2023-04-27 13:32:14 +0100

    export DISPLAY=':99.0' Xvfb :99 -screen 0 1400x900x24 > /dev/null 2>&1 &

--- .circleci/unittest/linux_libs/scripts_gym/run_test.sh
+++ .circleci/unittest/linux_libs/scripts_gym/run_test.sh
@@ -27,7 +27,8 @@ print('LD_LIBRARY_PATH', os.environ.get('LD_LIBRARY_PATH'))
 from mujoco_py.builder import get_nvidia_lib_dir
 print('NVIDIA LIB DIR', get_nvidia_lib_dir())
 """
-
+export DISPLAY=':99.0'
+Xvfb :99 -screen 0 1400x900x24 > /dev/null 2>&1 &
 python .circleci/unittest/helpers/coverage_run_parallel.py -m pytest test/test_libs.py --instafail -v --durations 20 -k "gym" --error-for-skips
 coverage combine
 coverage xml -i
