6f58ec686c6ca81d48574d9a979f6d823ff3baed
Vincent Moens <vincentmoens@gmail.com>
2023-04-28 14:56:17 +0100

    Europe/Moscow

--- .circleci/unittest/linux_examples/scripts/setup_env.sh
+++ .circleci/unittest/linux_examples/scripts/setup_env.sh
@@ -111,7 +111,8 @@ fi
 
 
 # https://github.com/lhelontra/tensorflow-on-arm/issues/13#issuecomment-489296444
-TZ=Etc/UTC apt-get install software-properties-common -y
+export TZ=Europe/London
+apt-get install software-properties-common -y
 add-apt-repository ppa:ubuntu-toolchain-r/test
 apt-get update
 apt-get install gcc-4.9
