8765ab60e8fcc6c61f2d4557db12380bc8b5e85b
vmoens <vincentmoens@gmail.com>
2023-05-03 09:26:15 +0100

    amend

--- .circleci/unittest/linux_libs/scripts_robohive/install_and_run_test.sh
+++ .circleci/unittest/linux_libs/scripts_robohive/install_and_run_test.sh
@@ -4,7 +4,7 @@ unset PYTORCH_VERSION
 # For unittest, nightly PyTorch is used as the following section,
 # so no need to set PYTORCH_VERSION.
 # In fact, keeping PYTORCH_VERSION forces us to hardcode PyTorch version in config.
-apt-get update && apt-get install -y git wget gcc g++
+apt-get update && apt-get install -y git wget libglew-dev libx11-dev x11proto-dev g++ gcc
 
 set -e
 set -v
@@ -52,3 +52,26 @@ python -c "import tensordict"
 printf "* Installing torchrl\n"
 python setup.py develop
 python -c "import torchrl"
+
+# Extracted from run_test.sh to run once.
+
+export PYTORCH_TEST_WITH_SLOW='1'
+python -m torch.utils.collect_env
+# Avoid error: "fatal: unsafe repository"
+git config --global --add safe.directory '*'
+
+root_dir="$(git rev-parse --show-toplevel)"
+env_dir="${root_dir}/env"
+lib_dir="${env_dir}/lib"
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$lib_dir
+export MKL_THREADING_LAYER=GNU
+
+python .circleci/unittest/helpers/coverage_run_parallel.py -m pytest test/smoke_test.py -v --durations 20
+python .circleci/unittest/helpers/coverage_run_parallel.py -m pytest test/smoke_test_deps.py -v --durations 20 -k 'test_gym'
+
+export DISPLAY=':99.0'
+Xvfb :99 -screen 0 1400x900x24 > /dev/null 2>&1 &
+python .circleci/unittest/helpers/coverage_run_parallel.py -m pytest test/test_libs.py --instafail -v --durations 20 -k "robohive" --error-for-skips
+coverage combine
+coverage xml -i
