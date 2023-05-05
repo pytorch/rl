24abc755c01d6e6beeedf793ec0aa1d3b7a6fcc0
xmaples <5900204+xmaples@users.noreply.github.com>
2023-05-06 04:35:23 +0800

    [BugFix] extract the info dict from a list (#1131)
    Co-authored-by: vmoens <vincentmoens@gmail.com>

--- test/test_libs.py
+++ test/test_libs.py
@@ -100,24 +100,24 @@ ATOL = 1e-1
 
 
 @pytest.mark.skipif(not _has_gym, reason="no gym library found")
-@pytest.mark.parametrize(
-    "env_name",
-    [
-        PONG_VERSIONED,
-        # PENDULUM_VERSIONED,
-        HALFCHEETAH_VERSIONED,
-    ],
-)
-@pytest.mark.parametrize("frame_skip", [1, 3])
-@pytest.mark.parametrize(
-    "from_pixels,pixels_only",
-    [
-        [False, False],
-        [True, True],
-        [True, False],
-    ],
-)
 class TestGym:
+    @pytest.mark.parametrize(
+        "env_name",
+        [
+            PONG_VERSIONED,
+            # PENDULUM_VERSIONED,
+            HALFCHEETAH_VERSIONED,
+        ],
+    )
+    @pytest.mark.parametrize("frame_skip", [1, 3])
+    @pytest.mark.parametrize(
+        "from_pixels,pixels_only",
+        [
+            [False, False],
+            [True, True],
+            [True, False],
+        ],
+    )
     def test_gym(self, env_name, frame_skip, from_pixels, pixels_only):
         if env_name == PONG_VERSIONED and not from_pixels:
             # raise pytest.skip("already pixel")
@@ -176,6 +176,23 @@ class TestGym:
         assert final_seed0 == final_seed2
         assert_allclose_td(tdrollout[0], rollout2, rtol=RTOL, atol=ATOL)
 
+    @pytest.mark.parametrize(
+        "env_name",
+        [
+            PONG_VERSIONED,
+            # PENDULUM_VERSIONED,
+            HALFCHEETAH_VERSIONED,
+        ],
+    )
+    @pytest.mark.parametrize("frame_skip", [1, 3])
+    @pytest.mark.parametrize(
+        "from_pixels,pixels_only",
+        [
+            [False, False],
+            [True, True],
+            [True, False],
+        ],
+    )
     def test_gym_fake_td(self, env_name, frame_skip, from_pixels, pixels_only):
         if env_name == PONG_VERSIONED and not from_pixels:
             # raise pytest.skip("already pixel")
@@ -194,6 +211,37 @@ class TestGym:
             pixels_only=pixels_only,
         )
         check_env_specs(env)
+
+    def test_info_reader(self):
+        try:
+            import gym_super_mario_bros as mario_gym
+        except ImportError as err:
+            try:
+                import gym
+
+                # with 0.26 we must have installed gym_super_mario_bros
+                # Since we capture the skips as errors, we raise a skip in this case
+                # Otherwise, we just return
+                if (
+                    version.parse("0.26.0")
+                    <= version.parse(gym.__version__)
+                    < version.parse("0.27.0")
+                ):
+                    raise pytest.skip(f"no super mario bros: error=\n{err}")
+            except ImportError:
+                pass
+            return
+
+        env = mario_gym.make("SuperMarioBros-v0", apply_api_compatibility=True)
+        env = GymWrapper(env)
+
+        def info_reader(info, tensordict):
+            assert isinstance(info, dict)  # failed before bugfix
+
+        env.info_dict_reader = info_reader
+        env.reset()
+        env.rand_step()
+        env.rollout(3)
 
 
 @implement_for("gym", None, "0.26")
