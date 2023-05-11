4281cf7ddcf9fb5221cbacb3c68a9803773f2f46
vmoens <vincentmoens@gmail.com>
2023-05-11 21:09:49 +0100

    fix

--- torchrl/envs/common.py
+++ torchrl/envs/common.py
@@ -404,8 +404,7 @@ class EnvBase(nn.Module, metaclass=abc.ABCMeta):
     @action_spec.setter
     def action_spec(self, value: TensorSpec) -> None:
         try:
-            if self.__dict__.get('_input_spec', None) is not None:
-                self.input_spec.unlock_()
+            self.input_spec.unlock_()
             try:
                 delattr(self, "_action_key")
             except AttributeError:
@@ -479,8 +478,7 @@ class EnvBase(nn.Module, metaclass=abc.ABCMeta):
     @reward_spec.setter
     def reward_spec(self, value: TensorSpec) -> None:
         try:
-            if self.__dict__.get('_output_spec', None) is not None:
-                self.output_spec.unlock_()
+            self.output_spec.unlock_()
             try:
                 delattr(self, "_reward_key")
             except AttributeError:
@@ -558,8 +556,7 @@ class EnvBase(nn.Module, metaclass=abc.ABCMeta):
     @done_spec.setter
     def done_spec(self, value: TensorSpec) -> None:
         try:
-            if self.__dict__.get('_output_spec', None) is not None:
-                self.output_spec.unlock_()
+            self.output_spec.unlock_()
             try:
                 delattr(self, "_done_key")
             except AttributeError:
@@ -608,8 +605,7 @@ class EnvBase(nn.Module, metaclass=abc.ABCMeta):
     @observation_spec.setter
     def observation_spec(self, value: TensorSpec) -> None:
         try:
-            if self.__dict__.get('_output_spec', None) is not None:
-                self.output_spec.unlock_()
+            self.output_spec.unlock_()
             if not isinstance(value, CompositeSpec):
                 raise TypeError("The type of an observation_spec must be Composite.")
             elif value.shape[: len(self.batch_size)] != self.batch_size:
@@ -635,8 +631,7 @@ class EnvBase(nn.Module, metaclass=abc.ABCMeta):
     @state_spec.setter
     def state_spec(self, value: TensorSpec) -> None:
         try:
-            if self.__dict__.get('_input_spec', None) is not None:
-                self.input_spec.unlock_()
+            self.input_spec.unlock_()
             if not isinstance(value, CompositeSpec):
                 raise TypeError("The type of an state_spec must be Composite.")
             elif value.shape[: len(self.batch_size)] != self.batch_size:
