f3e2be991d588b99bf6dd2829fcf9986f726e5b7
Omkar Salpekar <osalpekar@fb.com>
2023-05-22 13:20:42 -0700

    Merge Distributed and Linux GPU job

--- .circleci/unittest/linux/scripts/run_test.sh
+++ .circleci/unittest/linux/scripts/run_test.sh
@@ -34,7 +34,7 @@ print(model_utils)
 
 python3 .circleci/unittest/helpers/coverage_run_parallel.py -m pytest test/smoke_test.py -v --durations 200
 python3 .circleci/unittest/helpers/coverage_run_parallel.py -m pytest test/smoke_test_deps.py -v --durations 200 -k 'test_gym or test_dm_control_pixels or test_dm_control or test_tb'
-python3 .circleci/unittest/helpers/coverage_run_parallel.py -m pytest --instafail -v --durations 200 --ignore test/test_distributed.py --ignore test/test_rlhf.py -k VC1
-python3 .circleci/unittest/helpers/coverage_run_parallel.py -m pytest --instafail -v --durations 200 --ignore test/test_distributed.py --ignore test/test_rlhf.py33
+python3 .circleci/unittest/helpers/coverage_run_parallel.py -m pytest --instafail -v --durations 200 --ignore test/test_rlhf.py -k VC1
+python3 .circleci/unittest/helpers/coverage_run_parallel.py -m pytest --instafail -v --durations 200 --ignore test/test_rlhf.py33
 coverage combine
 coverage xml -i
