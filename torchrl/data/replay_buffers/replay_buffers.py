b6639400b12fbbd83b26576795d2b0876dc1964f
vmoens <vincentmoens@gmail.com>
2023-05-25 21:38:31 +0100

    fix

--- torchrl/data/replay_buffers/replay_buffers.py
+++ torchrl/data/replay_buffers/replay_buffers.py
@@ -706,6 +706,11 @@ class TensorDictReplayBuffer(ReplayBuffer):
             stacked_td.set("_data", self._transform.inv(stacked_td.get("_data")))
 
         index = super()._extend(stacked_td)
+        # stacked_td.set(
+        #     "index",
+        #     torch.tensor(index, dtype=torch.int, device=stacked_td.device),
+        #     inplace=True,
+        # )
         self.update_tensordict_priority(stacked_td)
         return index
 
@@ -721,6 +726,9 @@ class TensorDictReplayBuffer(ReplayBuffer):
         else:
             priority = self._get_priority(data)
         index = data.get("index")
+        while index.shape != priority.shape:
+            # reduce index
+            index = index[..., 0]
         self.update_priority(index, priority)
 
     def sample(
