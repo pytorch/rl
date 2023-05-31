3619725902ef94b08ac0416ad3c71649c78f8619
Tom Begley <tomcbegley@gmail.com>
2023-05-31 14:44:10 +0000

    Use trlx script to test our sft and reward pipelines

--- examples/nanoChatGPT/models/actor_critic.py
+++ examples/nanoChatGPT/models/actor_critic.py
@@ -8,6 +8,7 @@ from torch.distributions.categorical import Categorical
 
 from torchrl.modules import (
     ActorValueOperator,
+    MaskedCategorical,
     SafeProbabilisticModule,
     SafeProbabilisticTensorDictSequential,
 )
@@ -16,6 +17,17 @@ from .transformer import init_transformer
 
 __all__ = ["ActorCritic", "init_actor_critic"]
 
+
+class Masker(nn.Module):
+    def __init__(self, k):
+        super().__init__()
+        self.k = k
+
+    def forward(self, logits):
+        _, top_indices = torch.topk(logits, k=self.k, sorted=False)
+        mask = torch.zeros_like(logits, dtype=torch.bool)
+        mask[torch.arange(logits.shape[0])[:, None], top_indices] = True
+        return mask
 
 class ActorCritic(ActorValueOperator):
     def __init__(self, base_model):
@@ -34,14 +46,15 @@ class ActorCritic(ActorValueOperator):
             TensorDictModule(base_model, in_keys=["input_ids", "attention_mask"], out_keys=["x"]),
             TensorDictModule(lambda x: x[:, -1, :], in_keys=["x"], out_keys=["x"]),
         )
-
+        masker = TensorDictModule(Masker(k=50), in_keys=["logits"], out_keys=["mask"])
         actor_head = TensorDictModule(actor_head, in_keys=["x"], out_keys=["logits"])
         actor_head = SafeProbabilisticTensorDictSequential(
             actor_head,
+            masker,
             SafeProbabilisticModule(
-                in_keys=["logits"],
+                in_keys=["logits", "mask"],
                 out_keys=["action"],
-                distribution_class=Categorical,
+                distribution_class=MaskedCategorical,
                 return_log_prob=True,
             ),
         )
@@ -53,10 +66,10 @@ class ActorCritic(ActorValueOperator):
 
 
 def init_actor_critic(config):
-    model_base = init_transformer(
+    base_model = init_transformer(
         config, as_tensordictmodule=False, skip_compilation=True
     )
-    a2c_model = ActorCritic(model_base)
+    a2c_model = ActorCritic(base_model)
     a2c_model.to(config["device"])
     actor = a2c_model.get_policy_operator()
     critic = a2c_model.get_value_operator()
