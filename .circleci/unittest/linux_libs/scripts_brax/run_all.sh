130b74baa9ec8453c5e515eda7f4e57c0f91f16e
Omkar Salpekar <osalpekar@fb.com>
2023-06-02 15:36:14 -0700

    relative paths in combined script

--- .circleci/unittest/linux_libs/scripts_brax/run_all.sh
+++ .circleci/unittest/linux_libs/scripts_brax/run_all.sh
@@ -2,7 +2,8 @@
 
 set -euxo pipefail
 
-bash setup_env.sh
-bash install.sh
-bash run_test.sh
-bash post_process.sh
+this_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+bash ${this_dir}/setup_env.sh
+bash ${this_dir}/install.sh
+bash ${this_dir}/run_test.sh
+bash ${this_dir}/post_process.sh
