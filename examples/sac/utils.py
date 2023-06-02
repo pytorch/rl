99f399fbf3201d5e79974fecabf659d7b8f68fcb
vmoens <vincentmoens@gmail.com>
2023-06-02 17:59:33 +0100

    amend

--- examples/sac/utils.py
+++ examples/sac/utils.py
@@ -130,6 +130,8 @@ def make_sac_agent(cfg, train_env, eval_env, device):
     # Define Actor Network
     in_keys = ["observation"]
     action_spec = train_env.action_spec
+    if train_env.batch_size:
+        action_spec = action_spec[(0,) * len(train_env.batch_size)]
     actor_net_kwargs = {
         "num_cells": cfg.network.hidden_sizes,
         "out_features": 2 * action_spec.shape[-1],
