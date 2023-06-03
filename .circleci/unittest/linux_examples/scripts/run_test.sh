b1df6cd4fc926b48b5857707fb9a488a6f13471b
vmoens <vincentmoens@gmail.com>
2023-06-03 07:45:18 +0100

    amend

--- .circleci/unittest/linux_examples/scripts/run_test.sh
+++ .circleci/unittest/linux_examples/scripts/run_test.sh
@@ -184,8 +184,7 @@ python .circleci/unittest/helpers/coverage_run_parallel.py examples/dqn/dqn.py \
   optim_steps_per_batch=1 \
   record_video=True \
   record_frames=4 \
-  buffer_size=120 \
-  logger.backend=
+  buffer_size=120
 python .circleci/unittest/helpers/coverage_run_parallel.py examples/ppo/ppo.py \
   env.num_envs=1 \
   collector.total_frames=48 \
