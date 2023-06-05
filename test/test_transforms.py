3da6e67a728d6630b76444936b12772ef078220b
vmoens <vincentmoens@gmail.com>
2023-06-05 10:09:34 +0200

    amend

--- test/test_transforms.py
+++ test/test_transforms.py
@@ -6010,7 +6010,7 @@ class TestVIP(TransformBase):
 
 @pytest.mark.skipif(not _has_vcmodels, reason="vc_models not installed")
 @pytest.mark.skipif(not torch.cuda.device_count(), reason="VC1 should run on cuda")
-@pytest.mark.parametrize("device", ['cuda:0'])
+@pytest.mark.parametrize("device", [torch.device('cuda:0')])
 class TestVC1(TransformBase):
     def test_transform_inverse(self, device):
         raise pytest.skip("no inverse for VC1Transform")
