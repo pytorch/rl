368b041479fad8d3ec8bbcaf5a1188114059c7c3
vmoens <vincentmoens@gmail.com>
2023-06-09 13:19:42 +0100

    amend

--- torchrl/envs/utils.py
+++ torchrl/envs/utils.py
@@ -199,7 +199,10 @@ def step_mdp(
         td_keys = {"action"}
 
     if td_keys:
-        out.update(tensordict.select(*td_keys))
+        # update does some checks that we can spare
+        # out.update(tensordict.select(*td_keys))
+        for key in td_keys:
+            out._set(key, tensordict.get(key))
     if next_tensordict is not None:
         return next_tensordict.update(out)
     else:
