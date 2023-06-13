996685244d1a0e165d157af7840686466695fa3d
vmoens <vincentmoens@gmail.com>
2023-06-13 14:40:18 +0100

    amend

--- .circleci/unittest/linux_libs/scripts_robohive/setup_env.sh
+++ .circleci/unittest/linux_libs/scripts_robohive/setup_env.sh
@@ -77,6 +77,6 @@ conda env update --file "${this_dir}/environment.yml" --prune
 #conda install -c conda-forge fltk -y
 
 #pip install git+https://github.com/vmoens/mj_envs@patch-2
-pip install git+https://github.com/vikashplus/robohive
+pip install git+https://github.com/vikashplus/robohive@dev
 
 #pip uninstall free-mujoco-py -y
