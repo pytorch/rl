e7bc46f3f4b4bbaf9fc0e92c63b8ce47c37ad610
Matteo Bettini <mb2389@cl.cam.ac.uk>
2023-06-14 10:39:32 -0400

    working at new iql

--- examples/multiagent/iql.py
+++ examples/multiagent/iql.py
@@ -4,6 +4,7 @@ import torch
 
 import wandb
 from models.mlp import MultiAgentMLP
+from tensordict.nn import TensorDictModule
 from torch import nn
 from torchrl.collectors import SyncDataCollector
 from torchrl.data.replay_buffers import ReplayBuffer
@@ -11,7 +12,7 @@ from torchrl.data.replay_buffers.samplers import SamplerWithoutReplacement
 from torchrl.data.replay_buffers.storages import LazyTensorStorage
 from torchrl.envs.libs.vmas import VmasEnv
 from torchrl.envs.utils import ExplorationType, set_exploration_type
-from torchrl.modules import EGreedyWrapper, QValueActor
+from torchrl.modules import EGreedyWrapper, QValueActor, QValueModule, SafeSequential
 from torchrl.objectives import DQNLoss, ValueEstimators
 from torchrl.record.loggers import generate_exp_name
 from torchrl.record.loggers.wandb import WandbLogger
@@ -32,7 +33,7 @@ def train(seed):
     torch.manual_seed(seed)
 
     # Log
-    log = True
+    log = False
 
     # Sampling
     frames_per_batch = 60_000  # Frames sampled each sampling iteration
@@ -98,8 +99,8 @@ def train(seed):
     env_config.update({"n_agents": env.n_agents, "scenario_name": scenario_name})
 
     # Policy
-    qnet = MultiAgentMLP(
-        n_agent_inputs=env.observation_spec["observation"].shape[-1],
+    net = MultiAgentMLP(
+        n_agent_inputs=env.observation_spec["agents", "observation"].shape[-1],
         n_agent_outputs=env.action_spec.space.n,
         n_agents=env.n_agents,
         centralised=False,
@@ -109,22 +110,35 @@ def train(seed):
         num_cells=256,
         activation_class=nn.Tanh,
     )
-    qnet = QValueActor(
-        module=qnet,
-        spec=env.unbatched_input_spec["action"],
-        in_keys=["observation"],
+    module = TensorDictModule(
+        net, in_keys=[("agents", "observation")], out_keys=[("agents", "action_value")]
     )
+    value_module = QValueModule(
+        action_value_key=("agents", "action_value"),
+        out_keys=[
+            env.action_key,
+            ("agents", "action_value"),
+            ("agents", "chosen_action_value"),
+        ],
+        action_space=env.unbatched_action_spec[env.action_key],
+    )
+    qnet = SafeSequential(module, value_module)
 
-    qnet = EGreedyWrapper(
-        qnet, eps_init=0.3, eps_end=0, annealing_num_steps=int(total_frames * (1 / 2))
+    qnet_explore = EGreedyWrapper(
+        qnet,
+        eps_init=0.3,
+        eps_end=0,
+        annealing_num_steps=int(total_frames * (1 / 2)),
+        action_key=env.action_key,
+        spec=env.action_spec,
     )
 
     with set_exploration_type(ExplorationType.RANDOM):
-        qnet(env.reset().to(training_device))
+        qnet_explore(env.reset().to(training_device))
 
     collector = SyncDataCollector(
         env,
-        qnet,
+        qnet_explore,
         device=vmas_device,
         storing_device=training_device,
         frames_per_batch=frames_per_batch,
@@ -138,7 +152,15 @@ def train(seed):
         collate_fn=lambda x: x,  # Make it not clone when sampling
     )
 
-    loss_module = DQNLoss(qnet)
+    loss_module = DQNLoss(
+        qnet,
+        action_space=env.unbatched_action_spec[env.action_key],
+    )
+    loss_module.set_keys(
+        action_value=("agents", "action_value"),
+        action=env.action_key,
+        value=("agents", "chosen_action_value"),
+    )
     loss_module.make_value_estimator(ValueEstimators.TD0, gamma=config["gamma"])
 
     optim = torch.optim.Adam(loss_module.parameters(), config["lr"])
