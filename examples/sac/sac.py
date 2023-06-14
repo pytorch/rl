c6976ead6a703a3618c451db735f3e4966a74b29
vmoens <vincentmoens@gmail.com>
2023-06-14 09:45:06 +0100

    amend

--- examples/sac/sac.py
+++ examples/sac/sac.py
@@ -64,7 +64,7 @@ def main(cfg: "DictConfig"):  # noqa: F821
         batch_size=cfg.optimization.batch_size,
         prb=cfg.replay_buffer.prb,
         buffer_size=cfg.replay_buffer.size,
-        device=device,
+        device="cpu",
     )
 
     # Make Optimizers
@@ -109,7 +109,7 @@ def main(cfg: "DictConfig"):  # noqa: F821
             (actor_losses, q_losses, alpha_losses) = ([], [], [])
             for _ in range(num_updates):
                 # sample from replay buffer
-                sampled_tensordict = replay_buffer.sample().clone()
+                sampled_tensordict = replay_buffer.sample().to(device, non_blocking=True)
 
                 loss_td = loss_module(sampled_tensordict)
 
