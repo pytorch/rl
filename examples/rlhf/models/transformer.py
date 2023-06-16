71c7d3ec9e5462e372ea162800a54ccf00444454
Tom Begley <tomcbegley@gmail.com>
2023-06-16 18:37:10 +0000

    remove unused pathlib import

--- examples/rlhf/models/transformer.py
+++ examples/rlhf/models/transformer.py
@@ -2,13 +2,9 @@
 #
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
-from pathlib import Path
-
 import torch
 from tensordict.nn import TensorDictModule
 from transformers import GPT2LMHeadModel
-
-HERE = Path(__file__).parent
 
 
 def init_transformer(
