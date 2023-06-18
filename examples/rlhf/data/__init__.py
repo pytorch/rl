ca14b76e61912fc5c41bd360b88a2258642a21ec
vmoens <vincentmoens@gmail.com>
2023-06-17 21:27:20 -0500

    amend

--- examples/rlhf/data/__init__.py
+++ examples/rlhf/data/__init__.py
@@ -2,7 +2,6 @@
 #
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
-from torchrl.data.rlhf.dataset import get_reward_dataloader
-from .openai_summarize_tldr import get_prompt_dataloader
+from torchrl.data.rlhf.tldr import get_prompt_dataloader_tldr
 
-__all__ = ["get_prompt_dataloader", "get_reward_dataloader"]
+__all__ = ["get_prompt_dataloader_tldr"]
