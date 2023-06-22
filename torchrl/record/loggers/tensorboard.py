699d09c4a0edca0d7d1cfe16ed072330a648040d
vmoens <vincentmoens@gmail.com>
2023-06-22 08:01:25 +0100

    amend

--- torchrl/record/loggers/tensorboard.py
+++ torchrl/record/loggers/tensorboard.py
@@ -3,6 +3,7 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 import os
+from typing import Sequence
 
 from torch import Tensor
 
