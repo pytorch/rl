402a75001fea83b8db3f0f93ae46b33296497e42
Tom Begley <tomcbegley@gmail.com>
2023-06-26 10:11:03 +0000

    Add reward model test

--- test/test_rlhf.py
+++ test/test_rlhf.py
@@ -10,6 +10,7 @@ from pathlib import Path
 import datasets
 import numpy as np
 import pytest
+import torch
 
 from _utils_internal import get_default_devices
 from tensordict import is_tensor_collection, MemmapTensor, TensorDict
@@ -26,6 +27,7 @@ from torchrl.data.rlhf.reward import (
     PairwiseDataset,
     pre_tokenization_hook,
 )
+from torchrl.modules.models.rlhf import GPT2RewardModel
 
 HERE = Path(__file__).parent
 
@@ -162,6 +164,37 @@ def test_get_dataloader(
         assert not is_tensor_collection(dl)
     else:
         assert not is_tensor_collection(dl)
+
+
+@pytest.mark.parametrize("batch_size", [5, 6])
+@pytest.mark.parametrize("block_size", [15, 50])
+@pytest.mark.parametrize("device", get_default_devices())
+def test_reward_model(tmpdir, batch_size, block_size, device):
+    dataset_name = f"{HERE}/datasets_mini/openai_summarize_tldr"
+    tensorclass_type = PromptData
+    dl = get_dataloader(
+        batch_size,
+        block_size,
+        tensorclass_type,
+        device,
+        dataset_name=dataset_name,
+        infinite=True,
+        prefetch=0,
+        split="train",
+        root_dir=tmpdir,
+        from_disk=True,
+    )
+
+    reward_model = GPT2RewardModel().to(device)
+
+    batch = next(dl)
+
+    rewards, end_scores = reward_model(
+        input_ids=batch.input_ids, attention_mask=batch.attention_mask
+    )
+
+    assert rewards.shape == torch.Size([batch_size, block_size])
+    assert end_scores.shape == torch.Size([batch_size])
 
 
 if __name__ == "__main__":
