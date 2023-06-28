70145968d7553ac04e3134b558221943a05f1f5f
vmoens <vincentmoens@gmail.com>
2023-06-28 19:37:35 +0200

    amend

--- .circleci/unittest/windows_optdepts/scripts/run_test.sh
+++ .circleci/unittest/windows_optdepts/scripts/run_test.sh
@@ -9,4 +9,4 @@ this_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source "$this_dir/set_cuda_envs.sh"
 
 python -m torch.utils.collect_env
-pytest --junitxml=test-results/junit.xml -v --durations 200  --ignore "test/test_distributed.py test/test_rlhf.py"
+pytest --junitxml=test-results/junit.xml -v --durations 200  --ignore test/test_distributed.py --ignore test/test_rlhf.py
