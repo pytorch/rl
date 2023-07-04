c2386df2b51acee713eb950c0529d995c5f03572
vmoens <vincentmoens@gmail.com>
2023-07-04 13:43:03 +0100

    amend

--- .circleci/unittest/linux/scripts/install.sh
+++ .circleci/unittest/linux/scripts/install.sh
@@ -51,6 +51,9 @@ python -c """
 from torchrl.envs.transforms.vc1 import VC1Transform
 VC1Transform.install_vc_models(auto_exit=True)
 """
+
 python -c """
 import vc_models
+from vc_models.models.vit import model_utils
+print(model_utils)
 """
