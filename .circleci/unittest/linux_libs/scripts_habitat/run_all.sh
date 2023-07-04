08cc14e7a24ecf9d29039211324c3d1fc7a66ad6
vmoens <vincentmoens@gmail.com>
2023-07-04 11:55:14 +0100

    amend

--- .circleci/unittest/linux_libs/scripts_habitat/run_all.sh
+++ .circleci/unittest/linux_libs/scripts_habitat/run_all.sh
@@ -5,10 +5,13 @@ set -v
 
 # Avoid error: "fatal: unsafe repository"
 apt-get update
-apt-get install -y git wget gcc c++ git wget freeglut3 freeglut3-dev
+apt-get install -y git wget
+apt-get install -y gcc c++
 
 this_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 bash ${this_dir}/setup_env.sh
 bash ${this_dir}/install.sh
+
+apt-get install -y freeglut3 freeglut3-dev
 bash ${this_dir}/run_test.sh
 bash ${this_dir}/post_process.sh
