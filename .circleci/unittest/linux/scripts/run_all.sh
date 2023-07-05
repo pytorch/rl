70a57054c184f0b61165f9a4ead84b68ed448f6b
vmoens <vincentmoens@gmail.com>
2023-07-05 10:51:21 +0100

    amend

--- .circleci/unittest/linux/scripts/run_all.sh
+++ .circleci/unittest/linux/scripts/run_all.sh
@@ -9,14 +9,27 @@ set -v
 
 if [[ $OSTYPE != 'darwin'* ]]; then
   apt-get update && apt-get upgrade -y
-  apt-get install -y vim git wget g++ gcc
+  apt-get install -y vim git wget
 
   apt-get install -y libglfw3 libgl1-mesa-glx libosmesa6 libglew-dev
   apt-get install -y libglvnd0 libgl1 libglx0 libegl1 libgles2
+
+  if [ "${CU_VERSION:-}" == cpu ] ; then
+    # solves version `GLIBCXX_3.4.29' not found for tensorboard
+    apt-get install -y gcc-4.9
+    apt-get upgrade -y libstdc++6
+    apt-get dist-upgrade -y
+  else
+    apt-get install -y g++ gcc
+  fi
+
 fi
 
 this_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-cp $this_dir/10_nvidia.json /usr/share/glvnd/egl_vendor.d/10_nvidia.json
+if [[ $OSTYPE != 'darwin'* ]]; then
+  # from cudagl docker image
+  cp $this_dir/10_nvidia.json /usr/share/glvnd/egl_vendor.d/10_nvidia.json
+fi
 
 # Avoid error: "fatal: unsafe repository"
 git config --global --add safe.directory '*'
