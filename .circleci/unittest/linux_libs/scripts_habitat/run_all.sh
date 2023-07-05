a83e03a7531aa3b14abc32659da68275ed477308
vmoens <vincentmoens@gmail.com>
2023-07-05 21:12:05 +0100

    amend

--- .circleci/unittest/linux_libs/scripts_habitat/run_all.sh
+++ .circleci/unittest/linux_libs/scripts_habitat/run_all.sh
@@ -11,8 +11,8 @@ apt-get install -y libglfw3 libgl1-mesa-glx libosmesa6 libglew-dev
 apt-get install -y libglvnd0 libgl1 libglx0 libegl1 libgles2
 
 apt-get install -y g++ gcc
-apt-get upgrade -y libstdc++6
-apt-get install -y libgcc
+#apt-get upgrade -y libstdc++6
+#apt-get install -y libgcc
 apt-get dist-upgrade -y
 
 this_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
