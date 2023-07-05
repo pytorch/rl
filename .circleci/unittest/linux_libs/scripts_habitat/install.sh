68d96c1044990431afda053da146fd256b527ea5
vmoens <vincentmoens@gmail.com>
2023-07-05 20:58:49 +0100

    amend

--- .circleci/unittest/linux_libs/scripts_habitat/install.sh
+++ .circleci/unittest/linux_libs/scripts_habitat/install.sh
@@ -8,35 +8,22 @@ set -v
 eval "$(./conda/bin/conda shell.bash hook)"
 conda activate ./env
 
-if [ "${CU_VERSION:-}" == cpu ] ; then
-    version="cpu"
-else
-    if [[ ${#CU_VERSION} -eq 4 ]]; then
-        CUDA_VERSION="${CU_VERSION:2:1}.${CU_VERSION:3:1}"
-    elif [[ ${#CU_VERSION} -eq 5 ]]; then
-        CUDA_VERSION="${CU_VERSION:2:2}.${CU_VERSION:4:1}"
-    fi
-    echo "Using CUDA $CUDA_VERSION as determined by CU_VERSION ($CU_VERSION)"
-    version="$(python -c "print('.'.join(\"${CUDA_VERSION}\".split('.')[:2]))")"
+if [[ ${#CU_VERSION} -eq 4 ]]; then
+    CUDA_VERSION="${CU_VERSION:2:1}.${CU_VERSION:3:1}"
+elif [[ ${#CU_VERSION} -eq 5 ]]; then
+    CUDA_VERSION="${CU_VERSION:2:2}.${CU_VERSION:4:1}"
 fi
-
+echo "Using CUDA $CUDA_VERSION as determined by CU_VERSION ($CU_VERSION)"
+version="$(python -c "print('.'.join(\"${CUDA_VERSION}\".split('.')[:2]))")"
 
 # submodules
 git submodule sync && git submodule update --init --recursive
 
 printf "Installing PyTorch with %s\n" "${CU_VERSION}"
-if [ "${CU_VERSION:-}" == cpu ] ; then
-    # conda install -y pytorch torchvision cpuonly -c pytorch-nightly
-    # use pip to install pytorch as conda can frequently pick older release
-#    conda install -y pytorch cpuonly -c pytorch-nightly
-    pip3 install --pre torch --extra-index-url https://download.pytorch.org/whl/nightly/cpu --force-reinstall
-else
-    pip3 install --pre torch --extra-index-url https://download.pytorch.org/whl/nightly/cu116 --force-reinstall
-fi
+pip3 install --pre torch --extra-index-url https://download.pytorch.org/whl/nightly/cu116 --force-reinstall
 
 # install tensordict
 pip3 install git+https://github.com/pytorch-labs/tensordict.git
-#pip3 install tensordict-nightly
 
 # smoke test
 python3 -c "import functorch;import tensordict"
--- .circleci/unittest/linux_libs/scripts_habitat/run_all.sh
+++ .circleci/unittest/linux_libs/scripts_habitat/run_all.sh
@@ -10,9 +10,10 @@ apt-get install -y vim git wget
 apt-get install -y libglfw3 libgl1-mesa-glx libosmesa6 libglew-dev
 apt-get install -y libglvnd0 libgl1 libglx0 libegl1 libgles2
 
-apt-get upgrade -y libstdc++6
-apt-get dist-upgrade -y
 apt-get install -y g++ gcc
+apt-get upgrade -y libstdc++6
+apt-get install -y libgcc
+apt-get dist-upgrade -y
 
 this_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 # from cudagl docker image
