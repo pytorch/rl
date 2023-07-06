f8b160e5e6ea40d7455976b8c93c39504ea12f80
vmoens <vincentmoens@gmail.com>
2023-07-06 09:09:04 +0100

    amend

--- .circleci/unittest/linux_libs/scripts_habitat/run_test.sh
+++ .circleci/unittest/linux_libs/scripts_habitat/run_test.sh
@@ -37,7 +37,7 @@ conda deactivate && conda activate ./env
 # we can install this now but not before installing tensordict and torchrl, g++ version will break the compilation
 # https://stackoverflow.com/questions/72540359/glibcxx-3-4-30-not-found-for-librosa-in-conda-virtual-environment-after-tryin
 conda install -y -c conda-forge gcc=12.1.0
-conda install -y -c conda-forge libstdcxx-ng=12
+#conda install -y -c conda-forge libstdcxx-ng=12
 
 # this workflow only tests the libs
 python -c "import habitat;import habitat.gym"
