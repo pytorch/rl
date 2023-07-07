3b16598b644de8d9d845a5b59e76e4119560c36f
vmoens <vincentmoens@gmail.com>
2023-07-07 15:57:26 +0100

    lint

--- torchrl/collectors/collectors.py
+++ torchrl/collectors/collectors.py
@@ -813,9 +813,7 @@ class SyncDataCollector(DataCollectorBase):
             traj_ids[traj_done_or_terminated] = traj_ids.max() + torch.arange(
                 1, traj_done_or_terminated.sum() + 1, device=traj_ids.device
             )
-            self._tensordict.set(
-                ("collector", "traj_ids"), traj_ids
-            )
+            self._tensordict.set(("collector", "traj_ids"), traj_ids)
 
     @torch.no_grad()
     def rollout(self) -> TensorDictBase:
