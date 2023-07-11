42e6bb23baa7462bd88912865b2cf8128c5946e0
Matteo Bettini <matbet@meta.com>
2023-07-11 13:50:32 +0100

    tests
    Signed-off-by: Matteo Bettini <matbet@meta.com>

--- torchrl/objectives/multiagent/qmixer.py
+++ torchrl/objectives/multiagent/qmixer.py
@@ -123,6 +123,7 @@ class QMixerLoss(LossModule):
         ...        ),
         ...    },
         ...    [32],
+        ... )
         >>> loss(qnet(td))
         TensorDict(
             fields={
