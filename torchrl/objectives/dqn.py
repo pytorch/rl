9a7b21629bd85ab449cc2f4350b930bdbc462143
Matteo Bettini <matbet@meta.com>
2023-07-12 11:49:00 +0100

    device dqn
    Signed-off-by: Matteo Bettini <matbet@meta.com>

--- torchrl/objectives/dqn.py
+++ torchrl/objectives/dqn.py
@@ -282,13 +282,23 @@ class DQNLoss(LossModule):
             a tensor containing the DQN loss.
 
         """
-        td_copy = tensordict.clone(False)
+        if self.device is not None:
+            warnings.warn(
+                "The use of a device for the objective function will soon be deprecated",
+                category=DeprecationWarning,
+            )
+            device = self.device
+        else:
+            device = tensordict.device
+        tddevice = tensordict.to(device)
+
+        td_copy = tddevice.clone(False)
         self.value_network(
             td_copy,
             params=self.value_network_params,
         )
 
-        action = tensordict.get(self.tensor_keys.action)
+        action = tddevice.get(self.tensor_keys.action)
         pred_val = td_copy.get(self.tensor_keys.action_value)
 
         if self.action_space == "categorical":
