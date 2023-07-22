d6ccbb40a48b7edce2263f42587166e39b1621fa
Omkar Salpekar <osalpekar@fb.com>
2023-07-21 17:00:03 -0700

    remove unneeded testing

--- .circleci/unittest/linux_stable/scripts/run_test.sh
+++ .circleci/unittest/linux_stable/scripts/run_test.sh
@@ -21,12 +21,12 @@ export MKL_THREADING_LAYER=GNU
 export CKPT_BACKEND=torch
 # export PYGLFW_LIBRARY="/usr/lib64/libglfw.so.3"
 
-echo "TESTING GYM REGKEYS"
-echo "MUJOCO_GL"
-echo $MUJOCO_GL
-# python -c "import glfw"
-python -c "from ale_py import ALEInterface"
-python -c "import gymnasium; print(gymnasium.envs.registration.registry.keys())"
+# echo "TESTING GYM REGKEYS"
+# echo "MUJOCO_GL"
+# echo $MUJOCO_GL
+# # python -c "import glfw"
+# python -c "from ale_py import ALEInterface"
+# python -c "import gymnasium; print(gymnasium.envs.registration.registry.keys())"
 
 python .circleci/unittest/helpers/coverage_run_parallel.py -m pytest test/smoke_test.py -v --durations 200
 python .circleci/unittest/helpers/coverage_run_parallel.py -m pytest test/smoke_test_deps.py -v --durations 200 -k 'test_gym or test_dm_control_pixels or test_dm_control or test_tb'
