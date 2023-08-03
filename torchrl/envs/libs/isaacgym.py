b656b865219586ca353aaf08a7c52322518ba430
vmoens <vincentmoens@gmail.com>
2023-08-03 16:06:43 +0100

    amend

--- torchrl/envs/libs/isaacgym.py
+++ torchrl/envs/libs/isaacgym.py
@@ -15,8 +15,8 @@ from torchrl.envs.libs.gym import GymWrapper
 class IsaacGymWrapper(GymWrapper):
     def __init__(self, env, *, num_envs, **kwargs):
         super().__init__(env, **kwargs)
-        self.input_spec = self.input_spec.expand(num_envs, *self.input_spec.shape)
-        self.output_spec = self.output_spec.expand(num_envs, *self.output_spec.shape)
+        self.__dict__['_input_spec'] = self.input_spec.expand(num_envs, *self.input_spec.shape)
+        self.__dict__['_output_spec'] = self.output_spec.expand(num_envs, *self.output_spec.shape)
 
     def read_action(self, action):
         """Reads the action obtained from the input TensorDict and transforms it in the format expected by the contained environment.
