04c246b109cee1b87665e32c8a395d4a4c9e0839
vmoens <vincentmoens@gmail.com>
2023-08-09 09:55:05 -0400

    typo

--- benchmarks/ecosystem/bench.py
+++ benchmarks/ecosystem/bench.py
@@ -1,4 +1,4 @@
-len(workers)# Copyright (c) Meta Platforms, Inc. and affiliates.
+# Copyright (c) Meta Platforms, Inc. and affiliates.
 #
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
