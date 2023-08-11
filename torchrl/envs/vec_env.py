940f137c84d79be078bd47b841272c513fc574a6
vmoens <vincentmoens@gmail.com>
2023-08-11 05:21:15 -0400

    amend

--- torchrl/envs/vec_env.py
+++ torchrl/envs/vec_env.py
@@ -385,11 +385,11 @@ class _BatchedEnv(EnvBase):
         self._selected_keys.add("_reset")
 
         # input keys
-        self._selected_input_keys = self._env_input_keys
+        self._selected_input_keys = {_unravel_key_to_tuple(key) for key in self._env_input_keys}
         # output keys after reset
-        self._selected_reset_keys = self._env_obs_keys + [self.done_key] + ["_reset"]
+        self._selected_reset_keys = {_unravel_key_to_tuple(key) for key in self._env_obs_keys + [self.done_key] + ["_reset"]}
         # output keys after step
-        self._selected_step_keys = self._env_output_keys
+        self._selected_step_keys = {_unravel_key_to_tuple(key) for key in self._env_output_keys}
 
         if self._single_task:
             shared_tensordict_parent = shared_tensordict_parent.select(
@@ -841,6 +841,7 @@ class ParallelEnv(_BatchedEnv):
             cur_td = _fuse_tensordicts(
                 self.shared_tensordict_parent,
                 tensordict,
+                selected=self._selected_reset_keys,
                 excluded=(("next",), ("_reset",)),
             )
         # elif done.any():
@@ -863,6 +864,7 @@ class ParallelEnv(_BatchedEnv):
             cur_td = _fuse_tensordicts(
                 next_tensordict,
                 tensordict,
+                selected=self._selected_reset_keys,
                 excluded=(_unravel_key_to_tuple(self.reward_key),),
             )
         if "next" in tensordict.keys():
@@ -1130,8 +1132,6 @@ def _run_worker_pipe_shared_mem(
         # _unravel_key_to_tuple(env.done_key),
         _unravel_key_to_tuple(env.action_key),
     }
-    _selected_reset_keys = {_unravel_key_to_tuple(key) for key in _selected_reset_keys}
-    _selected_step_keys = {_unravel_key_to_tuple(key) for key in _selected_step_keys}
 
     while True:
         try:
