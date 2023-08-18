c51993f96fe14e0bc627f3dbe6dce9f105cbc5c8
BY571 <sebastian.dittert@gmx.de>
2023-08-18 09:56:43 +0200

    update td3

--- examples/td3/utils.py
+++ examples/td3/utils.py
@@ -237,18 +237,13 @@ def make_loss_module(cfg, model):
 
 
 def make_optimizer(cfg, loss_module):
-    critic_params = list(loss_module.qvalue_network_params.flatten_keys().values())
-    actor_params = list(loss_module.actor_network_params.flatten_keys().values())
+    critic_params = loss_module.qvalue_network_params.flatten_keys().values()
+    actor_params = loss_module.actor_network_params.flatten_keys().values()
 
-    optimizer_actor = optim.Adam(
-        actor_params,
-        lr=cfg.optimization.lr,  # weight_decay=cfg.optimization.weight_decay,
-        # eps=1e-4
-    )
+    optimizer_actor = optim.Adam(actor_params, lr=cfg.optimization.lr, eps=1e-4)
     optimizer_critic = optim.Adam(
         critic_params,
         lr=cfg.optimization.lr,
-        # eps=1e-4,
-        # weight_decay=cfg.optimization.weight_decay,
+        eps=1e-4,
     )
     return optimizer_actor, optimizer_critic
