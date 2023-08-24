5031e5dd06ebee135d8668ba4690b4d779878e9c
Matteo Bettini <matbet@meta.com>
2023-08-24 11:45:50 +0100

    ci
    Signed-off-by: Matteo Bettini <matbet@meta.com>

--- .circleci/unittest/linux_libs/scripts_pettingzoo/setup_env.sh
+++ .circleci/unittest/linux_libs/scripts_pettingzoo/setup_env.sh
@@ -7,8 +7,6 @@
 
 set -e
 
-apt-get update && apt-get install swig3.0
-ln -s /usr/bin/swig3.0 /usr/bin/swig
 
 this_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 # Avoid error: "fatal: unsafe repository"
