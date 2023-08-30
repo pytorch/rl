01fc27a0fe70fc63a9db9ee724a0f93f44866084
Matteo Bettini <55539777+matteobettini@users.noreply.github.com>
2023-08-30 13:24:53 +0100

    Update torchrl/envs/common.py
    Co-authored-by: Vincent Moens <vincentmoens@gmail.com>

--- torchrl/envs/common.py
+++ torchrl/envs/common.py
@@ -412,7 +412,7 @@ class EnvBase(nn.Module, metaclass=abc.ABCMeta):
         - "_observation_spec": the spec of all other environment outputs
 
         This attibute is locked and should be read-oonly.
-        Instead, to set the specs contained in it, use the respecitve properties.
+        Instead, to set the specs contained in it, use the respective properties.
 
         Examples:
             >>> from torchrl.envs.libs.gym import GymEnv
