550d397b88f9c1a9e5f17968e736e347027869ab
albert bou <albertbou92@gmail.com>
2023-09-06 14:59:04 +0200

    vtrace module

---
+++ examples/impala/impala.py
@@ -0,0 +1,215 @@
+"""
+This script reproduces the Proximal Policy Optimization (PPO) Algorithm
+results from Schulman et al. 2017 for the on Atari Environments.
+"""
+import hydra
+
+
+@hydra.main(config_path=".", config_name="config_atari", version_base="1.1")
+def main(cfg: "DictConfig"):  # noqa: F821
+
+    import time
+
+    import numpy as np
+    import torch.optim
+    import tqdm
+
+    from tensordict import TensorDict
+    from torchrl.collectors import SyncDataCollector
+    from torchrl.collectors.distributed import RPCDataCollector
+    from torchrl.data import LazyMemmapStorage, TensorDictReplayBuffer
+    from torchrl.data.replay_buffers.samplers import SamplerWithoutReplacement
+    from torchrl.envs import ExplorationType, set_exploration_type
+    from torchrl.objectives import ClipPPOLoss
+    from torchrl.record.loggers import generate_exp_name, get_logger
+    from torchrl.objectives.value.vtrace import VTrace
+    from utils import make_parallel_env, make_ppo_models
+
+    device = "cpu" if not torch.cuda.is_available() else "cuda"
+
+    # Correct for frame_skip
+    frame_skip = 4
+    total_frames = cfg.collector.total_frames // frame_skip
+    frames_per_batch = cfg.collector.frames_per_batch // frame_skip
+    mini_batch_size = cfg.loss.mini_batch_size // frame_skip
+    test_interval = cfg.logger.test_interval // frame_skip
+
+    # Create models (check utils_atari.py)
+    actor, critic, critic_head = make_ppo_models(cfg.env.env_name)
+    actor, critic, critic_head = (
+        actor.to(device),
+        critic.to(device),
+        critic_head.to(device),
+    )
+
+    # Create collector
+    # collector = RPCDataCollector(
+    #     create_env_fn=[make_parallel_env(cfg.env.env_name, device)] * 2,
+    #     policy=actor,
+    #     frames_per_batch=frames_per_batch,
+    #     total_frames=total_frames,
+    #     storing_device="cpu",
+    #     max_frames_per_traj=-1,
+    #     sync=False,
+    # )
+    collector = SyncDataCollector(
+        create_env_fn=make_parallel_env(cfg.env.env_name, device),
+        policy=actor,
+        frames_per_batch=frames_per_batch,
+        total_frames=total_frames,
+        device=device,
+        storing_device=device,
+        max_frames_per_traj=-1,
+    )
+
+    # Create data buffer
+    sampler = SamplerWithoutReplacement()
+    data_buffer = TensorDictReplayBuffer(
+        storage=LazyMemmapStorage(frames_per_batch),
+        sampler=sampler,
+        batch_size=mini_batch_size,
+    )
+
+    # Create loss and adv modules
+    vtrace_module = VTrace(
+        gamma=cfg.loss.gamma,
+        lmbda=cfg.loss.gae_lambda,
+        value_network=critic,
+        average_gae=False,
+    )
+    loss_module = ClipPPOLoss(
+        actor=actor,
+        critic=critic,
+        clip_epsilon=cfg.loss.clip_epsilon,
+        loss_critic_type=cfg.loss.loss_critic_type,
+        entropy_coef=cfg.loss.entropy_coef,
+        critic_coef=cfg.loss.critic_coef,
+        normalize_advantage=True,
+    )
+
+    # Create optimizer
+    optim = torch.optim.Adam(
+        loss_module.parameters(),
+        lr=cfg.optim.lr,
+        weight_decay=cfg.optim.weight_decay,
+        eps=cfg.optim.eps,
+    )
+
+    # Create logger
+    exp_name = generate_exp_name("PPO", f"{cfg.logger.exp_name}_{cfg.env.env_name}")
+    logger = get_logger(cfg.logger.backend, logger_name="ppo", experiment_name=exp_name)
+
+    # Create test environment
+    test_env = make_parallel_env(cfg.env.env_name, device, is_test=True)
+    test_env.eval()
+
+    # Main loop
+    collected_frames = 0
+    num_network_updates = 0
+    start_time = time.time()
+    pbar = tqdm.tqdm(total=total_frames)
+    num_mini_batches = frames_per_batch // mini_batch_size
+    total_network_updates = (
+            (total_frames // frames_per_batch) * cfg.loss.ppo_epochs * num_mini_batches
+    )
+
+    for data in collector:
+
+        frames_in_batch = data.numel()
+        collected_frames += frames_in_batch * frame_skip
+        pbar.update(data.numel())
+
+        # Train loging
+        episode_rewards = data["next", "episode_reward"][data["next", "done"]]
+        if len(episode_rewards) > 0:
+            logger.log_scalar(
+                "reward_train", episode_rewards.mean().item(), collected_frames
+            )
+
+        # Apply episodic end of life
+        data["done"].copy_(data["end_of_life"])
+        data["next", "done"].copy_(data["next", "end_of_life"])
+
+        losses = TensorDict({}, batch_size=[cfg.loss.ppo_epochs, num_mini_batches])
+        for j in range(cfg.loss.ppo_epochs):
+
+            # Compute VTrace
+            with torch.no_grad():
+                import ipdb; ipdb.set_trace()
+                data = vtrace_module(data)
+            data_reshape = data.reshape(-1)
+
+            # Update the data buffer
+            data_buffer.extend(data_reshape)
+
+            for i, batch in enumerate(data_buffer):
+
+                # Linearly decrease the learning rate and clip epsilon
+                alpha = 1 - (num_network_updates / total_network_updates)
+                if cfg.optim.anneal_lr:
+                    for g in optim.param_groups:
+                        g["lr"] = cfg.optim.lr * alpha
+                if cfg.loss.anneal_clip_epsilon:
+                    loss_module.clip_epsilon.copy_(cfg.loss.clip_epsilon * alpha)
+                num_network_updates += 1
+
+                # Get a data batch
+                batch = batch.to(device)
+
+                # Forward pass PPO loss
+                loss = loss_module(batch)
+                losses[j, i] = loss.select(
+                    "loss_critic", "loss_entropy", "loss_objective"
+                ).detach()
+                loss_sum = (
+                        loss["loss_critic"] + loss["loss_objective"] + loss["loss_entropy"]
+                )
+
+                # Backward pass
+                loss_sum.backward()
+                torch.nn.utils.clip_grad_norm_(
+                    list(loss_module.parameters()), max_norm=cfg.optim.max_grad_norm
+                )
+
+                # Update the networks
+                optim.step()
+                optim.zero_grad()
+
+        losses = losses.apply(lambda x: x.float().mean(), batch_size=[])
+        for key, value in losses.items():
+            logger.log_scalar(key, value.item(), collected_frames)
+        logger.log_scalar("lr", alpha * cfg.optim.lr, collected_frames)
+        logger.log_scalar(
+            "clip_epsilon", alpha * cfg.loss.clip_epsilon, collected_frames
+        )
+
+        # Test logging
+        with torch.no_grad(), set_exploration_type(ExplorationType.MODE):
+            if (collected_frames - frames_in_batch) // test_interval < (
+                    collected_frames // test_interval
+            ):
+                actor.eval()
+                test_rewards = []
+                for _ in range(cfg.logger.num_test_episodes):
+                    td_test = test_env.rollout(
+                        policy=actor,
+                        auto_reset=True,
+                        auto_cast_to_device=True,
+                        break_when_any_done=True,
+                        max_steps=10_000_000,
+                    )
+                    reward = td_test["next", "episode_reward"][td_test["next", "done"]]
+                    test_rewards = np.append(test_rewards, reward.cpu().numpy())
+                    del td_test
+                logger.log_scalar("reward_test", test_rewards.mean(), collected_frames)
+                actor.train()
+
+        collector.update_policy_weights_()
+
+    end_time = time.time()
+    execution_time = end_time - start_time
+    print(f"Training took {execution_time:.2f} seconds to finish")
+
+
+if __name__ == "__main__":
+    main()
--- torchrl/objectives/value/vtrace.py
+++ torchrl/objectives/value/vtrace.py
@@ -4,9 +4,22 @@
 # LICENSE file in the root directory of this source tree.
 
 import math
-from typing import Tuple, Union
+from typing import List, Optional, Union, Tuple
 
 import torch
+from tensordict.nn import (
+    dispatch,
+    is_functional,
+    set_skip_existing,
+    TensorDictModule,
+    TensorDictModuleBase,
+)
+from tensordict.tensordict import TensorDictBase
+from tensordict.utils import NestedKey
+from torch import nn, Tensor
+from torchrl.objectives.utils import hold_out_net
+from advantages import ValueEstimatorBase, _self_set_skip_existing, _self_set_grad_enabled, _call_value_nets
+from functional import _transpose_time, SHAPE_ERR
 
 
 def _c_val(
@@ -27,8 +40,8 @@ def _dv_val(
 ) -> Tuple[torch.Tensor, torch.Tensor]:
     rho = _c_val(log_pi, log_mu, rho_bar)
     next_vals = torch.cat([vals[:, 1:], torch.zeros_like(vals[:, :1])], 1)
-    dv = rho * (rewards + gamma * next_vals - vals)
-    return dv, rho
+    deltas = rho * (rewards + gamma * next_vals - vals)
+    return deltas, rho
 
 
 def _vtrace(
@@ -40,6 +53,7 @@ def _vtrace(
     rho_bar: Union[float, torch.Tensor] = 1.0,
     c_bar: Union[float, torch.Tensor] = 1.0,
 ) -> Tuple[torch.Tensor, torch.Tensor]:
+
     T = vals.shape[1]
     if not isinstance(gamma, torch.Tensor):
         gamma = torch.full_like(vals, gamma)
@@ -54,5 +68,277 @@ def _vtrace(
             vals[:, t] + dv[:, t] + gamma[:, t] * c[:, t] * (v_out[-1] - vals[:, t + 1])
         )
         v_out.append(_v_out)
-    v_out = torch.stack(list(reversed(v_out)), 1)
+    v_out = torch.stack(list(reversed(v_out)), 1)  # values
     return v_out, rho
+
+@_transpose_time
+def vtrace_correction(
+        gamma: float,
+        lmbda: float,
+        state_value: torch.Tensor,
+        next_state_value: torch.Tensor,
+        reward: torch.Tensor,
+        done: torch.Tensor,
+        time_dim: int = -2,
+) -> Tuple[torch.Tensor, torch.Tensor]:
+    """V-Trace off-policy correction method.
+
+    Refer to "IMPALA: Scalable Distributed Deep-RL with Importance Weighted  Actor-Learner Architectures"
+    https://arxiv.org/abs/1802.01561 for more context.
+
+    Args:
+        gamma (scalar): exponential mean discount.
+        lmbda (scalar): trajectory discount.
+        log_pi (Tensor): log probability of taking actions in the environment.
+        log_mu (Tensor): log probability of taking actions in the environment.
+        state_value (Tensor): value function result with old_state input.
+        next_state_value (Tensor): value function result with new_state input.
+        reward (Tensor): reward of taking actions in the environment.
+        done (Tensor): boolean flag for end of episode.
+        rho_bar (Union[float, Tensor]): clipping parameter for importance weights.
+        c_bar (Union[float, Tensor]): clipping parameter for importance weights.
+        time_dim (int): dimension where the time is unrolled. Defaults to -2.
+
+    All tensors (values, reward and done) must have shape
+    ``[*Batch x TimeSteps x *F]``, with ``*F`` feature dimensions.
+
+    """
+    if not (next_state_value.shape == state_value.shape == reward.shape == done.shape):
+        raise RuntimeError(SHAPE_ERR)
+    dtype = next_state_value.dtype
+    device = state_value.device
+
+    not_done = (~done).int()
+    *batch_size, time_steps, lastdim = not_done.shape
+    advantage = torch.empty(*batch_size, time_steps, lastdim, device=device, dtype=dtype)
+
+    prev_advantage = 0
+    gnotdone = gamma * not_done
+    delta = reward + (gnotdone * next_state_value) - state_value
+    discount = lmbda * gnotdone
+    for t in reversed(range(time_steps)):
+        prev_advantage = advantage[..., t, :] = delta[..., t, :] + (prev_advantage * discount[..., t, :])
+
+    value_target = advantage + state_value
+
+    return advantage, value_target
+
+
+class VTrace(ValueEstimatorBase):
+    """A class wrapper around V-Trace estimate functional.
+
+    Refer to "IMPALA: Scalable Distributed Deep-RL with Importance Weighted  Actor-Learner Architectures"
+    https://arxiv.org/abs/1802.01561 for more context.
+
+    Args:
+        gamma (scalar): exponential mean discount.
+        lmbda (scalar): trajectory discount.
+        value_network (TensorDictModule): value operator used to retrieve the value estimates.
+        average_gae (bool): if ``True``, the resulting GAE values will be standardized.
+            Default is ``False``.
+        differentiable (bool, optional): if ``True``, gradients are propagated through
+            the computation of the value function. Default is ``False``.
+
+            .. note::
+              The proper way to make the function call non-differentiable is to
+              decorate it in a `torch.no_grad()` context manager/decorator or
+              pass detached parameters for functional modules.
+
+        # vectorized (bool, optional): whether to use the vectorized version of the
+        #     lambda return. Default is `True`.
+
+        skip_existing (bool, optional): if ``True``, the value network will skip
+            modules which outputs are already present in the tensordict.
+            Defaults to ``None``, ie. the value of :func:`tensordict.nn.skip_existing()`
+            is not affected.
+            Defaults to "state_value".
+        advantage_key (str or tuple of str, optional): [Deprecated] the key of
+            the advantage entry.  Defaults to ``"advantage"``.
+        value_target_key (str or tuple of str, optional): [Deprecated] the key
+            of the advantage entry.  Defaults to ``"value_target"``.
+        value_key (str or tuple of str, optional): [Deprecated] the value key to
+            read from the input tensordict.  Defaults to ``"state_value"``.
+
+        # shifted (bool, optional): if ``True``, the value and next value are
+        #     estimated with a single call to the value network. This is faster
+        #     but is only valid whenever (1) the ``"next"`` value is shifted by
+        #     only one time step (which is not the case with multi-step value
+        #     estimation, for instance) and (2) when the parameters used at time
+        #     ``t`` and ``t+1`` are identical (which is not the case when target
+        #     parameters are to be used). Defaults to ``False``.
+
+    VTrace will return an :obj:`"advantage"` entry containing the advantage value. It will also
+    return a :obj:`"value_target"` entry with the V-Trace target value.
+
+    # Finally, if :obj:`gradient_mode` is ``True``,
+    # an additional and differentiable :obj:`"value_error"` entry will be returned,
+    # which simple represents the difference between the return and the value network
+    # output (i.e. an additional distance loss should be applied to that signed value).
+
+    # .. note::
+    #   As other advantage functions do, if the ``value_key`` is already present
+    #   in the input tensordict, the VTrace module will ignore the calls to the value
+    #   network (if any) and use the provided value instead.
+
+    """
+
+    def __init__(
+            self,
+            *,
+            gamma: Union[float, torch.Tensor],
+            lmbda: float,
+            rho_bar: Union[float, torch.Tensor] = 1.0,
+            c_bar: Union[float, torch.Tensor] = 1.0,
+            value_network: TensorDictModule,
+            average_gae: bool = False,
+            differentiable: bool = False,
+            vectorized: bool = True,
+            skip_existing: Optional[bool] = None,
+            advantage_key: NestedKey = None,
+            value_target_key: NestedKey = None,
+            value_key: NestedKey = None,
+            shifted: bool = False,
+    ):
+        super().__init__(
+            shifted=shifted,
+            value_network=value_network,
+            differentiable=differentiable,
+            advantage_key=advantage_key,
+            value_target_key=value_target_key,
+            value_key=value_key,
+            skip_existing=skip_existing,
+        )
+        try:
+            device = next(value_network.parameters()).device
+        except (AttributeError, StopIteration):
+            device = torch.device("cpu")
+        self.register_buffer("gamma", torch.tensor(gamma, device=device))
+        self.register_buffer("lmbda", torch.tensor(lmbda, device=device))
+        self.register_buffer("rho_bar", torch.tensor(rho_bar, device=device))
+        self.register_buffer("c_bar", torch.tensor(c_bar, device=device))
+        self.average_gae = average_gae
+        self.vectorized = vectorized
+
+    @_self_set_skip_existing
+    @_self_set_grad_enabled
+    @dispatch
+    def forward(
+            self,
+            tensordict: TensorDictBase,
+            *unused_args,
+            params: Optional[List[Tensor]] = None,
+            target_params: Optional[List[Tensor]] = None,
+    ) -> TensorDictBase:
+        """Computes the V-Trace correction given the data in tensordict.
+
+        If a functional module is provided, a nested TensorDict containing the parameters
+        (and if relevant the target parameters) can be passed to the module.
+
+        Args:
+            tensordict (TensorDictBase): A TensorDict containing the data
+                (an observation key, "action", "reward", "done" and "next" tensordict state
+                as returned by the environment) necessary to compute the value estimates and the GAE.
+                The data passed to this module should be structured as :obj:`[*B, T, F]` where :obj:`B` are
+                the batch size, :obj:`T` the time dimension and :obj:`F` the feature dimension(s).
+            params (TensorDictBase, optional): A nested TensorDict containing the params
+                to be passed to the functional value network module.
+            target_params (TensorDictBase, optional): A nested TensorDict containing the
+                target params to be passed to the functional value network module.
+
+        Returns:
+            An updated TensorDict with an advantage and a value_error keys as defined in the constructor.
+
+        Examples:
+            >>> from tensordict import TensorDict
+            >>> value_net = TensorDictModule(
+            ...     nn.Linear(3, 1), in_keys=["obs"], out_keys=["state_value"]
+            ... )
+            >>> module = VTrace(
+            ...     gamma=0.98,
+            ...     lmbda=0.94,
+            ...     value_network=value_net,
+            ...     differentiable=False,
+            ... )
+            >>> obs, next_obs = torch.randn(2, 1, 10, 3)
+            >>> reward = torch.randn(1, 10, 1)
+            >>> done = torch.zeros(1, 10, 1, dtype=torch.bool)
+            >>> tensordict = TensorDict({"obs": obs, "next": {"obs": next_obs}, "done": done, "reward": reward}, [1, 10])
+            >>> _ = module(tensordict)
+            >>> assert "advantage" in tensordict.keys()
+
+        The module supports non-tensordict (i.e. unpacked tensordict) inputs too:
+
+        Examples:
+            >>> value_net = TensorDictModule(
+            ...     nn.Linear(3, 1), in_keys=["obs"], out_keys=["state_value"]
+            ... )
+            >>> module = VTrace(
+            ...     gamma=0.98,
+            ...     lmbda=0.94,
+            ...     value_network=value_net,
+            ...     differentiable=False,
+            ... )
+            >>> obs, next_obs = torch.randn(2, 1, 10, 3)
+            >>> reward = torch.randn(1, 10, 1)
+            >>> done = torch.zeros(1, 10, 1, dtype=torch.bool)
+            >>> advantage, value_target = module(obs=obs, reward=reward, done=done, next_obs=next_obs)
+
+        """
+        if tensordict.batch_dims < 1:
+            raise RuntimeError(
+                "Expected input tensordict to have at least one dimensions, got "
+                f"tensordict.batch_size = {tensordict.batch_size}"
+            )
+        reward = tensordict.get(("next", self.tensor_keys.reward))
+        device = reward.device
+        gamma, lmbda = self.gamma.to(device), self.lmbda.to(device)
+        steps_to_next_obs = tensordict.get(self.tensor_keys.steps_to_next_obs, None)
+        if steps_to_next_obs is not None:
+            gamma = gamma ** steps_to_next_obs.view_as(reward)
+
+        if self.value_network is not None:
+            if params is not None:
+                params = params.detach()
+                if target_params is None:
+                    target_params = params.clone(False)
+            with hold_out_net(self.value_network):
+                # we may still need to pass gradient, but we don't want to assign grads to
+                # value net params
+                value, next_value = _call_value_nets(
+                    value_net=self.value_network,
+                    data=tensordict,
+                    params=params,
+                    next_params=target_params,
+                    single_call=self.shifted,
+                    value_key=self.tensor_keys.value,
+                    detach_next=True,
+                )
+        else:
+            value = tensordict.get(self.tensor_keys.value)
+            next_value = tensordict.get(("next", self.tensor_keys.value))
+
+        done = tensordict.get(("next", self.tensor_keys.done))
+        if self.vectorized:
+            raise NotImplementedError
+        else:
+            adv, value_target = vtrace_correction(
+                gamma,
+                lmbda,
+                value,
+                next_value,
+                reward,
+                done,
+                time_dim=tensordict.ndim - 1,
+            )
+
+        if self.average_gae:
+            loc = adv.mean()
+            scale = adv.std().clamp_min(1e-4)
+            adv = adv - loc
+            adv = adv / scale
+
+        tensordict.set(self.tensor_keys.advantage, adv)
+        tensordict.set(self.tensor_keys.value_target, value_target)
+
+        return tensordict
+
