801bac54ec98d13975bc8d19b6415e14ecfe65db
Matteo Bettini <matbet@meta.com>
2023-09-14 16:36:07 +0100

    add to setup
    Signed-off-by: Matteo Bettini <matbet@meta.com>

--- setup.py
+++ setup.py
@@ -235,7 +235,7 @@ def _main(argv):
             "checkpointing": [
                 "torchsnapshot",
             ],
-            "marl": ["vmas"],
+            "marl": ["vmas", "pettingzoo>=1.24.1"],
         },
         zip_safe=False,
         classifiers=[
