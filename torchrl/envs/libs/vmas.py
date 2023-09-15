e31d3e863a05261b618bbacdb9d30346baa8b3a6
vmoens <vincentmoens@gmail.com>
2023-09-15 07:11:25 -0400

    lint

--- torchrl/envs/libs/vmas.py
+++ torchrl/envs/libs/vmas.py
@@ -17,7 +17,7 @@ from torchrl.data import (
 )
 from torchrl.envs.common import _EnvWrapper, EnvBase
 from torchrl.envs.libs.gym import _gym_to_torchrl_spec_transform, set_gym_backend
-from torchrl.envs.utils import _selective_unsqueeze, _classproperty
+from torchrl.envs.utils import _classproperty, _selective_unsqueeze
 
 IMPORT_ERR = None
 try:
