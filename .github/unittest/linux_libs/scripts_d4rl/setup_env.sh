7fa7956cb5e3383fafd9d70bf5ed130a38a242d4
vmoens <vincentmoens@gmail.com>
2023-09-15 03:35:53 -0400

    amend

--- .github/unittest/linux_libs/scripts_d4rl/setup_env.sh
+++ .github/unittest/linux_libs/scripts_d4rl/setup_env.sh
@@ -54,7 +54,7 @@ cd $root_dir/.mujoco/
 #wget https://github.com/deepmind/mujoco/releases/download/2.1.1/mujoco-2.1.1-linux-x86_64.tar.gz
 #tar -xf mujoco-2.1.1-linux-x86_64.tar.gz
 #wget https://mujoco.org/download/mujoco210-linux-x86_64.tar.gz
-wget https://www.roboti.us/download/mujoco200_linux.zip
+wget https://pytorch.s3.amazonaws.com/torchrl/github-artifacts/mujoco200_linux.zip
 unzip mujoco200_linux.zip
 wget https://pytorch.s3.amazonaws.com/torchrl/github-artifacts/mjkey.txt
 cp mjkey.txt ./mujoco200_linux/bin/
