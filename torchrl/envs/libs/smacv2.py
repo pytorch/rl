670e3fdb36c86cbc3271ee814b66527372baf4f1
Matteo Bettini <matbet@meta.com>
2023-09-19 19:15:59 +0100

    amend
    Signed-off-by: Matteo Bettini <matbet@meta.com>

--- torchrl/envs/libs/smacv2.py
+++ torchrl/envs/libs/smacv2.py
@@ -364,15 +364,6 @@ class SMACv2Wrapper(_EnvWrapper):
         action = tensordict.get(("agents", "action"))
         action_np = self.action_spec.to_numpy(action)
 
-        action_mask = tensordict.get(("agents", "action_mask"))
-        mask = torch.tensor(
-            self.get_avail_actions(), dtype=torch.bool, device=self.device
-        )
-        if (action_mask != mask).any():
-            breakpoint()
-        if not action_mask.gather(-1, action.unsqueeze(-1)).all():
-            breakpoint()
-
         # Actions are validated by the environment.
         try:
             reward, done, info = self._env.step(action_np)
