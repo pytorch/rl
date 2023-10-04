28c116fe2bb7bfc033f5775b99dbc24108050ae5
vmoens <vincentmoens@gmail.com>
2023-10-04 10:08:24 +0100

    amend

--- .github/unittest/linux_examples/scripts/run_test.sh
+++ .github/unittest/linux_examples/scripts/run_test.sh
@@ -286,8 +286,16 @@ python .github/unittest/helpers/coverage_run_parallel.py examples/multiagent/sac
   train.minibatch_size=100 \
   logger.backend=
 
-
 python .github/unittest/helpers/coverage_run_parallel.py examples/bandits/dqn.py --n_steps=100
+
+## RLHF
+python .github/unittest/helpers/coverage_run_parallel.py examples/rlhf/train_rlhf.py \
+  sys.device=cuda:0 sys.ref_device=cuda:0 \
+  model.name_or_path=gpt2 train.max_epochs=2 \
+  data.batch_size=2 train.ppo.ppo_batch_size=2 \
+  train.ppo.ppo_num_epochs=1 reward_model.name_or_path= \
+  train.ppo.episode_length=8 train.ppo.num_rollouts_per_epoch=4 \
+  data.block_size=110
 
 coverage combine
 coverage xml -i
