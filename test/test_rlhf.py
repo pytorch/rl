8148a3676bead22c4d0c278d87767fec97fd4521
Vincent Moens <vmoens@meta.com>
2023-10-05 11:48:23 -0400

    [Algorithm] RLHF end-to-end, clean (#1597)
    Co-authored-by: Alessandro Pietro Bardelli <apbard@users.noreply.github.com>
    Co-authored-by: Tom Begley <tomcbegley@gmail.com>

--- test/test_rlhf.py
+++ test/test_rlhf.py
@@ -453,7 +453,10 @@ class TestRollout:
 
     def _get_rollout_model(self, max_new_tokens=10):
         return RolloutFromModel(
-            self._model, self._ref_model, self._reward_model, max_new_tokens
+            model=self._model,
+            ref_model=self._ref_model,
+            reward_model=self._reward_model,
+            max_new_tokens=max_new_tokens,
         )
 
     def test_padded_right_to_left(self):
