f7c9bc64b64187f8de27ac7bd312b9be396a460e
albert bou <albertbou92@gmail.com>
2023-10-06 10:46:45 +0200

    fixes

--- docs/source/content_generation.py
+++ docs/source/content_generation.py
@@ -50,8 +50,8 @@ def generate_knowledge_base_references(knowledge_base_path: str) -> None:
     for file_path in file_paths:
         name = Path(file_path).stem
 
-        # Skip README.md, it is already included in `knowledge_base.rst`
-        if name == "README.md":
+        # Skip README, it is already included in `knowledge_base.rst`
+        if name == "README":
             continue
 
         # Existing files will be overwritten in 'w' mode
