44dfe8686a7d40a8408905829c118667b06462fc
vmoens <vincentmoens@gmail.com>
2023-10-19 09:55:18 -0700

    amend

--- torchrl/envs/transforms/transforms.py
+++ torchrl/envs/transforms/transforms.py
@@ -167,6 +167,7 @@ class Transform(nn.Module):
         self.in_keys_inv = in_keys_inv
         self.out_keys_inv = out_keys_inv
         self._missing_tolerance = False
+        # we use __dict__ to avoid having nn.Module placing these objects in the module list
         self.__dict__["_container"] = None
         self.__dict__["_parent"] = None
 
@@ -557,8 +558,6 @@ class TransformedEnv(EnvBase, metaclass=_TEnvPostInit):
 
     """
 
-    _transform: Transform
-
     def __init__(
         self,
         env: EnvBase,
@@ -566,6 +565,7 @@ class TransformedEnv(EnvBase, metaclass=_TEnvPostInit):
         cache_specs: bool = True,
         **kwargs,
     ):
+        self._transform=None
         device = kwargs.pop("device", None)
         if device is not None:
             env = env.to(device)
@@ -626,7 +626,7 @@ class TransformedEnv(EnvBase, metaclass=_TEnvPostInit):
 
     @property
     def transform(self) -> Transform:
-        return self._transform
+        return getattr(self, '_transform', None)
 
     @transform.setter
     def transform(self, transform: Transform):
@@ -635,10 +635,12 @@ class TransformedEnv(EnvBase, metaclass=_TEnvPostInit):
                 f"""Expected a transform of type torchrl.envs.transforms.Transform,
 but got an object of type {type(transform)}."""
             )
-        prev_transform = self.__dict__.get('_transform', None)
+        prev_transform = getattr(self, '_transform', None)
         if prev_transform is not None:
             prev_transform.empty_cache()
             prev_transform.reset_parent()
+        if not isinstance(transform, Transform):
+            raise ValueError(f"Transforms passed to {type(self)} must be instances of a `torch.envs.Transform` subclass. Got {type(transform)}.")
         transform = transform.to(self.device)
         transform.set_container(self)
         transform.eval()
