8d8060041e93bda9e5fd9980e9f0f995ec5e081c
maxweissenbacher <max.weissenbacher@proton.me>
2023-10-20 10:59:51 +0100

    Made quantile huber loss compatible with arbitrary batch sizes

--- examples/tqc/utils.py
+++ examples/tqc/utils.py
@@ -234,15 +234,19 @@ def quantile_huber_loss_f(quantiles, samples):
     """
     Quantile Huber loss from the original PyTorch TQC implementation.
     See: https://github.com/SamsungLabs/tqc_pytorch/blob/master/tqc/functions.py
+
+    quantiles is assumed to be of shape [batch size, n_nets, n_quantiles]
+    samples is assumed to be of shape [batch size, n_samples]
+    Arbitrary batch sizes are allowed.
     """
-    pairwise_delta = samples[:, None, None, :] - quantiles[:, :, :, None]  # batch x nets x quantiles x samples
+    pairwise_delta = samples[..., None, None, :] - quantiles[..., None]  # batch x n_nets x n_quantiles x n_samples
     abs_pairwise_delta = torch.abs(pairwise_delta)
     huber_loss = torch.where(abs_pairwise_delta > 1,
                              abs_pairwise_delta - 0.5,
                              pairwise_delta ** 2 * 0.5)
-    n_quantiles = quantiles.shape[2]
+    n_quantiles = quantiles.shape[-1]
     tau = torch.arange(n_quantiles, device=quantiles.device).float() / n_quantiles + 1 / 2 / n_quantiles
-    loss = (torch.abs(tau[None, None, :, None] - (pairwise_delta < 0).float()) * huber_loss).mean()
+    loss = (torch.abs(tau[..., None, :, None] - (pairwise_delta < 0).float()) * huber_loss).mean()
     return loss
 
 
