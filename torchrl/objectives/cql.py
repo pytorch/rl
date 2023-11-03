08001329e094213d70ae22ee65c7135d297c479a
BY571 <sebastian.dittert@gmx.de>
2023-11-03 16:36:55 +0100

    update loss docstring

--- torchrl/objectives/cql.py
+++ torchrl/objectives/cql.py
@@ -803,7 +803,81 @@ class CQLLoss(LossModule):
 class DiscreteCQLLoss(LossModule):
     """TorchRL implementation of the discrete CQL loss.
 
-    Presented in "Conservative Q-Learning for Offline Reinforcement Learning" https://arxiv.org/abs/2006.04779
+    This class implements the discrete conservative Q-learning (CQL) loss function, as presented in the paper
+    "Conservative Q-Learning for Offline Reinforcement Learning" (https://arxiv.org/abs/2006.04779).
+
+    Args:
+        value_network (Union[QValueActor, nn.Module]): The Q-value network used to estimate state-action values.
+    Keyword Args:
+        loss_function (Optional[str]): The distance function used to calculate the distance between the predicted
+            Q-values and the target Q-values. Defaults to ``l2``.
+        delay_value (bool): Whether to separate the target Q value
+            networks from the Q value networks used for data collection.
+            Default is ``True``.
+        gamma (float, optional): Discount factor. Default is ``None``.
+        action_space: The action space of the environment. If None, it is inferred from the value network.
+            Defaults to None.
+        priority_key (str, optional): [Deprecated, use .set_keys(priority_key=priority_key) instead]
+            Tensordict key where to write the
+            priority (for prioritized replay buffer usage). Defaults to ``"td_error"``.
+
+
+    Examples:
+        >>> from torchrl.modules import MLP
+        >>> from torchrl.data import OneHotDiscreteTensorSpec
+        >>> n_obs, n_act = 4, 3
+        >>> value_net = MLP(in_features=n_obs, out_features=n_act)
+        >>> spec = OneHotDiscreteTensorSpec(n_act)
+        >>> actor = QValueActor(value_net, in_keys=["observation"], action_space=spec)
+        >>> loss = DiscreteCQLLoss(actor, action_space=spec)
+        >>> batch = [10,]
+        >>> data = TensorDict({
+        ...     "observation": torch.randn(*batch, n_obs),
+        ...     "action": spec.rand(batch),
+        ...     ("next", "observation"): torch.randn(*batch, n_obs),
+        ...     ("next", "done"): torch.zeros(*batch, 1, dtype=torch.bool),
+        ...     ("next", "terminated"): torch.zeros(*batch, 1, dtype=torch.bool),
+        ...     ("next", "reward"): torch.randn(*batch, 1)
+        ... }, batch)
+        >>> loss(data)
+        TensorDict(
+            fields={
+                loss: Tensor(shape=torch.Size([]), device=cuda:0, dtype=torch.float32, is_shared=True),
+                loss_cql: Tensor(shape=torch.Size([]), device=cpu, dtype=torch.float32, is_shared=False)},
+            batch_size=torch.Size([]),
+            device=None,
+            is_shared=False)
+
+    This class is compatible with non-tensordict based modules too and can be
+    used without recurring to any tensordict-related primitive. In this case,
+    the expected keyword arguments are:
+    ``["observation", "next_observation", "action", "next_reward", "next_done", "next_terminated"]``,
+    and a single loss value is returned.
+
+    Examples:
+        >>> from torchrl.objectives import DiscreteCQLLoss
+        >>> from torchrl.data import OneHotDiscreteTensorSpec
+        >>> from torch import nn
+        >>> import torch
+        >>> n_obs = 3
+        >>> n_action = 4
+        >>> action_spec = OneHotDiscreteTensorSpec(n_action)
+        >>> value_network = nn.Linear(n_obs, n_action) # a simple value model
+        >>> dcql_loss = DiscreteCQLLoss(value_network, action_space=action_spec)
+        >>> # define data
+        >>> observation = torch.randn(n_obs)
+        >>> next_observation = torch.randn(n_obs)
+        >>> action = action_spec.rand()
+        >>> next_reward = torch.randn(1)
+        >>> next_done = torch.zeros(1, dtype=torch.bool)
+        >>> next_terminated = torch.zeros(1, dtype=torch.bool)
+        >>> loss_val = dcql_loss(
+        ...     observation=observation,
+        ...     next_observation=next_observation,
+        ...     next_reward=next_reward,
+        ...     next_done=next_done,
+        ...     next_terminated=next_terminated,
+        ...     action=action)
     """
 
     @dataclass
