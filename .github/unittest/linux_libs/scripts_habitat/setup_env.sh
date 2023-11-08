935f6b2c28b8fc31130a897c395e34b459af6ac0
vmoens <vincentmoens@gmail.com>
2023-11-08 14:14:15 -0500

    libxcursor

--- .github/unittest/linux_libs/scripts_habitat/setup_env.sh
+++ .github/unittest/linux_libs/scripts_habitat/setup_env.sh
@@ -11,7 +11,7 @@ set -v
 this_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 # Avoid error: "fatal: unsafe repository"
 #apt-get update && apt-get install -y git wget gcc g++ unzip
-apt-get install -y libxrandr-dev libxinerama-dev
+apt-get install -y libxrandr-dev libxinerama-dev libxcursor-dev
 
 git config --global --add safe.directory '*'
 root_dir="$(git rev-parse --show-toplevel)"
