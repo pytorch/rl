ac5e122409ef247ba882855019ac2662d10652dd
vmoens <vincentmoens@gmail.com>
2023-11-08 08:21:02 -0500

    td version

--- setup.py
+++ setup.py
@@ -169,7 +169,7 @@ def _main(argv):
     if is_nightly:
         tensordict_dep = "tensordict-nightly"
     else:
-        tensordict_dep = "tensordict>=0.2.0"
+        tensordict_dep = "tensordict>=0.2.2"
 
     if is_nightly:
         version = get_nightly_version()
