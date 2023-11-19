e47dbc32a920b5958d8afdd8d0f439cddd27b35e
albert bou <albertbou92@gmail.com>
2023-11-19 11:50:22 +0100

    fix example

--- examples/impala/impala_single_node.py
+++ examples/impala/impala_single_node.py
@@ -88,6 +88,7 @@ def main(cfg: "DictConfig"):  # noqa: F821
         entropy_coef=cfg.loss.entropy_coef,
         critic_coef=cfg.loss.critic_coef,
     )
+    loss_module.set_keys(done="eol", terminated="eol")
 
     # Create optimizer
     optim = torch.optim.RMSprop(
