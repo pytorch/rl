33608a83b85f271e9fd46a1386f1d619fe673215
albert bou <albertbou92@gmail.com>
2023-11-29 19:19:19 +0100

    LSTMModule test

--- test/test_tensordictmodules.py
+++ test/test_tensordictmodules.py
@@ -1672,6 +1672,7 @@ class TestLSTMModule:
         lstm_module(padded)
 
     @pytest.mark.parametrize("shape", [[], [2], [2, 3], [2, 3, 4]])
+    @pytest.mark.parametrize("python_based", [True, False])
     def test_single_step(self, shape, python_based):
         td = TensorDict(
             {
