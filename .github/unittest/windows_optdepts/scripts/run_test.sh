31015ef18b124c735fb1c81b877e0dd6e1fde6be
vmoens <vincentmoens@gmail.com>
2023-12-18 10:16:43 +0000

    amend

--- .github/unittest/windows_optdepts/scripts/run_test.sh
+++ .github/unittest/windows_optdepts/scripts/run_test.sh
@@ -8,5 +8,8 @@ conda activate ./env
 this_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source "$this_dir/set_cuda_envs.sh"
 
+# we don't use torchsnapshot
+export CKPT_BACKEND=torch
+
 python -m torch.utils.collect_env
 pytest --junitxml=test-results/junit.xml -v --durations 200  --ignore test/test_distributed.py --ignore test/test_rlhf.py
--- .github/unittest/windows_optdepts/scripts/setup_env.sh
+++ .github/unittest/windows_optdepts/scripts/setup_env.sh
@@ -43,6 +43,3 @@ echo $(conda info -e)
 #conda env update --file "${this_dir}/environment.yml" --prune
 
 python -m pip install hypothesis future cloudpickle pytest pytest-cov pytest-mock pytest-instafail pytest-rerunfailures expecttest pyyaml scipy coverage
-
-# we don't use torchsnapshot
-export CKPT_BACKEND=torch
