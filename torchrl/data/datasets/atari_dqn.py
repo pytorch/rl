32e2523ccfe66f0d5121a8a5fd8b082a734c4ea8
vmoens <vincentmoens@gmail.com>
2024-01-09 10:55:34 +0000

    amend

--- torchrl/data/datasets/atari_dqn.py
+++ torchrl/data/datasets/atari_dqn.py
@@ -10,44 +10,63 @@ import os
 import gzip
 import tqdm
 import numpy as np
+from tensordict import TensorDict
 import torch
 import io
 from pathlib import Path
+from collections import defaultdict
+
+tempdir = "/Users/vmoens/Downloads/Pong/1"
+
 
 class AtariDQNExperienceReplay(TensorDictReplayBuffer):
-    available_datasets = ["Pong/1",]
+    available_datasets = ["Pong/1", ]
+
     def __init__(self, dataset_id):
         self.dataset_id = dataset_id
         storage = LazyMemmapStorage(1_000_000)
         super().__init__(storage=storage)
 
+    # def _download_dataset(self):
+    #     # with tempfile.TemporaryDirectory() as tempdir:
+    #     # command = f"gsutil -m cp -R gs://atari-replay-datasets/dqn/{self.dataset_id} {tempdir}"
+    #     # subprocess.run(command, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
-    def _download_dataset(self):
-        # with tempfile.TemporaryDirectory() as tempdir:
-        tempdir = "/Users/vmoens/Downloads/Pong/1"
-        # command = f"gsutil -m cp -R gs://atari-replay-datasets/dqn/{self.dataset_id} {tempdir}"
-        # subprocess.run(command, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    def _get_episode(self, episode, path):
+        gz_files = self._list_episodes(path)
+        files = gz_files[episode]
+        td = {}
+        for file in files:
+            name = str(Path(file).parts[-1]).split(".")[0]
+            with gzip.GzipFile(file) as f:
+                file_content = f.read()
+            t = torch.as_tensor(np.load(io.BytesIO(file_content)))
+            td[self._process_name(name)] = t
+        td = TensorDict.from_dict(td)
+        return td
 
+    @staticmethod
+    def _process_name(name):
+        if "store" in name:
+            return ("data", name.split("_")[1])
+        if name.endswith("_ckpt"):
+            return name[:-5]
+
+    def _list_episodes(self, path):
         gz_files = []
-        for root, dirs, files in os.walk(tempdir):
+        for root, dirs, files in os.walk(path):
             for file in files:
                 if file.endswith('.gz'):
                     gz_files.append(os.path.join(root, file))
+        episodes = defaultdict(list)
+        for file in gz_files:
+            filename = Path(file).parts[-1]
+            name, episode, extension = str(filename).split(".")
+            episode = int(episode)
+            episodes[episode].append(file)
+        return episodes
 
-        def _count_files(pattern):
-            return sum(pattern in filename for filename in gz_files)
 
-        pbar = tqdm.tqdm(gz_files)
-        for file in pbar:
-            name = str(Path(file).parts[-1]).split(".")[0]
-            # with open(file, "r") as fopen:
-            if "obs" in file:
-                print(name, file)
-                print("count", _count_files(name))
-                with gzip.GzipFile(file) as f:
-                    file_content = f.read()
-                t = torch.as_tensor(np.load(io.BytesIO(file_content)))
-                print(t.shape, t.dtype)
-                break
-
-AtariDQNExperienceReplay(AtariDQNExperienceReplay.available_datasets[0])._download_dataset()
+AtariDQNExperienceReplay(
+    AtariDQNExperienceReplay.available_datasets[0]
+    )._get_episode(0, tempdir)
