ae3162438d32896c67d7984dbde8ce689dae5bec
vmoens <vincentmoens@gmail.com>
2024-01-23 11:37:55 +0000

    amend

--- torchrl/record/loggers/wandb.py
+++ torchrl/record/loggers/wandb.py
@@ -19,8 +19,24 @@ _has_omegaconf = importlib.util.find_spec("omegaconf") is not None
 class WandbLogger(Logger):
     """Wrapper for the wandb logger.
 
+    The keyword arguments are mainly based on the :func:`wandb.init` kwargs.
+    See the doc `here <https://docs.wandb.ai/ref/python/init>`__.
+
     Args:
         exp_name (str): The name of the experiment.
+        offline (bool, optional): if ``True``, the logs will be stored locally
+            only. Defaults to ``False``.
+        save_dir (path, optional): the directory where to save data. Exclusive with
+            ``log_dir``.
+        log_dir (path, optional): the directory where to save data. Exclusive with
+            ``save_dir``.
+        id (str, optional): A unique ID for this run, used for resuming.
+            It must be unique in the project, and if you delete a run you can't reuse the ID.
+        project (str, optional): The name of the project where you're sending
+            the new run. If the project is not specified, the run is put in
+            an ``"Uncategorized"`` project.
+        **kwargs: Extra keyword arguments for ``wandb.init``. See relevant page for
+            more info.
 
     """
 
