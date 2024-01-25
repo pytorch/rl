208264adc006f9ce4ccb68da93cb9b2a41aaeb11
vmoens <vincentmoens@gmail.com>
2024-01-25 17:39:27 +0000

    amend

--- examples/ppo/utils_atari.py
+++ examples/ppo/utils_atari.py
@@ -60,7 +60,7 @@ def make_base_env(
 
 def make_parallel_env(env_name, num_envs, device, is_test=False):
     env = ParallelEnv(
-        num_envs, EnvCreator(lambda: make_base_env(env_name), device=device)
+        num_envs, EnvCreator(lambda: make_base_env(env_name)), device=device
     )
     env = TransformedEnv(env)
     env.append_transform(ToTensorImage())
