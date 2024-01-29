6277226411fe88142fd0bc1627204f3c813b64a1
Vincent Moens <vmoens@meta.com>
2024-01-29 20:51:23 +0000

    [Doc] Make tutos runnable without colab (#1826)

--- tutorials/sphinx-tutorials/torchrl_demo.py
+++ tutorials/sphinx-tutorials/torchrl_demo.py
@@ -128,6 +128,18 @@ This demo was presented at ICML 2022 on the industry demo day.
 import warnings
 
 warnings.filterwarnings("ignore")
+
+from torch import multiprocessing
+
+# TorchRL prefers spawn method, that restricts creation of  ``~torchrl.envs.ParallelEnv`` inside
+# `__main__` method call, but for the easy of reading the code switch to fork
+# which is also a default spawn method in Google's Colaboratory
+try:
+    multiprocessing.set_start_method("fork")
+except RuntimeError:
+    assert multiprocessing.get_start_method() == "fork"
+
+
 # sphinx_gallery_end_ignore
 
 import torch
