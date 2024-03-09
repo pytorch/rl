e547ab0e1b39c2641e2067ca4a848fbb29a8c228
albert bou <albertbou92@gmail.com>
2024-03-09 10:32:53 +0100

    format

--- torchrl/objectives/cql.py
+++ torchrl/objectives/cql.py
@@ -612,7 +612,9 @@ class CQLLoss(LossModule):
                 # tensordict.del_("scale")
 
         return (
-            tensordict.select(*self.actor_network.in_keys, self.tensor_keys.action, strict=False),
+            tensordict.select(
+                *self.actor_network.in_keys, self.tensor_keys.action, strict=False
+            ),
             sample_log_prob,
         )
 
