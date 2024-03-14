3723dc210103a1aadc6c43a93315f67cce1324d7
albert bou <albertbou92@gmail.com>
2024-03-14 12:52:38 +0100

    fix test

--- torchrl/objectives/utils.py
+++ torchrl/objectives/utils.py
@@ -524,9 +524,12 @@ def _clip_value_loss(
     loss_value: torch.Tensor,
     loss_critic_type: str,
 ):
-    """Computed a clipped state value and returns the most
-    pessimistic value prediction between clipped and non-clipped.
-    the value loss and computes the clip fraction."""
+    """Value clipping method for loss computation.
+
+    This method computes a clipped state value from the old state value and the state value,
+    and returns the most pessimistic value prediction between clipped and non-clipped options.
+    It also computes the clip fraction.
+    """
     state_value_clipped = old_state_value + (state_value - old_state_value).clamp(
         -clip_value, clip_value
     )
