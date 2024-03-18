0aa24f68905004a1c3a52e0d8abb6331c6ec3774
Vincent Moens <vmoens@meta.com>
2024-03-18 16:27:06 +0000

    lint

--- torchrl/modules/tensordict_module/actors.py
+++ torchrl/modules/tensordict_module/actors.py
@@ -3,7 +3,7 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 import warnings
-from typing import Any, Dict, List, Optional, Sequence, Tuple, Union
+from typing import Dict, List, Optional, Sequence, Tuple, Union
 
 import torch
 
