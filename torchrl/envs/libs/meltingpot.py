1db8cb244c4811aabab1f1bafb48b593d8c0c1d9
Matteo Bettini <mb2389@cl.cam.ac.uk>
2024-04-03 15:34:58 +0200

    examples in docs

--- torchrl/envs/libs/meltingpot.py
+++ torchrl/envs/libs/meltingpot.py
@@ -91,6 +91,60 @@ class MeltingpotWrapper(_EnvWrapper):
         If you deem the ``truncation`` signal necessary, set ``max_steps`` to
         ``None`` and use a :class:`~torchrl.envs.transforms.StepCounter` transform.
 
+    Examples:
+        >>> from meltingpot import substrate
+        >>> from torchrl.envs.libs.meltingpot import MeltingpotWrapper
+        >>> substrate_config = substrate.get_config("commons_harvest__open")
+        >>> mp_env = substrate.build_from_config(
+        ...     substrate_config, roles=substrate_config.default_player_roles
+        ... )
+        >>> env_torchrl = MeltingpotWrapper(env=mp_env)
+        >>> print(env_torchrl.rollout(max_steps=5))
+        TensorDict(
+            fields={
+                WORLD.RGB: Tensor(shape=torch.Size([5, 144, 192, 3]), device=cpu, dtype=torch.uint8, is_shared=False),
+                agents: TensorDict(
+                    fields={
+                        action: Tensor(shape=torch.Size([5, 7]), device=cpu, dtype=torch.int64, is_shared=False),
+                        observation: TensorDict(
+                            fields={
+                                COLLECTIVE_REWARD: Tensor(shape=torch.Size([5, 7, 1]), device=cpu, dtype=torch.float64, is_shared=False),
+                                READY_TO_SHOOT: Tensor(shape=torch.Size([5, 7, 1]), device=cpu, dtype=torch.float64, is_shared=False),
+                                RGB: Tensor(shape=torch.Size([5, 7, 88, 88, 3]), device=cpu, dtype=torch.uint8, is_shared=False)},
+                            batch_size=torch.Size([5, 7]),
+                            device=cpu,
+                            is_shared=False)},
+                    batch_size=torch.Size([5, 7]),
+                    device=cpu,
+                    is_shared=False),
+                done: Tensor(shape=torch.Size([5, 1]), device=cpu, dtype=torch.bool, is_shared=False),
+                next: TensorDict(
+                    fields={
+                        WORLD.RGB: Tensor(shape=torch.Size([5, 144, 192, 3]), device=cpu, dtype=torch.uint8, is_shared=False),
+                        agents: TensorDict(
+                            fields={
+                                observation: TensorDict(
+                                    fields={
+                                        COLLECTIVE_REWARD: Tensor(shape=torch.Size([5, 7, 1]), device=cpu, dtype=torch.float64, is_shared=False),
+                                        READY_TO_SHOOT: Tensor(shape=torch.Size([5, 7, 1]), device=cpu, dtype=torch.float64, is_shared=False),
+                                        RGB: Tensor(shape=torch.Size([5, 7, 88, 88, 3]), device=cpu, dtype=torch.uint8, is_shared=False)},
+                                    batch_size=torch.Size([5, 7]),
+                                    device=cpu,
+                                    is_shared=False),
+                                reward: Tensor(shape=torch.Size([5, 7, 1]), device=cpu, dtype=torch.float64, is_shared=False)},
+                            batch_size=torch.Size([5, 7]),
+                            device=cpu,
+                            is_shared=False),
+                        done: Tensor(shape=torch.Size([5, 1]), device=cpu, dtype=torch.bool, is_shared=False),
+                        terminated: Tensor(shape=torch.Size([5, 1]), device=cpu, dtype=torch.bool, is_shared=False)},
+                    batch_size=torch.Size([5]),
+                    device=cpu,
+                    is_shared=False),
+                terminated: Tensor(shape=torch.Size([5, 1]), device=cpu, dtype=torch.bool, is_shared=False)},
+            batch_size=torch.Size([5]),
+            device=cpu,
+            is_shared=False)
+
     """
 
     git_url = "https://github.com/google-deepmind/meltingpot"
@@ -442,6 +496,55 @@ class MeltingpotEnv(MeltingpotWrapper):
         when the env reached ``max_steps`` and termination.
         If you deem the ``truncation`` signal necessary, set ``max_steps`` to
         ``None`` and use a :class:`~torchrl.envs.transforms.StepCounter` transform.
+
+    Examples:
+        >>> from torchrl.envs.libs.meltingpot import MeltingpotEnv
+        >>> env_torchrl = MeltingpotEnv("commons_harvest__open")
+        >>> print(env_torchrl.rollout(max_steps=5))
+        TensorDict(
+            fields={
+                WORLD.RGB: Tensor(shape=torch.Size([5, 144, 192, 3]), device=cpu, dtype=torch.uint8, is_shared=False),
+                agents: TensorDict(
+                    fields={
+                        action: Tensor(shape=torch.Size([5, 7]), device=cpu, dtype=torch.int64, is_shared=False),
+                        observation: TensorDict(
+                            fields={
+                                COLLECTIVE_REWARD: Tensor(shape=torch.Size([5, 7, 1]), device=cpu, dtype=torch.float64, is_shared=False),
+                                READY_TO_SHOOT: Tensor(shape=torch.Size([5, 7, 1]), device=cpu, dtype=torch.float64, is_shared=False),
+                                RGB: Tensor(shape=torch.Size([5, 7, 88, 88, 3]), device=cpu, dtype=torch.uint8, is_shared=False)},
+                            batch_size=torch.Size([5, 7]),
+                            device=cpu,
+                            is_shared=False)},
+                    batch_size=torch.Size([5, 7]),
+                    device=cpu,
+                    is_shared=False),
+                done: Tensor(shape=torch.Size([5, 1]), device=cpu, dtype=torch.bool, is_shared=False),
+                next: TensorDict(
+                    fields={
+                        WORLD.RGB: Tensor(shape=torch.Size([5, 144, 192, 3]), device=cpu, dtype=torch.uint8, is_shared=False),
+                        agents: TensorDict(
+                            fields={
+                                observation: TensorDict(
+                                    fields={
+                                        COLLECTIVE_REWARD: Tensor(shape=torch.Size([5, 7, 1]), device=cpu, dtype=torch.float64, is_shared=False),
+                                        READY_TO_SHOOT: Tensor(shape=torch.Size([5, 7, 1]), device=cpu, dtype=torch.float64, is_shared=False),
+                                        RGB: Tensor(shape=torch.Size([5, 7, 88, 88, 3]), device=cpu, dtype=torch.uint8, is_shared=False)},
+                                    batch_size=torch.Size([5, 7]),
+                                    device=cpu,
+                                    is_shared=False),
+                                reward: Tensor(shape=torch.Size([5, 7, 1]), device=cpu, dtype=torch.float64, is_shared=False)},
+                            batch_size=torch.Size([5, 7]),
+                            device=cpu,
+                            is_shared=False),
+                        done: Tensor(shape=torch.Size([5, 1]), device=cpu, dtype=torch.bool, is_shared=False),
+                        terminated: Tensor(shape=torch.Size([5, 1]), device=cpu, dtype=torch.bool, is_shared=False)},
+                    batch_size=torch.Size([5]),
+                    device=cpu,
+                    is_shared=False),
+                terminated: Tensor(shape=torch.Size([5, 1]), device=cpu, dtype=torch.bool, is_shared=False)},
+            batch_size=torch.Size([5]),
+            device=cpu,
+            is_shared=False)
 
 
     """
