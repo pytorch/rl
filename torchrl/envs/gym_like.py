78c92c0d85a97c741104d66c2d9ad2f26beb623e
Jonathanace <55035716+jonathanace@users.noreply.github.com>
2024-04-07 02:13:48 -0700

    [Documentation] Correct MaskedEnv Example in ActionMask Transform Documentation (#2060)
    (cherry picked from commit f85da4c2b45f2549f17a33d1757ea09700293d28)

--- torchrl/envs/gym_like.py
+++ torchrl/envs/gym_like.py
@@ -323,7 +323,7 @@ class GymLikeEnv(_EnvWrapper):
                 obs_dict, batch_size=tensordict.batch_size, _run_checks=False
             )
         tensordict_out = tensordict_out.to(self.device, non_blocking=True)
-            self._sync_device()
+        self._sync_device()
 
         if self.info_dict_reader and (info_dict is not None):
             if not isinstance(info_dict, dict):
