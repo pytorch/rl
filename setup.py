d88c6cdadeeeb2f13f7ec782d2631d453f027ac7
Vincent Moens <vmoens@meta.com>
2024-04-25 13:13:55 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -170,7 +170,7 @@ def _main(argv):
     if is_nightly:
         tensordict_dep = "tensordict-nightly"
     else:
-        tensordict_dep = "tensordict==0.4.0"
+        tensordict_dep = "tensordict>=0.4.0"
 
     if is_nightly:
         version = get_nightly_version()
