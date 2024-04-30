232dbb43eac9ed49854c7a44fd03d211f6f4b0c0
albert bou <albertbou92@gmail.com>
2024-04-30 15:34:08 +0200

    add example

--- torchrl/modules/utils/utils.py
+++ torchrl/modules/utils/utils.py
@@ -7,7 +7,48 @@ import warnings
 
 
 def get_primers_from_module(module):
-    """Get all tensordict primers from all submodules of a module."""
+    """Get all tensordict primers from all submodules of a module.
+
+    This method is useful for retrieving primers from modules that are contained within a
+    parent module.
+
+    Args:
+        module (torch.nn.Module): The parent module.
+
+    Returns:
+        TensorDictPrimer: A TensorDictPrimer Transform.
+
+    Example:
+        >>> from torchrl.modules.utils import get_primers_from_module
+        >>> from torchrl.modules import GRUModule, MLP
+        >>> from tensordict.nn import TensorDictModule, TensorDictSequential
+
+        # Define a GRU module
+        >>> gru_module = GRUModule(
+        ...     input_size=10,
+        ...     hidden_size=10,
+        ...     num_layers=1,
+        ...     in_keys=["input", "recurrent_state", "is_init"],
+        ...     out_keys=["features", ("next", "recurrent_state")],
+        ... )
+
+        # Define a head module
+        >>> head = TensorDictModule(
+        ...     MLP(
+        ...         in_features=10,
+        ...         out_features=10,
+        ...         num_cells=[],
+        ...     ),
+        ...     in_keys=["features"],
+        ...     out_keys=["output"],
+        ... )
+
+        # Create a sequential model
+        >>> model = TensorDictSequential(gru_module, head)
+
+        # Retrieve primers from the model
+        >>> primers = get_primers_from_module(model)
+    """
     primers = []
 
     def make_primers(submodule):
