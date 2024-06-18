32fb1816a19f65285815679c60960d66aa47ae6b
Vincent Moens <vmoens@meta.com>
2024-06-18 14:12:55 +0100

    amend

--- torchrl/envs/libs/brax.py
+++ torchrl/envs/libs/brax.py
@@ -676,8 +676,8 @@ class _BraxEnvStep(torch.autograd.Function):
             device=ctx.env.device,
             batch_size=ctx.env.batch_size,
         )
-        grad_action_np = _ndarray_to_tensor(grad_action)
-        assert grad_action_np.device == ctx.env.device, (grad_action, grad_action_np)
+        grad_action_np = _ndarray_to_tensor(grad_action).to(ctx.env.device)
+        assert grad_action_np.device == ctx.env.device, (grad_action, grad_action_np, grad_action.__dlpack_device__())
         grad_action = grad_action_np
         grad_state_qp = {
             key: val if key not in none_keys else None
