8438770c8d44addacbd003dbfc46c5619f0db164
Vincent Moens <vmoens@meta.com>
2024-06-20 13:27:36 +0100

    amend

--- sota-implementations/cql/cql_online.py
+++ sota-implementations/cql/cql_online.py
@@ -227,8 +227,10 @@ def main(cfg: "DictConfig"):  # noqa: F821
     torchrl_logger.info(f"Training took {execution_time:.2f} seconds to finish")
 
     collector.shutdown()
-    eval_env.close()
-    train_env.close()
+    if not eval_env.is_closed:
+        eval_env.close()
+    if not train_env.is_closed:
+        train_env.close()
 
 
 if __name__ == "__main__":
