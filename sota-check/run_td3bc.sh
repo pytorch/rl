13d700dcd3a0d44967660d1f263660e7e826fcd8
BY571 <sebastian.dittert@gmx.de>
2024-06-27 11:54:53 +0200

    update sota-check

--- sota-check/run_td3bc.sh
+++ sota-check/run_td3bc.sh
@@ -11,7 +11,7 @@ current_commit=$(git rev-parse --short HEAD)
 project_name="torchrl-example-check-$current_commit"
 group_name="td3bc_offline"
 export PYTHONPATH=$(dirname $(dirname $PWD))
-python $PYTHONPATH/sota-implementations/td3+bc/td3+bc.py \
+python $PYTHONPATH/sota-implementations/td3_bc/td3_bc.py \
   logger.backend=wandb \
   logger.project_name="$project_name" \
   logger.group_name="$group_name"
