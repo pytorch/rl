95b6a1689f84d5ca5ab3e844f24e576d09043582
Vincent Moens <vmoens@meta.com>
2024-07-08 16:09:34 +0100

    [BugFix] Fix tensordict private imports (#2275)

--- torchrl/envs/batched_envs.py
+++ torchrl/envs/batched_envs.py
@@ -27,7 +27,7 @@ from tensordict import (
     TensorDict,
     TensorDictBase,
 )
-from tensordict._tensordict import unravel_key
+from tensordict._C import unravel_key
 from torch import multiprocessing as mp
 from torchrl._utils import (
     _check_for_faulty_process,
