bf410171fea1824bf9a3ad222f824da3d8e0f187
Vincent Moens <vmoens@meta.com>
2024-07-09 16:05:11 +0100

    init

--- setup.py
+++ setup.py
@@ -274,6 +274,7 @@ def _main(argv):
             "Programming Language :: Python :: 3.9",
             "Programming Language :: Python :: 3.10",
             "Programming Language :: Python :: 3.11",
+            "Programming Language :: Python :: 3.12",
             "License :: OSI Approved :: MIT License",
             "Operating System :: OS Independent",
             "Development Status :: 4 - Beta",
