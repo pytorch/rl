f0b6480f040726d2ea84698456076965869c9d44
Vincent Moens <vmoens@meta.com>
2024-07-22 19:05:14 +0100

    Update (base update)
    [ghstack-poisoned]

---
+++ torchrl/data/map/__init__.py
@@ -0,0 +1,8 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+from .hash import BinaryToDecimal, RandomProjectionHash, SipHash
+from .query import HashToInt, QueryModule
+from .tdstorage import TensorDictMap, TensorMap
