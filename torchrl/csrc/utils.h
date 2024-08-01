05ecfd45ca3ccc0d15471af0fca05c4047105d58
Vincent Moens <vmoens@meta.com>
2024-08-01 11:39:40 -0400

    init

--- torchrl/csrc/utils.h
+++ torchrl/csrc/utils.h
@@ -2,58 +2,24 @@
 //
 // This source code is licensed under the MIT license found in the
 // LICENSE file in the root directory of this source tree.
+// utils.h
+
+#pragma once
 
 #include <torch/extension.h>
 #include <torch/torch.h>
 
-#include <iostream>
+torch::Tensor safetanh(torch::Tensor input, float eps = 1e-6);
+torch::Tensor safeatanh(torch::Tensor input, float eps = 1e-6);
 
-using namespace torch::autograd;
-
-class SafeTanh : public Function<SafeTanh> {
+class SafeTanh : public torch::autograd::Function<SafeTanh> {
  public:
-  static torch::Tensor forward(AutogradContext* ctx, torch::Tensor input,
-                               float eps = 1e-6) {
-    auto out = torch::tanh(input);
-    auto lim = 1.0 - eps;
-    out = out.clamp(-lim, lim);
-    ctx->save_for_backward({out});
-    return out;
-  }
-
-  static tensor_list backward(AutogradContext* ctx, tensor_list grad_outputs) {
-    auto saved = ctx->get_saved_variables();
-    auto out = saved[0];
-    auto go = grad_outputs[0];
-    auto grad = go * (1 - out * out);
-    return {grad, torch::Tensor()};
-  }
+  static torch::Tensor forward(torch::autograd::AutogradContext* ctx, torch::Tensor input, float eps);
+  static torch::autograd::tensor_list backward(torch::autograd::AutogradContext* ctx, torch::autograd::tensor_list grad_outputs);
 };
 
-torch::Tensor safetanh(torch::Tensor input, float eps = 1e-6) {
-  return SafeTanh::apply(input, eps);
-}
-
-class SafeInvTanh : public Function<SafeInvTanh> {
+class SafeInvTanh : public torch::autograd::Function<SafeInvTanh> {
  public:
-  static torch::Tensor forward(AutogradContext* ctx, torch::Tensor input,
-                               float eps = 1e-6) {
-    auto lim = 1.0 - eps;
-    auto intermediate = input.clamp(-lim, lim);
-    ctx->save_for_backward({intermediate});
-    auto out = torch::atanh(intermediate);
-    return out;
-  }
-
-  static tensor_list backward(AutogradContext* ctx, tensor_list grad_outputs) {
-    auto saved = ctx->get_saved_variables();
-    auto input = saved[0];
-    auto go = grad_outputs[0];
-    auto grad = go / (1 - input * input);
-    return {grad, torch::Tensor()};
-  }
+  static torch::Tensor forward(torch::autograd::AutogradContext* ctx, torch::Tensor input, float eps);
+  static torch::autograd::tensor_list backward(torch::autograd::AutogradContext* ctx, torch::autograd::tensor_list grad_outputs);
 };
-
-torch::Tensor safeatanh(torch::Tensor input, float eps = 1e-6) {
-  return SafeInvTanh::apply(input, eps);
-}
