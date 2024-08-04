65396fd4d7c8148846f2185e8d21de36f747060a
Vincent Moens <vmoens@meta.com>
2024-08-04 16:29:45 -0400

    Update (base update)
    [ghstack-poisoned]

--- torchrl/data/__init__.py
+++ torchrl/data/__init__.py
@@ -3,6 +3,18 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+from .map import (
+    BinaryToDecimal,
+    HashToInt,
+    MCTSChildren,
+    MCTSForest,
+    MCTSNode,
+    QueryModule,
+    RandomProjectionHash,
+    SipHash,
+    TensorDictMap,
+    TensorMap,
+)
 from .postprocs import MultiStep
 from .replay_buffers import (
     Flat2TED,
