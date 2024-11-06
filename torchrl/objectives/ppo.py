1f338815633fda6f8a8671fe9beaa7d97bf260d4
Pau Riba <pau.riba@helsing.ai>
2024-11-06 11:42:42 +0000

    No need to unsqueeze

--- torchrl/objectives/ppo.py
+++ torchrl/objectives/ppo.py
@@ -463,8 +463,8 @@ class PPOLoss(LossModule):
     def get_entropy_bonus(self, dist: d.Distribution) -> torch.Tensor:
         try:
             entropy = dist.entropy()
-            if is_tensor_collection(entropy) and hasattr(dist, "entropy_key"):
-                entropy = entropy.get(dist.entropy_key).unsqueeze(-1)
+            if is_tensor_collection(entropy):
+                entropy = entropy.get(dist.entropy_key)
         except NotImplementedError:
             x = dist.rsample((self.samples_mc_entropy,))
             log_prob = dist.log_prob(x)
