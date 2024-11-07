b5033cde494b9d4fd14e82db8445cfda23bc27a5
Vincent Moens <vmoens@meta.com>
2024-11-07 10:00:53 +0000

    amend

--- .github/unittest/linux_libs/scripts_habitat/run_test.sh
+++ .github/unittest/linux_libs/scripts_habitat/run_test.sh
@@ -46,7 +46,7 @@ mkdir data
 git lfs update
 
 #python -m habitat_sim.utils.datasets_download --uids rearrange_dataset_v1 rearrange_pick_dataset_v0 rearrange_task_assets --data-path ./data
-python -m habitat_sim.utils.datasets_download --uids rearrange_task_assets --data-path ./data --prune_lfs 0
+python -m habitat_sim.utils.datasets_download --uids rearrange_task_assets --data-path ./data --no-prune
 
 python -c "import habitat;import habitat.gym"
 python -c """from torchrl.envs.libs.habitat import HabitatEnv
