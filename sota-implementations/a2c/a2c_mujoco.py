fc024d4eb3fa7f0cb750d5419e74123b08b727d5
Vincent Moens <vmoens@meta.com>
2024-11-18 12:38:17 +0000

    Update (base update)
    [ghstack-poisoned]

--- sota-implementations/a2c/a2c_mujoco.py
+++ sota-implementations/a2c/a2c_mujoco.py
@@ -5,11 +5,7 @@
 import warnings
 
 import hydra
-
 import torch
-from tensordict.nn import CudaGraphModule
-from torchrl._utils import logger as torchrl_logger
-from torchrl.record import VideoRecorder
 
 torch.set_float32_matmul_precision("high")
 
@@ -22,13 +18,16 @@ def main(cfg: "DictConfig"):  # noqa: F821
     import torch.optim
     import tqdm
 
-    from torchrl._utils import timeit
+    from tensordict.nn import CudaGraphModule
+
+    from torchrl._utils import logger as torchrl_logger, timeit
     from torchrl.collectors import SyncDataCollector
     from torchrl.data import LazyTensorStorage, TensorDictReplayBuffer
     from torchrl.data.replay_buffers.samplers import SamplerWithoutReplacement
     from torchrl.envs import ExplorationType, set_exploration_type
     from torchrl.objectives import A2CLoss
     from torchrl.objectives.value.advantages import GAE
+    from torchrl.record import VideoRecorder
     from torchrl.record.loggers import generate_exp_name, get_logger
     from utils_mujoco import eval_model, make_env, make_ppo_models
 
