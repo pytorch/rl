d90b9e3d1a1a92e43aafa0b7693e45766795ea46
Vincent Moens <vmoens@meta.com>
2024-11-25 11:34:30 +0000

    [BugFix] Fix imports
    ghstack-source-id: db85f2611c1c0b22e9179b4fdd6c2dcea78ac8dd
    Pull Request resolved: https://github.com/pytorch/rl/pull/2605

--- benchmarks/test_objectives_benchmarks.py
+++ benchmarks/test_objectives_benchmarks.py
@@ -1152,4 +1152,7 @@ def test_iql_speed(
 
 if __name__ == "__main__":
     args, unknown = argparse.ArgumentParser().parse_known_args()
-    pytest.main([__file__, "--capture", "no", "--exitfirst", "--benchmark-group-by", "func"] + unknown)
+    pytest.main(
+        [__file__, "--capture", "no", "--exitfirst", "--benchmark-group-by", "func"]
+        + unknown
+    )
