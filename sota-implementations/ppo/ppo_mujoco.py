f601ccef52225ff3a533c0e7e2116759a54d1702
Vincent Moens <vmoens@meta.com>
2024-12-15 13:47:57 -0800

    Update
    [ghstack-poisoned]

--- sota-implementations/ppo/ppo_mujoco.py
+++ sota-implementations/ppo/ppo_mujoco.py
@@ -223,7 +223,8 @@ def main(cfg: "DictConfig"):  # noqa: F821
 
                 # Compute GAE
                 with torch.no_grad(), timeit("adv"):
-                    data = adv_module(data.to(device))
+                    torch.compiler.cudagraph_mark_step_begin()
+                    data = adv_module(data)
                 with timeit("rb - extend"):
                     # Update the data buffer
                     data_reshape = data.reshape(-1)
