ce2de4371f84fca171480c6a7aff60261f3a137d
Vincent Moens <vmoens@meta.com>
2024-12-17 17:20:35 +0000

    Update
    [ghstack-poisoned]

--- sota-implementations/cql/utils.py
+++ sota-implementations/cql/utils.py
@@ -185,7 +185,7 @@ def make_offline_replay_buffer(rb_cfg):
         dataset_id=rb_cfg.dataset,
         split_trajs=False,
         batch_size=rb_cfg.batch_size,
-        sampler=SamplerWithoutReplacement(drop_last=False),
+        sampler=SamplerWithoutReplacement(drop_last=True),
         prefetch=4,
         direct_download=True,
     )
