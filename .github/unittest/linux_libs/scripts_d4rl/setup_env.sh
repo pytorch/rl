9b0b2fccee1e94f2d8c01de37c447562becefd01
Vincent Moens <vmoens@meta.com>
2025-02-21 09:42:13 +0000

    amend

--- .github/unittest/linux_libs/scripts_d4rl/setup_env.sh
+++ .github/unittest/linux_libs/scripts_d4rl/setup_env.sh
@@ -10,7 +10,24 @@ set -v
 
 this_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 # Avoid error: "fatal: unsafe repository"
-apt-get update && apt-get install -y git wget gcc g++ unzip
+apt-get update && apt-get install -y \
+    git \
+    wget \
+    gcc \
+    g++ \
+    unzip \
+    curl \
+    patchelf \
+    libosmesa6-dev \
+    libgl1-mesa-glx \
+    libglfw3 \
+    swig3.0 \
+    libglew-dev \
+    libglvnd0 \
+    libgl1 \
+    libglx0 \
+    libegl1 \
+    libgles2
 
 git config --global --add safe.directory '*'
 root_dir="$(git rev-parse --show-toplevel)"
--- .github/unittest/linux_libs/scripts_gym/setup_env.sh
+++ .github/unittest/linux_libs/scripts_gym/setup_env.sh
@@ -7,9 +7,27 @@
 
 set -e
 
+apt-get update && apt-get install -y \
+    git \
+    wget \
+    gcc \
+    g++ \
+    unzip \
+    curl \
+    patchelf \
+    libosmesa6-dev \
+    libgl1-mesa-glx \
+    libglfw3 \
+    swig3.0 \
+    libglew-dev \
+    libglvnd0 \
+    libgl1 \
+    libglx0 \
+    libegl1 \
+    libgles2
+
 this_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 # Avoid error: "fatal: unsafe repository"
-apt-get update && apt-get install -y git wget gcc g++
 
 git config --global --add safe.directory '*'
 root_dir="$(git rev-parse --show-toplevel)"
