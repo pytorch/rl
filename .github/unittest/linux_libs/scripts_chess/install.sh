43bb521ae58be08af0602ec8f4726ed9618f482d
Vincent Moens <vmoens@meta.com>
2025-02-21 08:26:55 +0000

    amend

--- .github/unittest/linux_libs/scripts_chess/install.sh
+++ .github/unittest/linux_libs/scripts_chess/install.sh
@@ -4,6 +4,7 @@ unset PYTORCH_VERSION
 # For unittest, nightly PyTorch is used as the following section,
 # so no need to set PYTORCH_VERSION.
 # In fact, keeping PYTORCH_VERSION forces us to hardcode PyTorch version in config.
+apt-get update && apt-get install -y git wget gcc g++
 
 set -e
 
