9f12960aafc63f28384b20e756843a0531c1a50b
Vincent Moens <vmoens@meta.com>
2025-03-06 14:29:58 -0800

    Update (base update)
    [ghstack-poisoned]

---
+++ torchrl/modules/llm/__init__.py
@@ -0,0 +1,8 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+from .transformers_policy import from_hf_transformers
+
+__all__ = ["from_hf_transformers"]
