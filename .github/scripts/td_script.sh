18c4c8d634bd0ffc6929b4b1c03778eb0783ccb5
Vincent Moens <vmoens@meta.com>
2025-03-10 16:32:13 +0000

    Update (base update)
    [ghstack-poisoned]

--- .github/scripts/td_script.sh
+++ .github/scripts/td_script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-export TORCHRL_BUILD_VERSION=0.7.0
+export TORCHRL_BUILD_VERSION=0.8.0
 
-${CONDA_RUN} pip install git+https://github.com/pytorch/tensordict.git -U
+pip install git+https://github.com/pytorch/tensordict.git -U
