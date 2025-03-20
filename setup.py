966f436189a129a0b95dd1865d9953da3ef6b9f0
Vincent Moens <vmoens@meta.com>
2025-03-20 10:14:23 -0700

    Update
    [ghstack-poisoned]

--- setup.py
+++ setup.py
@@ -273,7 +273,7 @@ def _main(argv):
         ),
         ext_modules=get_extensions(),
         cmdclass={
-            "build_ext": BuildExtension.with_options(),
+            "build_ext": BuildExtension.with_options(no_python_abi_suffix=True),
             "clean": clean,
         },
         install_requires=[
