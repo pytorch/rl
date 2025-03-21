860de555ac48707a979d70bdcb0a4c841f491947
Vincent Moens <vmoens@meta.com>
2025-03-20 18:52:29 -0700

    Update
    [ghstack-poisoned]

--- torchrl/envs/async_envs.py
+++ torchrl/envs/async_envs.py
@@ -5,6 +5,7 @@
 from __future__ import annotations
 
 import multiprocessing
+import threading
 from functools import partial
 
 # import queue
@@ -17,31 +18,121 @@ from tensordict import lazy_stack, maybe_dense_stack, TensorDict, TensorDictBase
 
 from tensordict.tensorclass import NonTensorData
 
-from torchrl.envs import EnvBase, GymEnv
+from torchrl.envs.libs import GymEnv
+from torchrl.envs.common import _EnvPostInit, EnvBase
+import queue
+from concurrent.futures import ThreadPoolExecutor, as_completed
+import abc
 
+class _AsyncEnvMeta(_EnvPostInit):
+    def __call__(cls, *args, **kwargs):
+        backend = kwargs.get("backend", "threading")
+        if cls is AsyncEnvPool:
+            if backend == "threading":
+                instance: MTAsyncEnvPool = MTAsyncEnvPool(*args, **kwargs)
+            elif backend == "multiprocessing":
+                instance: MPAsyncEnvPool = MPAsyncEnvPool(*args, **kwargs)
+            elif backend == "asyncio":
+                instance: MPAsyncEnvPool = MPAsyncEnvPool(*args, **kwargs)
+            else:
+                raise NotImplementedError
+            return instance
+        else:
+            return super().__call__(*args, **kwargs)
 
-class AsyncEnv(EnvBase):
+class AsyncEnvPool(EnvBase, metaclass=_AsyncEnvMeta):
     _env_idx_key = "_env_idx"
 
     def __init__(
         self,
-        env_maker: Callable[[], EnvBase]
-        | EnvBase
-        | list[EnvBase]
-        | list[Callable[[], EnvBase]],
+        env_makers: Callable[[], EnvBase]
+                    | EnvBase
+                    | list[EnvBase]
+                    | list[Callable[[], EnvBase]],
+        *,
         backend: Literal[
-            "multithreading", "multiprocessing", "asyncio"
-        ] = "multithreading",
+            "threading", "multiprocessing", "asyncio"
+        ] = "threading",
         stack: Literal["dense", "maybe_dense", "lazy"] = "dense",
     ) -> None:
-        if not isinstance(env_maker, Sequence):
-            env_maker = [env_maker]
+        if not isinstance(env_makers, Sequence):
+            env_makers = [env_makers]
         self.min_get = 1
-        self.env_maker = env_maker
-        self.num_envs = len(env_maker)
+        self.env_makers = env_makers
+        self.num_envs = len(env_makers)
         self.backend = backend
-        if backend != "multithreading":
+
+        if stack == "dense":
+            self._stack_func = torch.stack
+        elif stack == "maybe_dense":
+            self._stack_func = maybe_dense_stack
+        elif stack == "lazy":
+            self._stack_func = lazy_stack
+        else:
             raise NotImplementedError
+
+        self._setup()
+
+    def _reset(self, tensordict: TensorDictBase, **kwargs) -> TensorDictBase:
+        raise NotImplementedError
+    def _step(
+        self,
+        tensordict: TensorDictBase,
+    ) -> TensorDictBase:
+        raise NotImplementedError
+    def _set_seed(self, seed: int | None):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def _setup(self) -> None:
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def async_step_send(self, tensordict: TensorDictBase) -> None:
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def async_step_recv(self, min_get: int | None = None) -> TensorDictBase:
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def async_step_and_maybe_reset_send(self, tensordict: TensorDictBase) -> None:
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def async_step_and_maybe_reset_recv(self, min_get: int | None = None) -> TensorDictBase:
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def async_reset_send(self, tensordict: TensorDictBase) -> None:
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def async_reset_recv(self, min_get: int | None = None) -> TensorDictBase:
+        raise NotImplementedError
+
+
+    def __del__(self):
+        self._maybe_shutdown()
+
+    def _maybe_shutdown(self):
+        try:
+            self.shutdown()
+        except Exception:
+            pass
+    @abc.abstractmethod
+    def shutdown(self):
+        raise NotImplementedError
+
+    def close(self, *, raise_if_closed: bool = True):
+        if raise_if_closed:
+            self.shutdown()
+        else:
+            self._maybe_shutdown()
+
+
+class MPAsyncEnvPool(AsyncEnvPool):
+    def _setup(self) -> None:
         self.step_queue = Queue(maxsize=self.num_envs)
         self.reset_queue = Queue(maxsize=self.num_envs)
         self.step_reset_queue = Queue(maxsize=self.num_envs)
@@ -57,10 +148,10 @@ class AsyncEnv(EnvBase):
         for i in range(num_threads):
             # thread = threading.Thread(target=_env_exec, kwargs={"i": i, "env_or_factory": self.env_maker[i], "input_queue": self.input_queue[i], "step_queue": self.step_queue, "reset_queue": self.reset_queue})
             thread = multiprocessing.Process(
-                target=_env_exec,
+                target=self._env_exec,
                 kwargs={
                     "i": i,
-                    "env_or_factory": self.env_maker[i],
+                    "env_or_factory": self.env_makers[i],
                     "input_queue": self.input_queue[i],
                     "step_reset_queue": self.step_reset_queue,
                     "step_queue": self.step_queue,
@@ -70,33 +161,6 @@ class AsyncEnv(EnvBase):
             self.threads.append(thread)
             thread.start()
 
-        if stack == "dense":
-            self._stack_func = torch.stack
-        elif stack == "maybe_dense":
-            self._stack_func = maybe_dense_stack
-        elif stack == "lazy":
-            self._stack_func = lazy_stack
-        else:
-            raise NotImplementedError
-
-    def step(self, tensordict):
-        raise NotImplementedError
-
-    def _step(self, tensordict):
-        raise NotImplementedError
-
-    def _reset(self, tensordict):
-        raise NotImplementedError
-
-    def _set_seed(self, seed: int | None):
-        raise NotImplementedError
-
-    def reset(
-        self,
-        tensordict: TensorDictBase | None = None,
-        **kwargs,
-    ) -> TensorDictBase:
-        raise NotImplementedError
 
     def async_step_send(self, tensordict: TensorDictBase) -> None:
         # puts tds in a queue and ask for env.step
@@ -165,31 +229,25 @@ class AsyncEnv(EnvBase):
 
     def _wait_for_one_and_get(self, q, min_get):
         items = [q.get()]
+        q.task_done()
 
-        print("no wait")
         try:
             while True:
                 item = q.get_nowait()
                 items.append(item)
+                q.task_done()
         except Empty:
             pass
-        print("get", items)
+
         # Retrieve all other available items
         while len(items) < min_get:
             item = q.get()
             items.append(item)
+            q.task_done()
 
         return items
 
-    def __del__(self):
-        self._maybe_shutdown()
-
-    def _maybe_shutdown(self):
-        try:
-            self.shutdown()
-        except Exception:
-            pass
-
+    @abc.abstractmethod
     def shutdown(self):
         for env_id in range(self.num_envs):
             self.input_queue[env_id].put(("shutdown", None))
@@ -197,51 +255,144 @@ class AsyncEnv(EnvBase):
         for thread in self.threads:
             thread.join()
 
-    def close(self, *, raise_if_closed: bool = True):
-        if raise_if_closed:
-            self.shutdown()
+    @classmethod
+    def _env_exec(
+            cls, i, env_or_factory, input_queue, step_queue, step_reset_queue, reset_queue
+    ):
+        if not isinstance(env_or_factory, EnvBase):
+            env = env_or_factory()
         else:
-            self._maybe_shutdown()
+            env = env_or_factory
+
+        while True:
+            print("getting msg")
+            msg, data = input_queue.get()
+            print("msg", msg)
+            if msg == "reset":
+                data = env.reset(data.copy())
+                data.set("_env_idx", NonTensorData(i))
+                reset_queue.put(data)
+            elif msg == "step_and_maybe_reset":
+                data, data_ = env.step_and_maybe_reset(data.copy())
+                data.set("_env_idx", NonTensorData(i))
+                data_.set("_env_idx", NonTensorData(i))
+                step_reset_queue.put((data, data_))
+            elif msg == "step":
+                data = env.step(data.copy())
+                data.set("_env_idx", NonTensorData(i))
+                step_queue.put(data)
+            elif msg == "shutdown":
+                env.close()
+                break
+            else:
+                raise RuntimeError
+            q.task_done()
+        return
 
 
-def _env_exec(
-    i, env_or_factory, input_queue, step_queue, step_reset_queue, reset_queue
-):
-    if not isinstance(env_or_factory, EnvBase):
-        env = env_or_factory()
-    else:
-        env = env_or_factory
-    print("env", env)
-    while True:
-        print("getting msg")
-        msg, data = input_queue.get()
-        print("msg", msg)
-        if msg == "reset":
-            data = env.reset(data.copy())
-            data.set("_env_idx", NonTensorData(i))
-            reset_queue.put(data)
-        elif msg == "step_and_maybe_reset":
-            data, data_ = env.step_and_maybe_reset(data.copy())
-            data.set("_env_idx", NonTensorData(i))
-            data_.set("_env_idx", NonTensorData(i))
-            step_reset_queue.put((data, data_))
-        elif msg == "step":
-            data = env.step(data.copy())
-            data.set("_env_idx", NonTensorData(i))
-            step_queue.put(data)
-        elif msg == "shutdown":
-            env.close()
-            return
-        else:
-            raise RuntimeError
+class MTAsyncEnvPool(AsyncEnvPool):
+    def _setup(self) -> None:
+        self._pool = ThreadPoolExecutor(max_workers=self.num_envs)
+        self.envs = [
+            env_factory() if not isinstance(env_factory, EnvBase) else env_factory for env_factory in self.env_makers
+        ]
+        self._reset_futures = []
+        self._step_futures = []
+        self._step_and_maybe_reset_futures = []
 
+    @classmethod
+    def _step_func(cls, env_td: Tuple[EnvBase, TensorDictBase]):
+        env, td = env_td
+        return env.step(td)
+    @classmethod
+    def _reset_func(cls, env_td: Tuple[EnvBase, TensorDictBase]):
+        env, td = env_td
+        return env.reset(td)
+    @classmethod
+    def _step_and_maybe_reset_func(cls, env_td: Tuple[EnvBase, TensorDictBase]):
+        env, td = env_td
+        return env.step_and_maybe_reset(td)
+
+    def async_step_send(self, tensordict: TensorDictBase) -> None:
+        idx = tensordict[self._env_idx_key]
+        if isinstance(idx, int):
+            idx = [idx]
+        tds = tensordict.unbind(0)
+        envs = [self.envs[idx] for idx in idx]
+        futures = [self._pool.submit(self._step_func, (env, td)) for env, td in zip(envs, tds)]
+        self._step_futures.extend(futures)
+
+    def async_step_recv(self, min_get: int | None = None) -> TensorDictBase:
+        if min_get is None:
+            min_get = self.min_get
+        results = []
+        futures = self._step_futures
+        completed_futures = []
+        for future in as_completed(futures):
+            results.append(future.result())
+            completed_futures.append(future)
+            if len(results) >= min_get and sum([f.done() for f in futures]) == 0:
+                break
+        self._step_futures = [f for f in self._step_futures if f not in completed_futures]
+        return self._stack_func(results)
+
+    def async_step_and_maybe_reset_send(self, tensordict: TensorDictBase) -> None:
+        idx = tensordict[self._env_idx_key]
+        if isinstance(idx, int):
+            idx = [idx]
+        tds = tensordict.unbind(0)
+        envs = [self.envs[idx] for idx in idx]
+        futures = [self._pool.submit(self._step_and_maybe_reset_func, (env, td)) for env, td in zip(envs, tds)]
+        self._step_and_maybe_reset_futures.extend(futures)
+
+    def async_step_and_maybe_reset_recv(self, min_get: int | None = None) -> TensorDictBase:
+        if min_get is None:
+            min_get = self.min_get
+        results = []
+        futures = self._step_and_maybe_reset_futures
+        completed_futures = []
+        for future in as_completed(futures):
+            results.append(future.result())
+            completed_futures.append(future)
+            if len(results) >= min_get and sum([f.done() for f in futures]) == 0:
+                break
+        self._step_and_maybe_reset_futures = [f for f in self._step_and_maybe_reset_futures if f not in completed_futures]
+        return self._stack_func(results)
+
+    def async_reset_send(self, tensordict: TensorDictBase) -> None:
+        idx = tensordict[self._env_idx_key]
+        if isinstance(idx, int):
+            idx = [idx]
+        tds = tensordict.unbind(0)
+        envs = [self.envs[idx] for idx in idx]
+        futures = [self._pool.submit(self._reset_func, (env, td)) for env, td in zip(envs, tds)]
+        self._reset_futures.extend(futures)
+
+    def async_reset_recv(self, min_get: int | None = None) -> TensorDictBase:
+        if min_get is None:
+            min_get = self.min_get
+        results = []
+        futures = self._reset_futures
+        completed_futures = []
+        for future in as_completed(futures):
+            results.append(future.result())
+            completed_futures.append(future)
+            if len(results) >= min_get and sum([f.done() for f in futures]) == 0:
+                break
+        self._reset_futures = [f for f in self._reset_futures if f not in completed_futures]
+        print('results', results)
+        return self._stack_func(results)
+
+    def shutdown(self):
+        self._pool.shutdown()
 
 if __name__ == "__main__":
-    envs = AsyncEnv(
-        [partial(GymEnv, "Pendulum-v1"), partial(GymEnv, "Pendulum-v1")], stack="lazy"
+    torch.set_num_threads(10)
+    envs = AsyncEnvPool(
+        [partial(GymEnv, "Pendulum-v1"), partial(GymEnv, "Pendulum-v1")], stack="lazy", backend="threading",
     )
     print("send")
-    envs.async_reset_send(TensorDict(_env_idx=range(2), batch_size=2))
+    envs.async_reset_send(TensorDict(_env_idx=torch.arange(2), batch_size=2))
     print("recv")
     r = envs.async_reset_recv(min_get=2)
     print(r)
