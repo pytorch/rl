d49b20e199282a0a5542b2b17aa3e22e6c4fe53f
Vincent Moens <vmoens@meta.com>
2025-03-26 17:25:13 +0000

    Update
    [ghstack-poisoned]

--- .github/scripts/td_script.sh
+++ .github/scripts/td_script.sh
@@ -3,12 +3,4 @@
 export TORCHRL_BUILD_VERSION=0.7.0
 pip install --upgrade setuptools
 
-if pip list | grep -q torch; then
-    echo "Torch is installed."
-    ${CONDA_RUN} pip install git+https://github.com/pytorch/tensordict.git -U
-elif [[ -n "${SMOKE_TEST_SCRIPT:-}" ]]; then
-    ${CONDA_RUN} ${PIP_INSTALL_TORCH}
-    ${CONDA_RUN} pip install git+https://github.com/pytorch/tensordict.git -U
-else
-    echo "Torch is not installed - tensordict will be installed later."
-fi
+${CONDA_RUN} pip install git+https://github.com/pytorch/tensordict.git -U
