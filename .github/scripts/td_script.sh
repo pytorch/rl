df443d681a5260f3a08aa0113be86b2795d338b0
Vincent Moens <vmoens@meta.com>
2025-03-27 09:13:38 +0000

    Update
    [ghstack-poisoned]

--- .github/scripts/td_script.sh
+++ .github/scripts/td_script.sh
@@ -3,13 +3,23 @@
 export TORCHRL_BUILD_VERSION=0.7.0
 pip install --upgrade setuptools
 
+# Check if ARCH is set to aarch64
+ARCH=${ARCH:-}  # This sets ARCH to an empty string if it's not defined
+
 if pip list | grep -q torch; then
     echo "Torch is installed."
-#    ${CONDA_RUN} pip install git+https://github.com/pytorch/tensordict.git -U --no-deps
-    ${CONDA_RUN} pip install tensordict-nightly -U
+    if [[ "$ARCH" == "aarch64" ]]; then
+        ${CONDA_RUN} pip install git+https://github.com/pytorch/tensordict.git -U --no-deps
+    else
+        ${CONDA_RUN} pip install tensordict-nightly -U
+    fi
 elif [[ -n "${SMOKE_TEST_SCRIPT:-}" ]]; then
     ${CONDA_RUN} ${PIP_INSTALL_TORCH}
-    ${CONDA_RUN} pip install tensordict-nightly -U
+    if [[ "$ARCH" == "aarch64" ]]; then
+        ${CONDA_RUN} pip install git+https://github.com/pytorch/tensordict.git -U --no-deps
+    else
+        ${CONDA_RUN} pip install tensordict-nightly -U
+    fi
 else
     echo "Torch is not installed - tensordict will be installed later."
 fi
