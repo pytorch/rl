905a02251b856d3213bd22fd0d1d5e39b96fbd1a
Vincent Moens <vmoens@meta.com>
2025-04-25 08:33:05 +0100

    Update
    [ghstack-poisoned]

--- .github/scripts/pre-build-script-win.sh
+++ .github/scripts/pre-build-script-win.sh
@@ -5,5 +5,5 @@ pip install --upgrade setuptools
 export TORCHRL_BUILD_VERSION=0.8.0
 
 ${CONDA_RUN} pip install "pybind11[global]"
-${CONDA_RUN} conda install anaconda::cmake
+${CONDA_RUN} conda install anaconda::cmake -y
 ${CONDA_RUN} pip install git+https://github.com/pytorch/tensordict.git -U
