b0c58e9461cf08890b391dfe45c42c4aa6240231
Vincent Moens <vmoens@meta.com>
2025-04-25 08:15:36 +0100

    Update
    [ghstack-poisoned]

--- .github/scripts/pre-build-script.sh
+++ .github/scripts/pre-build-script.sh
@@ -2,4 +2,6 @@
 
 pip install --upgrade setuptools
 
+${CONDA_RUN} pip install "pybind11[global]"
+${CONDA_RUN} conda install anaconda::cmake
 ${CONDA_RUN} pip install git+https://github.com/pytorch/tensordict.git -U
--- .github/unittest/linux_olddeps/scripts_gym_0_13/setup_env.sh
+++ .github/unittest/linux_olddeps/scripts_gym_0_13/setup_env.sh
@@ -6,10 +6,24 @@
 # Do not install PyTorch and torchvision here, otherwise they also get cached.
 
 set -e
+set -v
 
 this_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-# Avoid error: "fatal: unsafe repository"
-apt-get update && apt-get install -y git wget gcc g++ cmake
+
+apt-get update && apt-get upgrade -y
+apt-get install -y vim git wget libsdl2-dev libsdl2-2.0-0 cmake
+
+apt-get install -y libglfw3 libgl1-mesa-glx libosmesa6 libglew-dev
+apt-get install -y libglvnd0 libgl1 libglx0 libegl1 libgles2
+
+if [ "${CU_VERSION:-}" == cpu ] ; then
+  # solves version `GLIBCXX_3.4.29' not found for tensorboard
+#    apt-get install -y gcc-4.9
+  apt-get upgrade -y libstdc++6
+  apt-get dist-upgrade -y
+else
+  apt-get install -y g++ gcc
+fi
 
 git config --global --add safe.directory '*'
 root_dir="$(git rev-parse --show-toplevel)"
