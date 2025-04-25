66ed4e66b8dbbca8c8549955a17ca0372fd2589e
Vincent Moens <vmoens@meta.com>
2025-04-25 13:01:46 +0100

    Update (base update)
    [ghstack-poisoned]

--- .github/scripts/td_script.sh
+++ .github/scripts/td_script.sh
@@ -8,22 +8,16 @@ ARCH=${ARCH:-}  # This sets ARCH to an empty string if it's not defined
 
 if pip list | grep -q torch; then
     echo "Torch is installed."
-    if [[ "$ARCH" == "aarch64" ]]; then
-        ${CONDA_RUN} pip install "pybind11[global]"
-        ${CONDA_RUN} conda install anaconda::cmake -y
-        ${CONDA_RUN} pip install git+https://github.com/pytorch/tensordict.git -U --no-deps
-    else
-        ${CONDA_RUN} pip install tensordict-nightly -U
-    fi
+    ${CONDA_RUN} pip install "pybind11[global]"
+    ${CONDA_RUN} conda install anaconda::cmake -y
+    ${CONDA_RUN} pip install git+https://github.com/pytorch/tensordict.git -U --no-deps
 elif [[ -n "${SMOKE_TEST_SCRIPT:-}" ]]; then
     ${CONDA_RUN} ${PIP_INSTALL_TORCH}
-    if [[ "$ARCH" == "aarch64" ]]; then
-        ${CONDA_RUN} pip install "pybind11[global]"
-        ${CONDA_RUN} conda install anaconda::cmake -y
-        ${CONDA_RUN} pip install git+https://github.com/pytorch/tensordict.git -U --no-deps
-    else
-        ${CONDA_RUN} pip install tensordict-nightly -U
-    fi
+    #    TODO: revert when nightlies of tensordict are fixed
+    #    if [[ "$ARCH" == "aarch64" ]]; then
+    ${CONDA_RUN} pip install "pybind11[global]"
+    ${CONDA_RUN} conda install anaconda::cmake -y
+    ${CONDA_RUN} pip install git+https://github.com/pytorch/tensordict.git -U --no-deps
 else
     echo "Torch is not installed - tensordict will be installed later."
 fi
