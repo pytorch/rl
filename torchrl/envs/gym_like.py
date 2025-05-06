321157f2689117ea0c8c6ec190a91d0e41178830
Vincent Moens <vmoens@meta.com>
2025-05-06 09:54:14 +0100

    Update
    [ghstack-poisoned]

--- torchrl/envs/gym_like.py
+++ torchrl/envs/gym_like.py
@@ -515,7 +515,7 @@ class GymLikeEnv(_EnvWrapper):
     def _reset(
         self, tensordict: TensorDictBase | None = None, **kwargs
     ) -> TensorDictBase:
-        if "_reset" in tensordict and not tensordict["_reset"].all():
+        if tensordict is not None and "_reset" in tensordict and not tensordict["_reset"].all():
             raise RuntimeError("Partial resets are not handled at this level.")
         obs, info = self._reset_output_transform(self._env.reset(**kwargs))
 
