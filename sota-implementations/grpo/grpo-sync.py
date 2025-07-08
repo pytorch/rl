9d8eac89e2ce424b247c057579a7b52462b71bbf
vmoens <vincentmoens@gmail.com>
2025-07-08 12:14:04 +0100

    amend

--- sota-implementations/grpo/grpo-sync.py
+++ sota-implementations/grpo/grpo-sync.py
@@ -209,6 +209,7 @@ def train(
                 with timeit("forward_pass"):
                     # Forward pass with mixed precision
                     with autocast("cuda", enabled=cfg.train.mixed_precision):
+                        torchrl_logger.info(f"Batch for forward pass: {batch.unbind(0)}")
                         loss = loss_fn(batch)
                         loss_val = (
                             loss.mean(reduce=True)
