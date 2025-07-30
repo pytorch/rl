a58db4f82856806f36f93aee33c7a42fd40ca917
Ibinarriaga8 <202206789@alu.comillas.edu>
2025-07-30 11:16:44 +0200

    [Feature] Add command for discrete CQL offline in test-sota

--- .github/unittest/linux_sota/scripts/test_sota.py
+++ .github/unittest/linux_sota/scripts/test_sota.py
@@ -106,6 +106,16 @@ commands = {
   replay_buffer.size=120 \
   logger.backend=
 """,
+    "discrete_cql_offline": """python sota-implementations/cql/discrete_cql_offline.py \
+  collector.total_frames=48 \
+  collector.init_random_frames=10 \
+  optim.batch_size=10 \
+  collector.frames_per_batch=16 \
+  collector.env_per_collector=2 \
+  replay_buffer.size=120 \
+  env.name=CartPole-v1 \
+  logger.backend=
+""",
     "redq": """python sota-implementations/redq/redq.py \
   num_workers=4 \
   collector.total_frames=48 \
