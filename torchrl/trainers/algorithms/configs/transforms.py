34d47a428464000024afa49603ff66ef8125a538
Ben Sprenger <ben.sprenger@rogers.com>
2025-12-12 00:06:44 -0800

    [BugFix] use correct field names in InitTrackerConfig (#3245)

--- torchrl/trainers/algorithms/configs/transforms.py
+++ torchrl/trainers/algorithms/configs/transforms.py
@@ -585,8 +585,7 @@ class RandomCropTensorDictConfig(TransformConfig):
 class InitTrackerConfig(TransformConfig):
     """Configuration for InitTracker transform."""
 
-    in_keys: list[str] | None = None
-    out_keys: list[str] | None = None
+    init_key: str | None = None
     _target_: str = "torchrl.envs.transforms.transforms.InitTracker"
 
     def __post_init__(self) -> None:
