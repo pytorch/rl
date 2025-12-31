1d41cca2c57dfeb8a504a9907f79b3aa7ec78cb5
vmoens <vincentmoens@gmail.com>
2025-12-31 09:09:06 +0000

    Update (base update)
    [ghstack-poisoned]

--- torchrl/testing/mp_helpers.py
+++ torchrl/testing/mp_helpers.py
@@ -7,6 +7,7 @@ from torchrl.data.utils import CloudpickleWrapper
 
 def decorate_thread_sub_func(func, num_threads):
     """Decorate a function to assert that the number of threads is correct."""
+
     def new_func(*args, **kwargs):
         assert torch.get_num_threads() == num_threads
         return func(*args, **kwargs)
