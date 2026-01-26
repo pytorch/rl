9b1e0194f6dd3741ae76f39a112d2cdf0f79b646
paramthakkar123 <paramthakkar864@gmail.com>
2026-01-26 18:36:59 +0530

    [Feature] Add __getattr__ in _dispatch_caller_parallel

--- torchrl/envs/batched_envs.py
+++ torchrl/envs/batched_envs.py
@@ -96,6 +96,13 @@ class _dispatch_caller_parallel:
         # if the object returned is not a callable
         return iter(self.__call__())
 
+    def __getattr__(self, name):
+        """Support chained attribute access: env_parallel.a.b -> sends ('a','b') to workers."""
+        if isinstance(self.attr, (tuple, list)):
+            new_attr = tuple([*self.attr, name])
+        else:
+            new_attr = (self.attr, name)
+        return _dispatch_caller_parallel(new_attr, self.parallel_env)
 
 class _dispatch_caller_serial:
     def __init__(self, list_callable: list[Callable, Any]):
@@ -2827,7 +2834,15 @@ def _run_worker_pipe_shared_mem(
         else:
             err_msg = f"{cmd} from env"
             try:
-                attr = getattr(env, cmd)
+                def _resolve_nested_attr(obj, attr):
+                    if isinstance(attr, (tuple, list)):
+                        cur = obj
+                        for _a in attr:
+                            cur = getattr(cur, _a)
+                        return cur
+                    return getattr(obj, attr)
+                
+                attr = _resolve_nested_attr(env, cmd)
                 if callable(attr):
                     args, kwargs = data
                     args_replace = []
