a74ecbc0117c98778f99da1762830c8473205f9a
Vincent Moens <vincentmoens@gmail.com>
2026-02-10 20:30:20 -0800

    Update (base update)
    [ghstack-poisoned]

---
+++ torchrl/modules/inference_server/__init__.py
@@ -0,0 +1,17 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+from torchrl.modules.inference_server._mp import MPTransport
+from torchrl.modules.inference_server._server import InferenceClient, InferenceServer
+from torchrl.modules.inference_server._threading import ThreadingTransport
+from torchrl.modules.inference_server._transport import InferenceTransport
+
+__all__ = [
+    "InferenceClient",
+    "InferenceServer",
+    "InferenceTransport",
+    "MPTransport",
+    "ThreadingTransport",
+]
