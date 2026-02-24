8ac3d03df3d6ec210bcc87f2249b8f75776e9176
Vincent Moens <vincentmoens@gmail.com>
2026-02-24 14:44:58 +0000

    Update (base update)
    [ghstack-poisoned]

--- torchrl/trainers/algorithms/__init__.py
+++ torchrl/trainers/algorithms/__init__.py
@@ -5,7 +5,9 @@
 
 from __future__ import annotations
 
+from .ddpg import DDPGTrainer
+from .dqn import DQNTrainer
 from .ppo import PPOTrainer
 from .sac import SACTrainer
 
-__all__ = ["PPOTrainer", "SACTrainer"]
+__all__ = ["DDPGTrainer", "DQNTrainer", "PPOTrainer", "SACTrainer"]
