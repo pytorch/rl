5b6de07b085e1cf50a90b377f6527f948698c009
Vincent Moens <vincentmoens@gmail.com>
2026-08-10 21:05:02 +0100

    [BugFix] Fix tutorial frame accounting (#4087)

--- tutorials/sphinx-tutorials/export.py
+++ tutorials/sphinx-tutorials/export.py
@@ -121,6 +121,11 @@ t0 = time.time()
 for data in collector:
     # Write data in replay buffer
     rb.extend(data)
+    num_frames = data.numel()
+    total_count += num_frames
+    total_episodes += data["next", "done"].sum().item()
+    # Update the exploration factor once per collected frame
+    exploration_module.step(num_frames)
     max_length = rb[:]["next", "step_count"].max()
     if len(rb) > init_rand_steps:
         # Optim loop (we do several optim steps
@@ -131,12 +136,8 @@ for data in collector:
             loss_vals["loss"].backward()
             optim.step()
             optim.zero_grad()
-            # Update exploration factor
-            exploration_module.step(data.numel())
             # Update target params
             updater.step()
-            total_count += data.numel()
-            total_episodes += data["next", "done"].sum()
     if max_length > 200:
         break
 
