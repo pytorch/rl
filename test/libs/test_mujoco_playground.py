2e46020bd8a0b892e7e7142c09aa88d286df4715
Vincent Moens <vincentmoens@gmail.com>
2026-09-12 08:49:35 +0100

    [Feature] Shared buffers for ParallelEnv workers that report their own metadata (#4341)

--- test/libs/test_mujoco_playground.py
+++ test/libs/test_mujoco_playground.py
@@ -195,7 +195,7 @@ class TestMujocoPlayground:
             assert isinstance(env, ParallelEnv)
             assert env.num_workers == 3
             assert env._metadata_from_workers
-            assert env._use_buffers is False
+            assert env._use_buffers == (not env.meta_data.has_dynamic_specs)
             assert not env.is_closed
             env.reset()
         finally:
